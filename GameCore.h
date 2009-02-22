c67329360f4e8721b7c99012cdfc55db2a91c9e7
Remy Demarest <remy.demarest@gmail.com>
2009-02-22 16:46:57 +0000

    Interface proposal
    git-svn-id: https://openemu.svn.sourceforge.net/svnroot/openemu/OpenEmu@104 a16923d9-94c8-4a39-ae78-11df2d60a2ba

--- GameCore.h
+++ GameCore.h
@@ -7,6 +7,8 @@
  *
  */
 
+#import <Cocoa/Cocoa.h>
+
 @protocol GameCore <NSObject>
 
 #pragma mark Execution
@@ -43,6 +45,57 @@
 @optional
 - (void) saveState: (NSString *) fileName;
 - (void) loadState: (NSString *) fileName;
+- (void) requestAudio: (int) frames inBuffer: (void*)buf;
+- (NSSize) outputSize;
+- (void) setRandomByte;
+@end
+
+@class GameDocument;
+
+@interface GameCore : NSObject
+{
+	NSThread *emulationThread;
+	GameDocument *document;
+}
+
+@property(assign) GameDocument *document;
+
+- (id)initWithDocument:(GameDocument *)document;
+
+#pragma mark Execution
+- (void)pauseEmulation:(BOOL) b;
+- (void)stopEmulation;
+- (void)startEmulation;
+- (void)resetEmulation;
+- (void)executeFrame;
+
+- (BOOL)loadFileAtPath:(NSString*)path;
+
+#pragma mark Video
+@property(readonly) NSInteger width;
+@property(readonly) NSInteger height;
+@property(readonly) const unsigned char *buffer;
+@property(readonly) GLenum pixelFormat;
+@property(readonly) GLenum pixelType;
+@property(readonly) GLenum internalPixelFormat;
+
+#pragma mark Audio
+@property(readonly) const UInt16 *soundBuffer;
+@property(readonly) NSInteger channelCount;
+@property(readonly) NSInteger frameSampleCount;
+@property(readonly) NSInteger soundBufferSize;
+@property(readonly) NSInteger sampleRate;
+
+#pragma mark Input
+- (void)player:(NSInteger)thePlayer didPressButton:(NSInteger)gameButton;
+- (void)player:(NSInteger)thePlayer didReleaseButton:(NSInteger)gameButton;
+
+#pragma mark Optional
+- (void)saveStateToFileAtPath:(NSString *)fileName;
+- (void)loadStateFromFileAtPath:(NSString *)fileName;
+
+
+#pragma mark Optional
 - (void) requestAudio: (int) frames inBuffer: (void*)buf;
 - (NSSize) outputSize;
 - (void) setRandomByte;
