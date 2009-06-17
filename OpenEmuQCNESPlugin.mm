24502a4e371a045e8a43fe6ee643023a28ac8864
Remy Demarest <remy.demarest@gmail.com>
2009-06-17 00:20:40 +0000

    All files are now licensed to BSD w00t
    git-svn-id: https://openemu.svn.sourceforge.net/svnroot/openemu/OpenEmu@311 a16923d9-94c8-4a39-ae78-11df2d60a2ba

--- OpenEmuQCNESPlugin.mm
+++ OpenEmuQCNESPlugin.mm
@@ -1,9 +1,29 @@
-//
-//  OpenEmuQCNESPlugin.m
-//  OpenEmuQCNES
-//
-//  A NES-only QC plugin for teh glitchy insanity.  Started by Dan Winckler on 11/16/08.
-//
+/*
+ Copyright (c) 2009, OpenEmu Team
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the OpenEmu Team nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /* It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering */
 //#import <OpenGL/CGLMacro.h>
