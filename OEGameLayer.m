dfd483abb14f010ef0f0d26284fe0140198b6a49
Josh Weinberg <daemoncollector@gmail.com>
2009-06-28 06:31:46 +0000

    Fix for the resizing of the layer
    git-svn-id: https://openemu.svn.sourceforge.net/svnroot/openemu/OpenEmu@398 a16923d9-94c8-4a39-ae78-11df2d60a2ba

--- OEGameLayer.m
+++ OEGameLayer.m
@@ -100,9 +100,10 @@
     return layerContext;
 }
 
-- (void)drawInCGLContext:(CGLContextObj)glContext pixelFormat:(CGLPixelFormatObj)pixelFormat forLayerTime:(CFTimeInterval)timeInterval displayTime:(const CVTimeStamp *)timeStamp
+- (CGSize)preferredFrameSize
 {
 	CALayer* superlayer = self.superlayer;
+	
 	
 	NSSize aspect;
 	if([gameCore respondsToSelector:@selector(outputSize)])
@@ -110,12 +111,36 @@
 	else
 		aspect = NSMakeSize([gameCore width], [gameCore height]);
 	
+	if(superlayer.bounds.size.width * (aspect.width * 1.0/aspect.height) > superlayer.bounds.size.height * (aspect.width * 1.0/aspect.height))
+		return CGSizeMake(superlayer.bounds.size.height * (aspect.width * 1.0/aspect.height), superlayer.bounds.size.height);
+	else
+		return CGSizeMake( superlayer.bounds.size.width, superlayer.bounds.size.width * (aspect.height* 1.0/aspect.width));
+	
+	
+	
+}
+
+- (void)drawInCGLContext:(CGLContextObj)glContext pixelFormat:(CGLPixelFormatObj)pixelFormat forLayerTime:(CFTimeInterval)timeInterval displayTime:(const CVTimeStamp *)timeStamp
+{
+	CALayer* superlayer = self.superlayer;
+
+	
+/*	[CATransaction begin];
+	[CATransaction setValue:[NSNumber numberWithBool:YES] forKey:kCATransactionDisableActions];
+	
+	NSSize aspect;
+	if([gameCore respondsToSelector:@selector(outputSize)])
+		aspect = [gameCore outputSize];
+	else
+		aspect = NSMakeSize([gameCore width], [gameCore height]);
 	
 	if(superlayer.bounds.size.width * (aspect.width * 1.0/aspect.height) > superlayer.bounds.size.height * (aspect.width * 1.0/aspect.height))
 		self.bounds = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, superlayer.bounds.size.height * (aspect.width * 1.0/aspect.height), superlayer.bounds.size.height);
 	else
 		self.bounds = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, superlayer.bounds.size.width, superlayer.bounds.size.width * (aspect.height* 1.0/aspect.width));
 	
+	[CATransaction commit];
+	*/
 	CGLSetCurrentContext(glContext);// (glContext);
 	CGLLockContext(glContext);
 	
