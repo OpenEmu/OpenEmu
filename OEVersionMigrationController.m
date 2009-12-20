0d6b19115c70d7726752ec3c99cc52892ae0b66c
Steve Streza <stevestreza@gmail.com>
2009-12-20 21:17:08 +0000

    Added detection code to version migrator to determine if the system is on a new installation (no migration needed), or an upgrade from pre-1.0.0b5 versions of OpenEmu (migration needed)
    git-svn-id: https://openemu.svn.sourceforge.net/svnroot/openemu/OpenEmu@650 a16923d9-94c8-4a39-ae78-11df2d60a2ba

--- OEVersionMigrationController.m
+++ OEVersionMigrationController.m
@@ -1,10 +1,29 @@
-//
-//  OEVersionMigrationController.m
-//  OpenEmu
-//
-//  Created by Steve Streza on 12/20/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/*
+ Copyright (c) 2009, OpenEmu Team
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the OpenEmu Team nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "OEVersionMigrationController.h"
 #import "SUStandardVersionComparator.h"
@@ -85,17 +104,27 @@ static OEVersionMigrationController *sDefaultMigrationController = nil;
 	self = [super init];
 	if(self){
 		self.versionComparator = [SUStandardVersionComparator defaultComparator];
+		
+		// we'll cheat here and rely on Sparkle's key
+		isFirstRun = ! [[NSUserDefaults standardUserDefaults] boolForKey:@"SUHasLaunchedBefore"];
 	}
 	return self;
 }
 
 -(void)runMigrationIfNeeded{
 	BOOL migrationNeeded = NO;
-	
 #define MigrationNeededIf(__cond) if(__cond){ migrationNeeded = YES; goto migrate; }
 	
 	NSString *currentVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString *)kCFBundleVersionKey];
 	NSString *mostRecentVersion = [[NSUserDefaults standardUserDefaults] objectForKey:@"OEMigrationLastVersion"];
+	
+	// We have to work around the fact that older versions of OpenEmu didn't stick a version key in the plist.
+	// If there is no current version, and the Sparkle key doesn't exist, then this is a new installation, not an upgrade.
+	// Thus, we log our current version and prevent the migration. Subsequent migrations will then have the new version.
+	if(!currentVersion && isFirstRun){
+		[[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:@"OEMigrationLastVersion"];
+		return;
+	}
 	
 	MigrationNeededIf(!mostRecentVersion);
 	
@@ -198,6 +227,8 @@ migrate:
 	
 	[migratorsForVersion addObject:migratorContainer];
 	[migratorContainer release];
+	
+	[self runMigrationIfNeeded];
 }
 
 -(void)dealloc{
