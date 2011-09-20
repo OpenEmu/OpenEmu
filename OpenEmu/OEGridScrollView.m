9921e2327b118553e52d4c253296e83cad190556
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2011-09-20 20:14:12 +0200

    GridView scrolling performance bosst

--- OpenEmu/OEGridScrollView.m
+++ OpenEmu/OEGridScrollView.m
@@ -7,101 +7,141 @@
 //
 
 #import "OEGridScrollView.h"
-#import "NSColor+IKSAdditions.h"
-@interface OEGridBackgroundView : NSView @end
+
+@interface OEGridBackgroundLayer : CALayer @end
+@interface OEGridScrollView (Private)
+- (void)_setup;
+@end
 
 @implementation OEGridScrollView
-
-#warning Constant redrawing is bad!
-- (void)drawRect:(NSRect)dirtyRect{
-	NSImage* lightning = [NSImage imageNamed:@"background_lighting"];
-	[lightning drawInRect:[self bounds] fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
-	
-	NSImage* noise = [NSImage imageNamed:@"noise"];
-	
-	float x, y;
-	for(y=0; y < self.bounds.size.height; y+= noise.size.height){
-		for(x=0; x < self.bounds.size.width; x+= noise.size.width){
-			[noise drawAtPoint:NSMakePoint(x, y) fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
-		}	
-		
-	}
-}
-
-- (BOOL)isFlipped{
-	return NO;
-}
-/*- (void)_setup{
-
-	/*
-	self.wantsLayer = YES;
-	CALayer* layer = self.layer;
-	
-	NSImage* lightningImage = [NSImage imageNamed:@"background_lighting"];
-	CALayer* lightningLayer = [CALayer layer];
-	lightningLayer.contents = lightningImage;
-	
-	CALayer* backgroundLayer = [CALayer layer];
-	backgroundLayer.backgroundColor = [[NSColor grayColor] CGColor];
-	
-	[lightningLayer display];
-	[backgroundLayer display];
-	
-	[layer addSublayer:backgroundLayer];
-	[layer addSublayer:lightningLayer];*
-
-}
-
 - (id)init {
     self = [super init];
     if (self) {
-		[self _setup];
-    }
-    return self;
-}
-- (id)initWithCoder:(NSCoder *)coder {
-    self = [super initWithCoder:coder];
-    if (self) {
-		[self _setup];
+        [self _setup];
     }
     return self;
 }
 - (id)initWithFrame:(NSRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-		[self _setup];
+        [self _setup];
     }
     return self;
+}
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self _setup];
+    }
+    return self;
+}
+
+- (BOOL)isFlipped{
+	return NO;
+}
+
+- (void)_setup{
+	CALayer* layer = [OEGridBackgroundLayer layer];
+	self.layer = layer;
+	self.wantsLayer = YES;
+	layer.needsDisplayOnBoundsChange = YES;
 }
 
 - (void)dealloc {
     [super dealloc];
 }
-*/
-
 @end
 
-@implementation OEGridBackgroundView
-- (BOOL)isFlipped{
-	return NO;
+@implementation OEGridBackgroundLayer
+static CGImageRef noiseImage = NULL;
+static CGPDFDocumentRef lightingImage = NULL;
+
+- (id) init{
+	if((self = [super init])){
+		// Load noise image if necessary
+		if(noiseImage==NULL){
+			NSString *path = [[NSBundle mainBundle] pathForResource:@"noise" ofType:@"png"];
+			
+			CGImageSourceRef imageSource = CGImageSourceCreateWithURL((CFURLRef)[NSURL fileURLWithPath:path], NULL);
+			if(imageSource){
+				noiseImage = CGImageSourceCreateImageAtIndex(imageSource, 0, NULL);
+				CFRelease(imageSource);
+			}
+		}
+		
+		// Load lighting image if necessary
+		if(lightingImage==NULL){	
+			NSString *path = [[NSBundle mainBundle] pathForResource:@"background_lighting" ofType:@"pdf"];
+			lightingImage = CGPDFDocumentCreateWithURL((CFURLRef)[NSURL fileURLWithPath:path]);
+		}
+	}
+	return self;
 }
 
-- (void)drawRect:(NSRect)dirtyRect{
-	NSImage* lightning = [NSImage imageNamed:@"background_lighting"];
-	[lightning drawInRect:[self bounds] fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
+- (id<CAAction>)actionForKey:(NSString *)event{
+	return nil;
+}
+
+- (void)drawInContext:(CGContextRef)context{
+	// retreive background image
+	NSRect lightningRect = [self bounds];
 	
-	NSImage* noise = [NSImage imageNamed:@"noise"];
+	CGContextSaveGState(context);
 	
-	float x, y;
-	for(y=0; y < self.bounds.size.height; y+= noise.size.height){
-		for(x=0; x < self.bounds.size.width; x+= noise.size.width){
-			[noise drawAtPoint:NSMakePoint(x, y) fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
-		}	
-		
+	CGPDFPageRef page = CGPDFDocumentGetPage(lightingImage, 1);
+	CGRect pdfRect = CGPDFPageGetBoxRect(page, kCGPDFCropBox);
+	
+	// Stretched lightning
+	//	lightningRect.size.height = bounds.size.height;
+	// CGContextTranslateCTM(context, 0, -NSMinY(visibleRect));
+	
+	// Static lightning
+	// nothing to do for this
+	
+	float widthScale = lightningRect.size.width / pdfRect.size.width;
+	float heightScale = lightningRect.size.height / pdfRect.size.height;
+	
+	// Flip
+	//	CGContextScaleCTM(context, 1, -1);
+	//	CGContextTranslateCTM(context, 0, -[self bounds].size.height);
+	
+	
+	CGContextScaleCTM (context, widthScale, heightScale);
+	CGContextDrawPDFPage(context, page);
+	
+	CGContextRestoreGState(context);
+	
+	// retrieve noise image size
+	float width = (float) CGImageGetWidth(noiseImage);
+	float height = (float) CGImageGetHeight(noiseImage);
+	
+	//compute coordinates to fill visible rect
+	float left, top, right, bottom;
+	
+	/* Scrolling Noise:*
+	 top = bounds.size.height - NSMaxY(visibleRect);
+	 top = fmod(top, height);
+	 top = height - top;
+	 
+	 right = NSMaxX(visibleRect);
+	 bottom = -height;*/
+	
+	
+	/* Static Noise:*/
+	// top = bounds.size.height - NSMaxY(visibleRect);
+	top = 0;
+	top = height - top;
+	
+	right = NSMaxX(lightningRect);
+	bottom = -height;
+	
+	// tile the image and take in account the offset to 'emulate' a scrolling background
+	for (top = lightningRect.size.height-top; top>bottom; top -= height){
+		for(left=0; left<right; left+=width){
+			CGContextDrawImage(context, CGRectMake(left, top, width, height), noiseImage);
+		}
 	}
 }
 
-- (BOOL)isOpaque{
-	return YES;
-}
 @end
