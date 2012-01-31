4dec49bb8a8323287de8dda02ed86e8cd99decf9
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2012-01-31 22:23:06 +0100

    improved replacing of private ivars

--- OpenEmu/OESidebarOutlineView.m
+++ OpenEmu/OESidebarOutlineView.m
@@ -48,10 +48,23 @@
     return self;
 }
 - (void)setupOutlineCell{
+    // Analyzer warns about leaking object here, that is not the case.
+    // We release the current instance variable and replace the pointer
+    // NSOutlineView should release our cell twice in its dealloc
     OESidebarOutlineButtonCell* sidebarOutlineCell = [[OESidebarOutlineButtonCell alloc] init];
+    [sidebarOutlineCell retain];
+    
+    void* currentCell;
+    object_getInstanceVariable(self, "_outlineCell", &currentCell);
+    [(id)currentCell release];
+    object_getInstanceVariable(self, "_trackingOutlineCell", &currentCell);
+    [(id)currentCell release];
+    
     
     object_setInstanceVariable(self, "_outlineCell", sidebarOutlineCell);
     object_setInstanceVariable(self, "_trackingOutlineCell", sidebarOutlineCell);
+    
+    // Read note above!
 }
 
 - (void)dealloc
