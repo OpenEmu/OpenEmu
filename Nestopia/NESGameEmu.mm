139a0798f892c3ab44097ed621bf2dbb71a1d95b
Alexander Strange <astrange@ithinksw.com>
2012-02-11 02:42:33 -0500

    Convert Nestopia to ARC

--- Nestopia/NESGameEmu.mm
+++ Nestopia/NESGameEmu.mm
@@ -61,22 +61,22 @@ UInt32 bufInPos, bufOutPos, bufUsed;
 static bool NST_CALLBACK VideoLock(void* userData, Nes::Api::Video::Output& video)
 {
     DLog(@"Locking: %@", userData);
-    return [(NESGameEmu*)userData lockVideo:&video];
+    return [(__bridge NESGameEmu*)userData lockVideo:&video];
 }
 
 static void NST_CALLBACK VideoUnlock(void* userData, Nes::Api::Video::Output& video)
 {
-    [(NESGameEmu*)userData unlockVideo:&video];
+    [(__bridge NESGameEmu*)userData unlockVideo:&video];
 }
 
 static bool NST_CALLBACK SoundLock(void* userData,Nes::Api::Sound::Output& sound)
 {
-    return [(NESGameEmu*)userData lockSound];
+    return [(__bridge NESGameEmu*)userData lockSound];
 }
 
 static void NST_CALLBACK SoundUnlock(void* userData,Nes::Api::Sound::Output& sound)
 {
-    [(NESGameEmu *)userData unlockSound];
+    [(__bridge NESGameEmu *)userData unlockSound];
 }
 
 - (id)init;
@@ -97,7 +97,7 @@ static void NST_CALLBACK SoundUnlock(void* userData,Nes::Api::Sound::Output& sou
 // for various file operations, usually called during image file load, power on/off and reset
 void NST_CALLBACK doFileIO(void *userData, Nes::Api::User::File& file)
 {
-    NESGameEmu *self = (NESGameEmu *)userData;
+    NESGameEmu *self = (__bridge NESGameEmu *)userData;
     
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSString *path = self->romPath;
@@ -367,10 +367,11 @@ void NST_CALLBACK doEvent(void* userData, Nes::Api::Machine::Event event,Nes::Re
     
     [self setRomPath:path];
     
-    Nes::Api::User::fileIoCallback.Set(doFileIO, self);
-    Nes::Api::User::logCallback.Set(doLog, self);
-    Nes::Api::Machine::eventCallback.Set(doEvent, self);
-    Nes::Api::User::questionCallback.Set(doQuestion, self);
+    void *userData = (__bridge void*)self;
+    Nes::Api::User::fileIoCallback.Set(doFileIO, userData);
+    Nes::Api::User::logCallback.Set(doLog, userData);
+    Nes::Api::Machine::eventCallback.Set(doEvent, userData);
+    Nes::Api::User::questionCallback.Set(doQuestion, userData);
     
     std::ifstream romFile([path cStringUsingEncoding:NSUTF8StringEncoding], std::ios::in | std::ios::binary);
     result = machine.Load(romFile, Nes::Api::Machine::FAVORED_NES_PAL, Nes::Api::Machine::ASK_PROFILE);
@@ -660,10 +661,6 @@ static int Heights[2] =
     delete nesSound;
     delete nesVideo;
     delete controls;
-    [romPath release];
-    [videoLock release];
-    [soundLock release];
-    [super dealloc];
 }
 
 - (void)didPushNESButton:(OENESButton)button forPlayer:(NSUInteger)player;
