45166c642fa2abfb6fb84c7ecfafb38a6be76133
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2012-05-12 15:11:29 +0200

    refactored new archive code

--- OpenEmu/ArchiveVGThrottling.m
+++ OpenEmu/ArchiveVGThrottling.m
@@ -11,7 +11,11 @@
 #import "ArchiveVGErrorCodes.h"
 @interface ArchiveVG ()
 - (id)performStandardCallWithOperation:(ArchiveVGOperation)operation format:(AVGOutputFormat)format andOptions:(NSArray*)options error:(NSError**)outError;
-- (void)performAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError* error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options;
+- (void)performAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError *error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options;
+- (void)performHighPriorityAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError *error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options;
+
+- (void)performAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError *error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options withQueue:(NSMutableArray*)queue;
+- (void)makeCallFromQueue;
 @end
 
 @implementation ArchiveVGThrottling
@@ -20,8 +24,8 @@
 	self = [super init];
 	if(!self) return nil;
 	
-	self.highPriorityQueue = [NSMutableArray array];
-	self.queue				 = [NSMutableArray array];
+	self.highPriorityQueue		= [NSMutableArray array];
+	self.normalPriorityQueue		= [NSMutableArray array];
 	
 	self.maximumCalls	 = 120;
 	self.availableCalls	 = 120;
@@ -29,6 +33,8 @@
 	
 	self.regenerartionThread = [[NSThread alloc] initWithTarget:self selector:@selector(callRegenerationThread) object:nil];
 	[self.regenerartionThread start];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(cleanUp) name:NSApplicationWillTerminateNotification object:NSApp];
 	
 	NSError			*error = nil;
 	NSDictionary	*result = [[ArchiveVG unthrottled] configWithError:&error];
@@ -60,18 +66,14 @@
 	return self;
 }
 
-- (void)callRegenerationThread
+- (void)cleanUp
 {
-	while (1) {
-		self.availableCalls += 1;
-		if(self.availableCalls > self.maximumCalls)
-			self.availableCalls = self.maximumCalls;
-		[self processQueue];
-		[NSThread sleepForTimeInterval:self.regernerationInterval];
-	}
+	[[self regenerartionThread] cancel];
+	self.regenerartionThread = nil;
 }
 
-@synthesize queue, highPriorityQueue;
+#pragma mark - Throttling
+@synthesize normalPriorityQueue, highPriorityQueue;
 
 - (BOOL)isOperationThrottled:(ArchiveVGOperation)operation
 {
@@ -93,40 +95,68 @@
 		default: return YES;
 	}
 }
-#pragma mark - 
+#pragma mark - Call Regeneration
+- (void)callRegenerationThread
+{
+	while (![[NSThread currentThread] isCancelled]) {
+		self.availableCalls += 1;
+		if([self availableCalls] > [self maximumCalls])
+			[self setAvailableCalls:[self maximumCalls]];
+		[self makeCallFromQueue];
+		[NSThread sleepForTimeInterval:[self regernerationInterval]];
+	}
+}
+- (void)makeCallFromQueue
+{
+	NSMutableArray *selectedQueue = nil;
+	if([[self highPriorityQueue] count])
+		selectedQueue = [self highPriorityQueue];
+	else if([[self normalPriorityQueue] count])
+		selectedQueue = [self normalPriorityQueue];
+	if(!selectedQueue) return;
+	
+	void(^block)();
+	block = [selectedQueue objectAtIndex:0];
+	[selectedQueue removeObjectAtIndex:0];
+	self.availableCalls --;
+	block();
+}
+
+@synthesize regenerartionThread;
+#pragma mark - ArchiveVG Overrides
 - (id)performStandardCallWithOperation:(ArchiveVGOperation)operation format:(AVGOutputFormat)format andOptions:(NSArray*)options error:(NSError**)outError
 {
 	if( ![self isOperationThrottled:operation] ) return [super performStandardCallWithOperation:operation format:format andOptions:options error:outError];
 	else
 	{
 		NSLog(@"NOTICE: using throttled api calls synchronously will can easily cause your thread to block for 5+ seconds.");
-		while (!self.availableCalls) {}
+		while (![self availableCalls]) {}
 		self.availableCalls --;
 		return [[ArchiveVG unthrottled] performStandardCallWithOperation:operation format:format andOptions:options error:outError];
 	}
 }
 
-- (void)performAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError* error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options
+- (void)performAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError *error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options
 {
-	[self performAsynchronousCallWithOperation:operation callback:block format:format andOptions:options withQueue:self.queue];
+	[self performAsynchronousCallWithOperation:operation callback:block format:format andOptions:options withQueue:[self normalPriorityQueue]];
 }
 
-- (void)performHighPriorityAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError* error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options
+- (void)performHighPriorityAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError *error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options
 {
-	[self performAsynchronousCallWithOperation:operation callback:block format:format andOptions:options withQueue:self.highPriorityQueue];
+	[self performAsynchronousCallWithOperation:operation callback:block format:format andOptions:options withQueue:[self highPriorityQueue]];
 }
 
-- (void)performAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError* error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options withQueue:(NSMutableArray*)selectedQueue
+- (void)performAsynchronousCallWithOperation:(ArchiveVGOperation)operation callback:(void(^)(id result, NSError *error))block format:(AVGOutputFormat)format andOptions:(NSArray*)options withQueue:(NSMutableArray*)selectedQueue
 {
 	if(![self isOperationThrottled:operation])
 		[self performAsynchronousCallWithOperation:operation callback:block format:format andOptions:options];
 	else {
-		void(^throttlingBlock)(id result, NSError* error) = ^(id result, NSError *error) {
+		void(^throttlingBlock)(id result, NSError *error) = ^(id result, NSError *error) {
 			// if we get an error because of throttling
 			if(error && [[error domain] isEqual:OEArchiveVGErrorDomain] && [error code]==AVGThrottlingErrorCode)
 			{
 				// notify throttling instance that all calls are used up
-				self.availableCalls = 0;
+				[self setAvailableCalls:0];
 				
 				// reschedule operation so it can be executed when a new call becomes available
 				[[ArchiveVG throttled] performHighPriorityAsynchronousCallWithOperation:operation callback:block format:format andOptions:options];
@@ -152,25 +182,6 @@
 	}
 }
 
-
-
-- (void)processQueue
-{
-	NSMutableArray *selectedQueue = nil;
-	if([[self highPriorityQueue] count])
-		selectedQueue = [self highPriorityQueue];
-	else if([[self queue] count])
-		selectedQueue = [self queue];
-	if(!selectedQueue) return;
-	
-	void(^block)();
-	block = [selectedQueue objectAtIndex:0];
-	[selectedQueue removeObjectAtIndex:0];
-	self.availableCalls --;
-	block();
-}
-
-@synthesize regenerartionThread;
 #pragma mark - Config
 @synthesize maximumCalls;
 @synthesize availableCalls;
