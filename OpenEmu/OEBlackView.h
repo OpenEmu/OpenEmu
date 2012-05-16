565841cbf20be6cca6abab9fb51a60f12ff4a78c
vade <vade@vade.info>
2012-05-16 13:47:41 -0400

    Yeah. Headers help.

---
+++ OpenEmu/OEBlackView.h
@@ -0,0 +1,13 @@
+//
+//  OEBlackView.h
+//  OpenEmu
+//
+//  Created by Anton Marini on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OEBlackView : NSView
+
+@end
