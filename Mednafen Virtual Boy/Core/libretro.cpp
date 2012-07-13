62634bb680ed252f70ce17642b2ca16ee419ddb6
clobber <brymaster@gmail.com>
2012-07-13 15:26:36 -0500

    Added red/black 2D look for VB

--- Mednafen Virtual Boy/Core/libretro.cpp
+++ Mednafen Virtual Boy/Core/libretro.cpp
@@ -36,10 +36,21 @@ void retro_init()
     if (environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &dir) && dir)
     {
         std::string save_path = saves;
+        //dox http://mednafen.sourceforge.net/documentation/09x/vb.html
         
         MDFNSetting vb_parallax = { "vb.disable_parallax", MDFNSF_EMU_STATE, "Disable parallax for BG and OBJ rendering", NULL, MDFNST_BOOL, "1", NULL, NULL, NULL };
+        MDFNSetting vb_anaglyph_preset = { "vb.anaglyph.preset", MDFNSF_EMU_STATE, "Disable anaglyph preset", NULL, MDFNST_BOOL, "disabled", NULL, NULL, NULL };
+        MDFNSetting vb_anaglyph_lcolor = { "vb.anaglyph.lcolor", MDFNSF_EMU_STATE, "Anaglyph l color", NULL, MDFNST_BOOL, "0xFF0000", NULL, NULL, NULL };
+        MDFNSetting vb_anaglyph_rcolor = { "vb.anaglyph.rcolor", MDFNSF_EMU_STATE, "Anaglyph r color", NULL, MDFNST_BOOL, "0x000000", NULL, NULL, NULL };
+        //MDFNSetting vb_allow_draw_skip = { "vb.allow_draw_skip", MDFNSF_EMU_STATE, "Allow draw skipping", NULL, MDFNST_BOOL, "1", NULL, NULL, NULL };
+        //MDFNSetting vb_instant_display_hack = { "vb.instant_display_hack", MDFNSF_EMU_STATE, "ADisplay latency reduction hack", NULL, MDFNST_BOOL, "1", NULL, NULL, NULL };
         MDFNSetting filesys = { "filesys.path_sav", MDFNSF_NOFLAGS, "Memcards", NULL, MDFNST_STRING, save_path.c_str() };
         settings.push_back(vb_parallax);
+        settings.push_back(vb_anaglyph_preset);
+        settings.push_back(vb_anaglyph_lcolor);
+        settings.push_back(vb_anaglyph_rcolor);
+        //settings.push_back(vb_allow_draw_skip);
+        //settings.push_back(vb_instant_display_hack);
         settings.push_back(filesys);
         MDFNI_Initialize(dir, settings);
     }
