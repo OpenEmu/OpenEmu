4265ffde1f1a1196c821b93187334c32b1809dd6
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2012-10-16 11:02:56 +0200

    Using themed headline labels in all preferences tabs.

---
+++ OpenEmu/OELabel.h
@@ -0,0 +1,12 @@
+//
+//  OETextField.h
+//  OETheme
+//
+//  Created by Christoph Leimbrock on 13.10.12.
+//  Copyright (c) 2012 OpenEmu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "OEControl.h"
+@interface OELabel : NSTextField <OEControl>
+@end
---
+++ OpenEmu/OELabelCell.m
@@ -0,0 +1,156 @@
+//
+//  OETextFieldCell.m
+//  OETheme
+//
+//  Created by Christoph Leimbrock on 13.10.12.
+//  Copyright (c) 2012 OpenEmu. All rights reserved.
+//
+
+#import "OELabelCell.h"
+@interface OELabelCell ()
+{
+    NSMutableParagraphStyle *_style;
+}
+@end
+@implementation OELabelCell
+
+- (void)setStringValue:(NSString *)aString
+{
+    [super setAttributedStringValue:[[NSAttributedString alloc] initWithString:aString attributes:[[self themeTextAttributes] textAttributesForState:[self OE_currentState]]]];
+}
+- (OEThemeState)OE_currentState
+{
+    // This is a convenience method that retrieves the current state of the button
+    BOOL focused      = NO;
+    BOOL windowActive = NO;
+    
+    if(((_stateMask & OEThemeStateAnyFocus) != 0) || ((_stateMask & OEThemeStateAnyWindowActivity) != 0))
+    {
+        // Set the focused, windowActive, and hover properties only if the state mask is tracking the button's focus, mouse hover, and window activity properties
+        NSWindow *window = [[self controlView] window];
+        
+        focused      = [window firstResponder] == [self controlView];
+        windowActive = ((_stateMask & OEThemeStateAnyWindowActivity) != 0) && ([window isMainWindow] || ([window parentWindow] && [[window parentWindow] isMainWindow]));
+    }
+    
+    return [OEThemeObject themeStateWithWindowActive:windowActive buttonState:[self state] selected:[self isHighlighted] enabled:[self isEnabled] focused:focused houseHover:[self isHovering]] & _stateMask;
+}
+
+- (NSDictionary *)OE_attributesForState:(OEThemeState)state
+{
+    // This is a convenience method for creating the attributes for an NSAttributedString
+    if(!_themeTextAttributes) return nil;
+    if(!_style) _style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+    
+    NSDictionary *attributes = [_themeTextAttributes textAttributesForState:state];
+    if(![attributes objectForKey:NSParagraphStyleAttributeName])
+    {
+        [_style setLineBreakMode:([self wraps] ? NSLineBreakByWordWrapping : NSLineBreakByClipping)];
+        [_style setAlignment:[self alignment]];
+        
+        NSMutableDictionary *newAttributes = [attributes mutableCopy];
+        [newAttributes setValue:_style forKey:NSParagraphStyleAttributeName];
+        attributes = [newAttributes copy];
+    }
+    
+    return attributes;
+}
+
+/*
+
+- (NSRect)titleRectForBounds:(NSRect)theRect
+{
+    NSRect result = [super titleRectForBounds:theRect];
+    return result;
+}
+
+
+- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
+    if(_themed)
+    {
+        NSRect textRect  = NSIntegralRect([self titleRectForBounds:cellFrame]);        
+        [[self attributedStringValue] drawInRect:textRect];
+    }
+    else
+        [super drawInteriorWithFrame:cellFrame inView:controlView];
+}
+
+
+- (NSAttributedString *)attributedStringValue
+{
+    NSDictionary *attributes = (_themed ? [self OE_attributesForState:[self OE_currentState]] : nil);
+    return attributes?[[NSAttributedString alloc] initWithString:[self title] attributes:attributes]:[super attributedStringValue];
+}
+*/
+- (void)OE_recomputeStateMask
+{
+    _themed    = (_backgroundThemeImage != nil || _themeImage != nil || _themeTextAttributes != nil);
+    _stateMask = [_backgroundThemeImage stateMask] | [_themeImage stateMask] | [_themeTextAttributes stateMask];
+}
+
+- (void)setThemeKey:(NSString *)key
+{
+    NSString *backgroundKey = key;
+    if(![key hasSuffix:@"_background"])
+    {
+        [self setThemeImageKey:key];
+        backgroundKey = [key stringByAppendingString:@"_background"];
+    }
+    [self setBackgroundThemeImageKey:backgroundKey];
+    [self setThemeTextAttributesKey:key];
+    
+    
+}
+
+- (void)setBackgroundThemeImageKey:(NSString *)key
+{
+    [self setBackgroundThemeImage:[[OETheme sharedTheme] themeImageForKey:key]];
+}
+
+- (void)setThemeImageKey:(NSString *)key
+{
+    [self setThemeImage:[[OETheme sharedTheme] themeImageForKey:key]];
+}
+
+- (void)setThemeTextAttributesKey:(NSString *)key
+{
+    [self setThemeTextAttributes:[[OETheme sharedTheme] themeTextAttributesForKey:key]];
+}
+
+- (void)setBackgroundThemeImage:(OEThemeImage *)backgroundThemeImage
+{
+    if(_backgroundThemeImage != backgroundThemeImage)
+    {
+        // TODO: Only invalidate area of the control view
+        _backgroundThemeImage = backgroundThemeImage;
+        [[self controlView] setNeedsDisplay:YES];
+        [self OE_recomputeStateMask];
+    }
+}
+
+- (void)setThemeImage:(OEThemeImage *)themeImage
+{
+    if(_themeImage != themeImage)
+    {
+        // TODO: Only invalidate area of the control view
+        _themeImage = themeImage;
+        [[self controlView] setNeedsDisplay:YES];
+        [self OE_recomputeStateMask];
+    }
+}
+
+- (void)setThemeTextAttributes:(OEThemeTextAttributes *)themeTextAttributes
+{
+    if(_themeTextAttributes != themeTextAttributes)
+    {
+        // TODO: Only invalidate area of the control view
+        _themeTextAttributes = themeTextAttributes;
+        [self setStringValue:[self stringValue]];
+        [self OE_recomputeStateMask];
+    }
+}
+
+@synthesize themed=_themed, themeImage=_themeImage, themeTextAttributes=_themeTextAttributes;
+@synthesize stateMask=_stateMask, backgroundThemeImage=_backgroundThemeImage, hovering=_hovering;
+@end
