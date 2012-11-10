4fe2f7e40c5f6d8851325d77f9a1e74f9082a0b3
Remy Demarest <remy.demarest@gmail.com>
2012-11-10 15:57:52 -0800

    Code cleanup.

--- OpenEmu/OEWiimoteDeviceHandler.h
+++ OpenEmu/OEWiimoteDeviceHandler.h
@@ -9,15 +9,19 @@
 #import <OpenEmuSystem/OpenEmuSystem.h>
 #import "Wiimote.h"
 #import "WiimoteTypes.h"
-@class OEHIDEvent, Wiimote;
-@interface OEWiimoteDeviceHandler : OEHIDDeviceHandler
-+ (id)deviceHandlerWithWiimote:(Wiimote*)aWiimote;
-- (id)initWithWiimote:(Wiimote*)aWiimote;
 
-- (OEHIDEvent*)eventWithWiiButton:(WiiButtonType)button;
+@class OEHIDEvent, Wiimote;
+
+@interface OEWiimoteDeviceHandler : OEHIDDeviceHandler
+
++ (id)deviceHandlerWithWiimote:(Wiimote *)aWiimote;
+- (id)initWithWiimote:(Wiimote *)aWiimote;
+
+- (OEHIDEvent *)eventWithWiiButton:(WiiButtonType)button;
 - (void)dispatchEventWithWiiButton:(WiiButtonType)button state:(BOOL)state;
 
-- (OEHIDEvent*)eventWithWiiJoystick:(WiiJoyStickType)joystick;
+- (OEHIDEvent *)eventWithWiiJoystick:(WiiJoyStickType)joystick;
 - (void)dispatchEventWithWiiJoystick:(WiiJoyStickType)joystick tiltX:(CGFloat)tiltX tiltY:(CGFloat)tiltY;
 - (void)dispatchEventWithWiiTrigger:(WiiTriggerType)trigger value:(CGFloat)triggerVal;
+
 @end
