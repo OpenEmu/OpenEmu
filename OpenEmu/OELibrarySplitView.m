a8b4f8599a8c6f2c3a69b4cc7a29fb4124f342de
Bavarious <bavarious@nserror.com>
2012-11-20 07:47:54 -0200

    Changes OELibrarySplitView’s delegate from weak to unsafe_unretained because the delegate inherits from NSViewController, which is weak-unavailable in 10.7. Fixes #345.

--- OpenEmu/OELibrarySplitView.m
+++ OpenEmu/OELibrarySplitView.m
@@ -44,8 +44,10 @@ static NSString * const _OESidebarWidthKey   = @"lastSidebarWidth";
 #pragma mark - OELibrarySplitViewDelegateProxy
 
 @interface OELibrarySplitViewDelegateProxy : NSObject <OELibrarySplitViewDelegate>
-@property(nonatomic, weak) id<NSSplitViewDelegate>        superDelegate;
-@property(nonatomic, weak) id<OELibrarySplitViewDelegate> localDelegate;
+@property(nonatomic, weak) id<NSSplitViewDelegate>                     superDelegate;
+@property(nonatomic, unsafe_unretained) id<OELibrarySplitViewDelegate> localDelegate;
+// can't use weak for localDelegate yet because OELibraryController, the delegate, inherits from NSViewController,
+// which is weak-unavailable in 10.7. See issue #345.
 @end
 
 #pragma mark - OELibrarySplitView
