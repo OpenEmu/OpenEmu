3ae230f5cdf63f60ceeeac69ebb86c4ebcd156fd
Bavarious <bavarious@nserror.com>
2012-11-26 04:41:58 -0200

    Improves library list/cover flow sorting. In particular:
    * we now use -localizedCaseInsensitiveCompare: to sort game titles;
    * lastPlayed is sorted by its date (year, month, day) components only;
    * we do not send -rearrangeObjects when sort descriptors change since the array controller automatically rearranges its objects;
    * whenever a game *or* ROM is changed, we reload visible rows.
    
    List view selection is now bound to the selection indexes of the games NSArrayController, and there’s a hack to keep this selection in sync with IKImageFlowView when sort descriptors change.
    
    Partial fix for issue #351.

---
+++ OpenEmu/NSDate+OESortAdditions.m
@@ -0,0 +1,48 @@
+/*
+ Copyright (c) 2012, OpenEmu Team
+
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the OpenEmu Team nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "NSDate+OESortAdditions.h"
+
+@implementation NSDate (OESortAdditions)
+
+- (NSComparisonResult)OE_compareDMYTranslatingNilToDistantPast:(NSDate *)anotherDate
+{
+    if(!anotherDate) return [self compare:[NSDate distantPast]];
+
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+
+    NSDateComponents *selfDMY = [gregorian components:(NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit) fromDate:self];
+    NSDateComponents *anotherDMY = [gregorian components:(NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit) fromDate:anotherDate];
+
+    if([selfDMY year]  != [anotherDMY year])  return ([selfDMY year]  > [anotherDMY year]  ? NSOrderedDescending : NSOrderedAscending);
+    if([selfDMY month] != [anotherDMY month]) return ([selfDMY month] > [anotherDMY month] ? NSOrderedDescending : NSOrderedAscending);
+    if([selfDMY day]   != [anotherDMY day])   return ([selfDMY day]   > [anotherDMY day]   ? NSOrderedDescending : NSOrderedAscending);
+
+    return NSOrderedSame;
+}
+
+@end
