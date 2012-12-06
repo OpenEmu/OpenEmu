c2e50a748aa04b9d1ce9ffde340df6eb5cae5a4a
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2012-12-06 16:17:02 +0100

    Disable toolbar if game view controller is visible in main window.

--- OpenEmu/OEGameViewController.m
+++ OpenEmu/OEGameViewController.m
@@ -56,6 +56,8 @@
 
 #import "OEPreferencesController.h"
 
+#import "OEMainWindowController.h"
+
 NSString *const OEGameVolumeKey = @"volume";
 NSString *const OEGameVideoFilterKey = @"videoFilter";
 NSString *const OEGameCoresInBackgroundKey = @"gameCoreInBackgroundThread";
@@ -240,6 +242,9 @@ typedef enum : NSUInteger
     
     if([window parentWindow] != nil) window = [window parentWindow];
 
+    if([[window windowController] isKindOfClass:[OEMainWindowController class]])
+        [[[window windowController] toolbarController] disableAllItems];    
+    
     [window addChildWindow:controlsWindow ordered:NSWindowAbove];
     
     [self OE_repositionControlsWindow];
