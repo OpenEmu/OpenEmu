548a213e4d55e7bf717772a2e10c17c3470a7dff
Remy Demarest <remy.demarest@gmail.com>
2013-03-23 22:14:27 -0700

    Cleanup licence layout.

--- OpenEmu/OEPrefControlsController.m
+++ OpenEmu/OEPrefControlsController.m
@@ -3,14 +3,14 @@
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- * Neither the name of the OpenEmu Team nor the
- names of its contributors may be used to endorse or promote products
- derived from this software without specific prior written permission.
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the OpenEmu Team nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
 
  THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -18,11 +18,12 @@
  DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 #import <Quartz/Quartz.h>
 
 #import "NSViewController+OEAdditions.h"
@@ -56,11 +57,11 @@ NSString *const OELastControlsPluginIdentifierKey = @"lastControlsPlugin";
 NSString *const OELastControlsPlayerKey           = @"lastControlsPlayer";
 NSString *const OELastControlsDeviceTypeKey       = @"lastControlsDevice";
 
-    @interface OEPrefControlsController ()
-        {
-        OESystemPlugin *selectedPlugin;
-        OEHIDEvent     *readingEvent;
-        NSMutableSet   *ignoredEvents;
+@interface OEPrefControlsController ()
+{
+    OESystemPlugin *selectedPlugin;
+    OEHIDEvent     *readingEvent;
+    NSMutableSet   *ignoredEvents;
 }
 
 - (void)OE_setCurrentBindingsForEvent:(OEHIDEvent *)anEvent;
@@ -68,12 +69,12 @@ NSString *const OELastControlsDeviceTypeKey       = @"lastControlsDevice";
 - (void)OE_setupControllerImageViewWithTransition:(NSString *)transition;
 - (void)OE_preparePaneWithNotification:(NSNotification *)notification;
 
-    // Only one event can be managed at a time, all events should be ignored until the currently read event went back to its null state
-    // All ignored events are stored until they go back to the null state
+// Only one event can be managed at a time, all events should be ignored until the currently read event went back to its null state
+// All ignored events are stored until they go back to the null state
 - (BOOL)OE_shouldRegisterEvent:(OEHIDEvent *)anEvent;
 
-    @property(nonatomic, readwrite) OESystemBindings *currentSystemBindings;
-    @property(nonatomic, readwrite) OEPlayerBindings *currentPlayerBindings;
+@property(nonatomic, readwrite) OESystemBindings *currentSystemBindings;
+@property(nonatomic, readwrite) OEPlayerBindings *currentPlayerBindings;
 
 @end
 
@@ -674,12 +675,12 @@ NSString *const OELastControlsDeviceTypeKey       = @"lastControlsDevice";
         NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:[plugin systemName] action:@selector(changeSystem:) keyEquivalent:@""];
         [item setTarget:self];
         [item setRepresentedObject:[plugin systemIdentifier]];
-
+        
         [item setImage:[plugin systemIcon]];
-
+        
         [consolesMenu addItem:item];
     }
-
+    
     [[self consolesPopupButton] setMenu:consolesMenu];
 }
 
