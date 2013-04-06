859f9981a2c3a4f26efd213dfd8c568e7e073488
Remy Demarest <remy.demarest@gmail.com>
2013-04-05 22:48:01 -0700

    Corrected value scaling.

--- OpenEmu/OEHIDEvent.m
+++ OpenEmu/OEHIDEvent.m
@@ -240,8 +240,8 @@ static inline CGFloat _OEScaledValueForAxis(NSInteger minimum, NSInteger value, 
         maximum -= zero;
     }
 
-    if(value < zero)      return -value / (CGFloat)minimum;
-    else if(value > zero) return  value / (CGFloat)maximum;
+    if(value < 0)      return -value / (CGFloat)minimum;
+    else if(value > 0) return  value / (CGFloat)maximum;
 
     return 0.0;
 }
