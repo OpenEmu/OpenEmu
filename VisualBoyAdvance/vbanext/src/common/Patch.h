5b2a250c4caa1b221c0be678e4961f2f64575c3e
Remy Demarest <remy.demarest@gmail.com>
2013-04-07 19:55:27 -0700

    Move VisualBoyAdvance into its own repository and add it as a submodule.

--- VisualBoyAdvance/vbanext/src/common/Patch.h
+++
@@ -1,8 +0,0 @@
-#ifndef PATCH_H
-#define PATCH_H
-
-#include "Types.h"
-
-bool applyPatch(const char *patchname, u8 **rom, int *size);
-
-#endif // PATCH_H
--- VisualBoyAdvance/vbanext/src/gb/gbPrinter.h
+++
@@ -1,8 +0,0 @@
-#ifndef GBPRINTER_H
-#define GBPRINTER_H
-
-#include "../System.h"
-
-uint8_t gbPrinterSend(uint8_t b);
-
-#endif // GBPRINTER_H
