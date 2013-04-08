bc8f91b5ab50467120916709ea18f72b4dc7e658
Remy Demarest <remy.demarest@gmail.com>
2013-04-07 18:57:23 -0700

    Move FCEU into its own repository and add it as a submodule.

--- FCEU/fceumm/src-fceumm/crc32.h
+++
@@ -1,1 +0,0 @@
-uint32 CalcCRC32(uint32 crc, uint8 *buf, uint32 len);
--- FCEU/fceumm/src-fceumm/ppuview.h
+++
@@ -1,8 +0,0 @@
-extern int PPUViewScanline;
-extern int PPUViewer;
-extern int scanline;
-
-void PPUViewDoBlit();
-void DoPPUView();
-void UpdatePPUView(int refreshchr);
-
