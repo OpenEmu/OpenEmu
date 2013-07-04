df193301c1396cbe9573792849accea2cb58ef11
clobber <brymaster@gmail.com>
2013-07-04 17:46:50 -0500

    Added PSP system plugin

---
+++ OpenEmu/PSP/OEPSPSystemResponderClient.h
@@ -0,0 +1,58 @@
+/*
+ Copyright (c) 2013, OpenEmu Team
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the OpenEmu Team nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import <Cocoa/Cocoa.h>
+
+@protocol OESystemResponderClient;
+
+typedef enum _OEPSPButton
+{
+    OEPSPButtonUp,
+    OEPSPButtonDown,
+    OEPSPButtonLeft,
+    OEPSPButtonRight,
+    OEPSPAnalogUp,
+    OEPSPAnalogDown,
+    OEPSPAnalogLeft,
+    OEPSPAnalogRight,
+    OEPSPButtonTriangle,
+    OEPSPButtonCircle,
+    OEPSPButtonCross,
+    OEPSPButtonSquare,
+    OEPSPButtonL1,
+    OEPSPButtonR1,
+    OEPSPButtonStart,
+    OEPSPButtonSelect,
+    OEPSPButtonCount
+} OEPSPButton;
+
+@protocol OEPSPSystemResponderClient <OESystemResponderClient, NSObject>
+
+- (oneway void)didMovePSPJoystickDirection:(OEPSPButton)button withValue:(CGFloat)value forPlayer:(NSUInteger)player;
+- (oneway void)didPushPSPButton:(OEPSPButton)button forPlayer:(NSUInteger)player;
+- (oneway void)didReleasePSPButton:(OEPSPButton)button forPlayer:(NSUInteger)player;
+
+@end
