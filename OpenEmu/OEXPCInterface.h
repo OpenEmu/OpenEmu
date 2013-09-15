aeace1ccfe59d252e332d20f8cb99b1104b5015c
clobber <brymaster@gmail.com>
2013-09-15 04:23:52 -0700

    NSXPCInterface does not look for the protocols that a protocol conforms to when looking for a method. Make our own OEXPCInterface that looks for the protocols among multiple NSXPCInterfaces.

--- OpenEmu/OEXPCInterface.h
+++ OpenEmu/OEXPCInterface.h
@@ -1,6 +1,6 @@
 /*
  Copyright (c) 2013, OpenEmu Team
-
+ 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
      * Redistributions of source code must retain the above copyright
@@ -11,7 +11,7 @@
      * Neither the name of the OpenEmu Team nor the
        names of its contributors may be used to endorse or promote products
        derived from this software without specific prior written permission.
-
+ 
  THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,10 +25,7 @@
  */
 
 #import <Foundation/Foundation.h>
-#import "OEGameCoreHelper.h"
 
-@protocol OEXPCGameCoreHelper <OEGameCoreHelper, NSObject>
-
-- (void)loadROMAtPath:(NSString *)romPath usingCorePluginAtPath:(NSString *)pluginPath systemPluginPath:(NSString *)systemPluginPath completionHandler:(void(^)(NSXPCListenerEndpoint *gameCoreEndpoint, NSError *error))completionHandler;
-
+@interface OEXPCInterface : NSProxy
+@property(assign) Protocol *protocol;
 @end
