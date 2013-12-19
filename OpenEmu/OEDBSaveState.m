OpenEmu/OpenEmu

commit 4b42c2ad453edb417d39ec8da076af1c742e6315

Trying to fix core data.

OpenEmu/OEDBSaveState.m
@@ -72,9 +72,8 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
 
 + (NSArray*)allStatesInDatabase:(OELibraryDatabase*)database
 {
-    NSManagedObjectContext *context = [database managedObjectContext];
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"SaveState"];
-    return [context executeFetchRequest:request error:nil];
+    return [database executeFetchRequest:request error:nil];
 }
 
 
@@ -85,7 +84,6 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
 
 + (OEDBSaveState*)saveStateWithURL:(NSURL*)url inDatabase:(OELibraryDatabase*)database
 {
-    NSManagedObjectContext  *context    = [database managedObjectContext];
     NSFetchRequest          *request    = [NSFetchRequest fetchRequestWithEntityName:@"SaveState"];
     
     NSString *absoluteString = [url absoluteString];
@@ -97,16 +95,20 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
     NSPredicate *predicate  = [NSPredicate predicateWithFormat:@"location == %@", absoluteString];
     [request setPredicate:predicate];
     
-    return [[context executeFetchRequest:request error:nil] lastObject];
+    return [[database executeFetchRequest:request error:nil] lastObject];
 }
 
 
-+ (id)OE_newSaveStateInContext:(NSManagedObjectContext*)context{
-	NSEntityDescription *description = [NSEntityDescription entityForName:@"SaveState" inManagedObjectContext:context];
-	OEDBSaveState *result = [[OEDBSaveState alloc] initWithEntity:description insertIntoManagedObjectContext:context];
-	
-	[result setTimestamp:[NSDate date]];
-	
++ (id)OE_newSaveStateInContext:(NSManagedObjectContext*)context
+{
+    __block OEDBSaveState *result = nil;
+    [context performBlockAndWait:^{
+        NSEntityDescription *description = [NSEntityDescription entityForName:@"SaveState" inManagedObjectContext:context];
+        OEDBSaveState *result = [[OEDBSaveState alloc] initWithEntity:description insertIntoManagedObjectContext:context];
+
+        [result setTimestamp:[NSDate date]];
+    }];
+
 	return result;
 }
 
@@ -117,7 +119,7 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
 
 + (id)createSaveStateWithURL:(NSURL *)url inDatabase:(OELibraryDatabase*)database
 {
-    OEDBSaveState *newSaveState = [self OE_newSaveStateInContext:[database managedObjectContext]];
+    OEDBSaveState *newSaveState = [self OE_newSaveStateInContext:[database unsafeContext]];
     [newSaveState setLocation:[url absoluteString]];
     if(![newSaveState readInfoPlist])
     {
@@ -129,7 +131,7 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
     
     NSError *error = nil;
 //TODO: use validation here instead of save
-    if(![[newSaveState managedObjectContext] save:&error])
+    if(![[newSaveState libraryDatabase] save:&error])
     {
         [newSaveState setLocation:nil];
         [newSaveState remove];
@@ -148,7 +150,7 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
 
 + (id)createSaveStateNamed:(NSString*)name forRom:(OEDBRom *)rom core:(OECorePlugin *)core withFile:(NSURL *)stateFileURL inDatabase:(OELibraryDatabase *)database
 {
-    OEDBSaveState *newSaveState = [self OE_newSaveStateInContext:[database managedObjectContext]];
+    OEDBSaveState *newSaveState = [self OE_newSaveStateInContext:[database unsafeContext]];
     [newSaveState setName:name];
     [newSaveState setRom:rom];
     [newSaveState setCoreIdentifier:[core bundleIdentifier]];
@@ -306,9 +308,11 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
 - (void)remove
 {
     [[NSFileManager defaultManager] removeItemAtURL:[self URL] error:nil];
-    NSManagedObjectContext *moc = [self managedObjectContext];
-    [moc deleteObject:self];
-    [moc save:nil];
+    NSManagedObjectContext *context = [self managedObjectContext];
+    [context performBlock:^{
+        [context deleteObject:self];
+        [context save:nil];
+    }];
 }
 
 - (void)removeIfMissing

OpenEmu/OECollectionViewController.m
@@ -162,7 +162,7 @@ static NSArray *OE_defaultSortDescriptors;
     [gamesController setAutomaticallyPreparesContent:NO];
     [gamesController setUsesLazyFetching:NO];
     
-    NSManagedObjectContext *context = [[OELibraryDatabase defaultDatabase] managedObjectContext];
+    NSManagedObjectContext *context = [[OELibraryDatabase defaultDatabase] unsafeContext];
     //[gamesController bind:@"managedObjectContext" toObject:context withKeyPath:@"" options:nil];
 
     OE_defaultSortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"cleanDisplayName" ascending:YES selector:@selector(caseInsensitiveCompare:)]];
@@ -228,7 +228,6 @@ static NSArray *OE_defaultSortDescriptors;
     // Watch the main thread's managed object context for changes
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(OE_managedObjectContextDidUpdate:) name:NSManagedObjectContextDidSaveNotification object:context];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(OE_managedObjectContextDidUpdate:) name:NSManagedObjectContextObjectsDidChangeNotification object:context];
-
     [[NSUserDefaults standardUserDefaults] addObserver:self forKeyPath:OEDisplayGameTitle options:0 context:NULL];
 
     // If the view has been loaded after a collection has been set via -setRepresentedObject:, set the appropriate
@@ -688,7 +687,7 @@ static NSArray *OE_defaultSortDescriptors;
     
     if([object isKindOfClass:[NSManagedObject class]])
     {
-        [[(NSManagedObject*)object managedObjectContext] save:nil];
+        [[(OEDBItem*)object libraryDatabase] save:nil];
     }
 
     // Search results may no longer be valid, reload
@@ -964,7 +963,7 @@ static NSArray *OE_defaultSortDescriptors;
         {
             OEDBCollection* collection = (OEDBCollection*)[self representedObject];
             [[collection mutableGames] minusSet:[NSSet setWithArray:selectedGames]];
-            [[collection managedObjectContext] save:nil];
+            [[collection libraryDatabase] save:nil];
         }
         [self setNeedsReload];
     }
@@ -993,11 +992,10 @@ static NSArray *OE_defaultSortDescriptors;
         }
         
         DLog(@"deleteFiles: %d", deleteFiles);
-        NSManagedObjectContext *moc = [[selectedGames lastObject] managedObjectContext];
         [selectedGames enumerateObjectsUsingBlock:^(OEDBGame *game, NSUInteger idx, BOOL *stopGames) {
             [game deleteByMovingFile:deleteFiles keepSaveStates:YES];
         }];
-        [moc save:nil];
+        [[[selectedGames lastObject] libraryDatabase] save:nil];
         
         [self setNeedsReload];
     }
@@ -1171,8 +1169,8 @@ static NSArray *OE_defaultSortDescriptors;
         }
         else return;
         
-        if([obj isKindOfClass:[NSManagedObject class]])
-            [[(NSManagedObject*)obj managedObjectContext] save:nil];
+        if([obj isKindOfClass:[OEDBItem class]])
+            [[(OEDBItem*)obj libraryDatabase] save:nil];
     }
 }
 
@@ -1403,12 +1401,12 @@ static NSArray *OE_defaultSortDescriptors;
 
     if(hasGameInsertions || hasGameDeletions)
     {
-        [self performSelector:@selector(noteNumbersChanged) onThread:[NSThread mainThread] withObject:nil waitUntilDone:YES];
+        [self performSelector:@selector(noteNumbersChanged) onThread:[NSThread mainThread] withObject:nil waitUntilDone:NO];
     }
     else if(hasGameUpdates)
     {
         // Nothing was removed or added, just updated so just update the visible items
-        [self performSelector:@selector(setNeedsReloadVisible) onThread:[NSThread mainThread] withObject:nil waitUntilDone:YES];
+        [self performSelector:@selector(setNeedsReloadVisible) onThread:[NSThread mainThread] withObject:nil waitUntilDone:NO];
     }
 }
 
@@ -1420,14 +1418,16 @@ static NSArray *OE_defaultSortDescriptors;
 
 - (void)OE_fetchGames
 {
-    NSError *error = nil;
-    BOOL ok = [gamesController fetchWithRequest:nil merge:NO error:&error];
+    __block BOOL ok = NO;
+    [[gamesController managedObjectContext] performBlockAndWait:^{
+        NSError *error = nil;
+        ok = [gamesController fetchWithRequest:nil merge:NO error:&error];
+    }];
+
     if(!ok)
-    {
-        NSLog(@"Error while fetching: %@", error);
-        return;
-    }
-    [self OE_updateBlankSlate];
+        NSLog(@"Error while fetching");
+    else
+        [self OE_updateBlankSlate];
 }
 
 - (void)updateViews
@@ -1488,15 +1488,18 @@ static NSArray *OE_defaultSortDescriptors;
     
     NSPredicate *pred = [self representedObject]?[[self representedObject] predicate]:[NSPredicate predicateWithValue:NO];
     [gamesController setFetchPredicate:pred];
-    
-    NSError *error = nil;
-    BOOL ok = [gamesController fetchWithRequest:nil merge:NO error:&error];
+
+    __block BOOL ok;
+    [[gamesController managedObjectContext] performBlockAndWait:^{
+        ok = [gamesController fetchWithRequest:nil merge:NO error:nil];
+    }];
+
     if(!ok)
     {
-        NSLog(@"Error while fetching: %@", error);
+        NSLog(@"Error while fetching");
         return;
     }
-    
+
     [gridView reloadData];
     [listView reloadData];
     [coverFlowView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];

OpenEmu/OEControlsButtonSetupView.m
@@ -555,7 +555,8 @@ NSComparisonResult headerSortingFunction(id obj1, id obj2, void *context)
                                          label:[[row objectForKey:OEControlListKeyLabelKey] stringByAppendingString:@":"]];
             }
         }
-        [elementGroups addObject:currentGroup];
+        if(currentGroup != nil)
+            [elementGroups addObject:currentGroup];
 
         [sections addObject:@{
          @"numberOfRows" : @(numberOfRows),

OpenEmu/OEDBCollection.h
@@ -25,8 +25,8 @@
  */
 
 #import <Cocoa/Cocoa.h>
-
-@interface OEDBCollection : NSManagedObject
+#import "OEDBItem.h"
+@interface OEDBCollection : OEDBItem
 
 // Core Data utilities
 + (NSString *)entityName;

OpenEmu/OEDBGame.m
@@ -56,14 +56,17 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
 
 + (id)createGameWithName:(NSString *)name andSystem:(OEDBSystem *)system inDatabase:(OELibraryDatabase *)database
 {
-    NSManagedObjectContext *context = [database managedObjectContext];
-    NSEntityDescription *description = [NSEntityDescription entityForName:@"Game" inManagedObjectContext:context];
-    
-    OEDBGame *game = [[OEDBGame alloc] initWithEntity:description insertIntoManagedObjectContext:context];
-    
-    [game setName:name];
-    [game setImportDate:[NSDate date]];
-    [game setSystem:system];
+    NSManagedObjectContext *context = [database unsafeContext];
+
+    __block OEDBGame *game = nil;
+    [context performBlockAndWait:^{
+        NSEntityDescription *description = [NSEntityDescription entityForName:@"Game" inManagedObjectContext:context];
+        game = [[OEDBGame alloc] initWithEntity:description insertIntoManagedObjectContext:context];
+
+        [game setName:name];
+        [game setImportDate:[NSDate date]];
+        [game setSystem:system];
+    }];
     
     return game;
 }
@@ -75,7 +78,7 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
 
 + (id)gameWithID:(NSManagedObjectID *)objID inDatabase:(OELibraryDatabase *)database
 {
-    return [[database managedObjectContext] objectWithID:objID];
+    return [database objectWithID:objID];
 }
 
 + (id)gameWithURIURL:(NSURL *)objIDUrl
@@ -101,13 +104,13 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
 }
 
 // returns the game from the default database that represents the file at url
-+ (id)gameWithURL:(NSURL *)url error:(NSError **)outError
++ (id)gameWithURL:(NSURL *)url error:(NSError *__autoreleasing*)outError
 {
     return [self gameWithURL:url inDatabase:[OELibraryDatabase defaultDatabase] error:outError];
 }
 
 // returns the game from the specified database that represents the file at url
-+ (id)gameWithURL:(NSURL *)url inDatabase:(OELibraryDatabase *)database error:(NSError **)outError
++ (id)gameWithURL:(NSURL *)url inDatabase:(OELibraryDatabase *)database error:(NSError *__autoreleasing*)outError
 {
     if(url == nil)
     {
@@ -143,23 +146,22 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
     return game;
 }
 
-+ (id)gameWithArchiveID:(id)archiveID error:(NSError **)outError
++ (id)gameWithArchiveID:(id)archiveID error:(NSError *__autoreleasing*)outError
 {
     return [self gameWithArchiveID:archiveID inDatabase:[OELibraryDatabase defaultDatabase] error:outError];
 }
 
-+ (id)gameWithArchiveID:(id)archiveID inDatabase:(OELibraryDatabase *)database error:(NSError **)outError
++ (id)gameWithArchiveID:(id)archiveID inDatabase:(OELibraryDatabase *)database error:(NSError *__autoreleasing*)outError
 {
     if([archiveID integerValue] == 0) return nil;
     
-    NSManagedObjectContext *context = [database managedObjectContext];
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:[self entityName]];
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"archiveID = %ld", [archiveID integerValue]];
     [fetchRequest setFetchLimit:1];
     [fetchRequest setIncludesPendingChanges:YES];
     [fetchRequest setPredicate:predicate];
     
-    NSArray *result = [context executeFetchRequest:fetchRequest error:outError];
+    NSArray *result = [database executeFetchRequest:fetchRequest error:outError];
     
     if(result == nil) return nil;
     
@@ -185,28 +187,23 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
 
 + (NSArray *)allGamesInDatabase:(OELibraryDatabase *)database error:(NSError *__autoreleasing *)error;
 {
-    NSManagedObjectContext *context = [database managedObjectContext];    
     NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:[self entityName]];
-    return [context executeFetchRequest:request error:error];
+    return [database executeFetchRequest:request error:error];
 }
 
 #pragma mark - Cover Art Database Sync / Info Lookup
 - (void)requestCoverDownload
 {
-    [[self managedObjectContext] performBlockAndWait:^{
-        [self setStatus:[NSNumber numberWithInt:OEDBGameStatusProcessing]];
-        [[self libraryDatabase] save:nil];
-        [[self libraryDatabase] startArchiveVGSync];
-    }];
+    [self setStatus:[NSNumber numberWithInt:OEDBGameStatusProcessing]];
+    [[self libraryDatabase] save:nil];
+    [[self libraryDatabase] startArchiveVGSync];
 }
 
 - (void)requestInfoSync
 {
-    [[self managedObjectContext] performBlockAndWait:^{
-        [self setStatus:[NSNumber numberWithInt:OEDBGameStatusProcessing]];
-        [[self libraryDatabase] save:nil];
-        [[self libraryDatabase] startArchiveVGSync];
-    }];
+    [self setStatus:[NSNumber numberWithInt:OEDBGameStatusProcessing]];
+    [[self libraryDatabase] save:nil];
+    [[self libraryDatabase] startArchiveVGSync];
 }
 
 - (void)performInfoSync
@@ -215,12 +212,10 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
     __block NSError *error = nil;
 
     NSString * const boxImageURLKey = @"boxImageURL";
-    
-    [[[self libraryDatabase] managedObjectContext] performBlockAndWait:^{
-        OEDBRom *rom = [[self roms] anyObject];
-        result = [[[OEGameInfoHelper sharedHelper] gameInfoForROM:rom error:&error] mutableCopy];
-    }];
-    
+
+    OEDBRom *rom = [[self roms] anyObject];
+    result = [[[OEGameInfoHelper sharedHelper] gameInfoForROM:rom error:&error] mutableCopy];
+
     if(result != nil && [result objectForKey:boxImageURLKey] != nil)
     {
         NSString *normalizedURL = [[result objectForKey:boxImageURLKey] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
@@ -230,22 +225,18 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
         if(image)
         {
             [result removeObjectForKey:boxImageURLKey];
-            [[[self libraryDatabase] managedObjectContext] performBlockAndWait:^{
-                [self setBoxImageByImage:image];
-            }];
+            [self setBoxImageByImage:image];
         }
     }
 
-    
-    [[[self libraryDatabase] managedObjectContext] performBlockAndWait:^{
-        if(result != nil)
-        {
-            [self setValuesForKeysWithDictionary:result];
-            [self setLastArchiveSync:[NSDate date]];
-        }
-        [self setStatus:@(OEDBGameStatusOK)];
-        [[self libraryDatabase] save:nil];
-    }];
+
+    if(result != nil)
+    {
+        [self setValuesForKeysWithDictionary:result];
+        [self setLastArchiveSync:[NSDate date]];
+    }
+    [self setStatus:@(OEDBGameStatusOK)];
+    [[self libraryDatabase] save:nil];
 }
 
 #pragma mark -
@@ -399,8 +390,11 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
         [aRom deleteByMovingFile:moveToTrash keepSaveStates:statesFlag];
         [mutableRoms removeObject:aRom];
     }
-    
-    [[self managedObjectContext] deleteObject:self];
+
+    NSManagedObjectContext *context = [self managedObjectContext];
+    [context performBlockAndWait:^{
+        [context deleteObject:self];
+    }];
 }
 
 + (NSString *)entityName
@@ -421,10 +415,15 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
     {
         OEDBImage *boxImage = [self boxImage];
         if(boxImage != nil)
-            [[boxImage managedObjectContext] deleteObject:boxImage];
-        
+        {
+            NSManagedObjectContext *context = [boxImage managedObjectContext];
+            [context performBlockAndWait:^{
+                [context deleteObject:boxImage];
+            }];
+        }
+
         if(img == nil) return;
-        
+
         boxImage = [OEDBImage imageWithImage:img inLibrary:[self libraryDatabase]];
         
         NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
@@ -445,8 +444,13 @@ NSString *const OEDisplayGameTitle = @"displayGameTitle";
 {
     OEDBImage *boxImage = [self boxImage];
     if(boxImage != nil)
-        [[boxImage managedObjectContext] deleteObject:boxImage];
-    
+    {
+        NSManagedObjectContext *context = [boxImage managedObjectContext];
+        [context performBlockAndWait:^{
+            [context deleteObject:boxImage];
+        }];
+    }
+
     boxImage = [OEDBImage imageWithURL:url inLibrary:[self libraryDatabase]];
     
     NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];

OpenEmu/OEDBImage.m
@@ -33,18 +33,22 @@
 @implementation OEDBImage
 + (id)imageWithImage:(NSImage *)image inLibrary:(OELibraryDatabase *)library
 {
-    NSManagedObjectContext  *context        = [library managedObjectContext];
-    NSEntityDescription     *desc           = [NSEntityDescription entityForName:@"Image" inManagedObjectContext:context];
-    OEDBImage               *imageObject    = [[self alloc] initWithEntity:desc insertIntoManagedObjectContext:context];
-    OEDBImageThumbnail      *thumbnailImage = [[OEDBImageThumbnail alloc] initWithImage:image size:NSZeroSize inLibrary:library];
-    
-    if(!thumbnailImage)
-    {
-        [context deleteObject:imageObject];
-        return nil;
-    }
-    
-    [imageObject addVersion:thumbnailImage];
+    __block OEDBImage *imageObject = nil;
+    NSManagedObjectContext *context = [library unsafeContext];
+    [context performBlockAndWait:^{
+        NSEntityDescription *desc = [NSEntityDescription entityForName:@"Image" inManagedObjectContext:context];
+        imageObject = [[OEDBImage alloc] initWithEntity:desc insertIntoManagedObjectContext:context];
+        OEDBImageThumbnail *thumbnailImage = [OEDBImageThumbnail imageWithImage:image size:NSZeroSize inLibrary:library];
+        if(!thumbnailImage)
+        {
+            [context deleteObject:imageObject];
+            imageObject = nil;
+        }
+        else
+        {
+            [imageObject addVersion:thumbnailImage];
+        }
+    }];
     return imageObject;
 }
 
@@ -192,7 +196,7 @@
     if(!originalImage){
         return;
     }
-    OEDBImageThumbnail  *newThumbnail        = [[OEDBImageThumbnail alloc] initWithImage:originalImage size:size inLibrary:library];
+    OEDBImageThumbnail  *newThumbnail = [OEDBImageThumbnail imageWithImage:originalImage size:size inLibrary:library];
     [self addVersion:newThumbnail];
 }
 @end

OpenEmu/OEDBImageThumbnail.h
@@ -30,7 +30,7 @@
 @class OEDBImage, OELibraryDatabase;
 @interface OEDBImageThumbnail : OEDBItem
 
-- (id)initWithImage:(NSImage*)image size:(NSSize)size inLibrary:(OELibraryDatabase*)library;
++ (id)imageWithImage:(NSImage*)image size:(NSSize)size inLibrary:(OELibraryDatabase*)library;
 
 @property (nonatomic, retain) NSNumber * height;
 @property (nonatomic, retain) NSNumber * width;

OpenEmu/OEDBImageThumbnail.m
@@ -32,89 +32,96 @@
 
 @implementation OEDBImageThumbnail
 
-- (id)initWithImage:(NSImage*)image size:(NSSize)size inLibrary:(OELibraryDatabase*)library{
-  
-    NSManagedObjectContext  *context     = [library managedObjectContext];
-    NSEntityDescription     *description = [OEDBImageThumbnail entityDescriptionInContext:context];
-    self = [super initWithEntity:description insertIntoManagedObjectContext:context];
-    if(!self) return nil;
-    
-    BOOL     resize      = !NSEqualSizes(size, NSZeroSize);
-    NSString *version    = !resize ? @"original" : [NSString stringWithFormat:@"%d", (int)size.width];
-    NSString *uuid       = [NSString stringWithUUID];
-    
-    NSURL    *coverFolderURL = [library coverFolderURL];
-              coverFolderURL = [coverFolderURL URLByAppendingPathComponent:version isDirectory:YES];
-    
-    NSURL *url          = [coverFolderURL URLByAppendingPathComponent:uuid];
-    
-    NSBitmapImageRep    *bitmapRep   = nil;
-    NSSize              imageSize;
++ (id)imageWithImage:(NSImage*)image size:(NSSize)size inLibrary:(OELibraryDatabase*)library
+{
+    __block OEDBImageThumbnail *result = nil;
+    NSManagedObjectContext *context = [library unsafeContext];
+    [context performBlockAndWait:^{
+        NSEntityDescription     *description = [OEDBImageThumbnail entityDescriptionInContext:context];
 
-    // find a bitmap representation
-    for(NSImageRep *rep in [image representations])
-    {
-        if([rep isKindOfClass:[NSBitmapImageRep class]])
+        result = [[OEDBImageThumbnail alloc] initWithEntity:description insertIntoManagedObjectContext:context];
+        if(result != nil)
         {
-            bitmapRep = (NSBitmapImageRep*)rep;
-            break;
-        }
-    }
 
-    if(bitmapRep)
-        imageSize = NSMakeSize([bitmapRep pixelsWide], [bitmapRep pixelsHigh]);
-    else
-        imageSize = [image size];
-    
-    float               aspectRatio      = imageSize.width / imageSize.height;
-    NSSize              thumbnailSize;
-    if(resize)
-    {       
-        thumbnailSize = aspectRatio<1 ? (NSSize){size.height * aspectRatio, size.height} : (NSSize){size.width, size.width / aspectRatio};
-        
-        // thumbnails only make sense if they are smaller than the original
-        if(thumbnailSize.width >= imageSize.width || thumbnailSize.height >= imageSize.height){
-            [context deleteObject:self];
-            return nil;
+            BOOL     resize      = !NSEqualSizes(size, NSZeroSize);
+            NSString *version    = !resize ? @"original" : [NSString stringWithFormat:@"%d", (int)size.width];
+            NSString *uuid       = [NSString stringWithUUID];
+
+            NSURL    *coverFolderURL = [library coverFolderURL];
+            coverFolderURL = [coverFolderURL URLByAppendingPathComponent:version isDirectory:YES];
+
+            NSURL *url          = [coverFolderURL URLByAppendingPathComponent:uuid];
+
+            NSBitmapImageRep    *bitmapRep   = nil;
+            NSSize              imageSize;
+
+            // find a bitmap representation
+            for(NSImageRep *rep in [image representations])
+            {
+                if([rep isKindOfClass:[NSBitmapImageRep class]])
+                {
+                    bitmapRep = (NSBitmapImageRep*)rep;
+                    break;
+                }
+            }
+
+            if(bitmapRep)
+                imageSize = NSMakeSize([bitmapRep pixelsWide], [bitmapRep pixelsHigh]);
+            else
+                imageSize = [image size];
+
+            float               aspectRatio      = imageSize.width / imageSize.height;
+            NSSize              thumbnailSize;
+            if(resize)
+            {
+                thumbnailSize = aspectRatio<1 ? (NSSize){size.height * aspectRatio, size.height} : (NSSize){size.width, size.width / aspectRatio};
+
+                // thumbnails only make sense if they are smaller than the original
+                if(thumbnailSize.width >= imageSize.width || thumbnailSize.height >= imageSize.height){
+                    [context deleteObject:result];
+                    return;
+                }
+
+                NSImage *thumbnailImage = [[NSImage alloc] initWithSize:thumbnailSize];
+                [thumbnailImage lockFocus];
+                [image drawInRect:(NSRect){{0, 0}, {thumbnailSize.width, thumbnailSize.height}} fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
+                bitmapRep = [[NSBitmapImageRep alloc] initWithFocusedViewRect:(NSRect){{0,0}, thumbnailSize}];
+                [thumbnailImage unlockFocus];
+            }
+            else
+            {
+                thumbnailSize = imageSize;
+
+                if(!bitmapRep) // none found, create one
+                {
+                    [image lockFocus];
+                    bitmapRep = [[NSBitmapImageRep alloc] initWithFocusedViewRect:(NSRect){{0,0}, thumbnailSize}];
+                    [image unlockFocus];
+                }
+            }
+
+            [result setRelativePath:[NSString stringWithFormat:@"%@/%@", version, uuid]];
+            [result setWidth:[NSNumber numberWithFloat:thumbnailSize.width]];
+            [result setHeight:[NSNumber numberWithFloat:thumbnailSize.height]];
+
+
+            // write image file
+            NSDictionary *properties = [NSDictionary dictionary];
+            NSData       *imageData  = [bitmapRep representationUsingType:NSPNGFileType properties:properties];
+            
+            [[NSFileManager defaultManager] createDirectoryAtURL:[url URLByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:nil error:nil];
+            
+            NSError *error = nil;
+            if(![imageData writeToURL:url options:0 error:&error]){
+                [context deleteObject:result];
+                result = nil;
+            }
         }
-        
-        NSImage *thumbnailImage = [[NSImage alloc] initWithSize:thumbnailSize];
-        [thumbnailImage lockFocus];
-        [image drawInRect:(NSRect){{0, 0}, {thumbnailSize.width, thumbnailSize.height}} fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
-        bitmapRep = [[NSBitmapImageRep alloc] initWithFocusedViewRect:(NSRect){{0,0}, thumbnailSize}];
-        [thumbnailImage unlockFocus];
-    }
-    else
-    {
-        thumbnailSize = imageSize;
-        
-        if(!bitmapRep) // none found, create one
-        {
-            [image lockFocus];
-            bitmapRep = [[NSBitmapImageRep alloc] initWithFocusedViewRect:(NSRect){{0,0}, thumbnailSize}];
-            [image unlockFocus];
-        }
-    }
+    }];
     
-    [self setRelativePath:[NSString stringWithFormat:@"%@/%@", version, uuid]];
-    [self setWidth:[NSNumber numberWithFloat:thumbnailSize.width]];
-    [self setHeight:[NSNumber numberWithFloat:thumbnailSize.height]];
-    
-    
-    // write image file
-    NSDictionary *properties = [NSDictionary dictionary];
-    NSData       *imageData  = [bitmapRep representationUsingType:NSPNGFileType properties:properties];
-    
-    [[NSFileManager defaultManager] createDirectoryAtURL:[url URLByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:nil error:nil];
-    
-    NSError *error = nil;
-    if(![imageData writeToURL:url options:0 error:&error]){
-       [context deleteObject:self];
-        self = nil;
-    }
-    
-    return self;
+    return result;
 }
+
 
 - (NSString*)absolutePath
 {

commit 42f657d4a02ac56889d690f206873d42176ef078

Fix save state creation. (Issue #791, bug introduced in 4b42c2ad453edb417d39ec8da076af1c742e6315)

OpenEmu/OEDBSaveState.m
@@ -104,8 +104,7 @@ NSString *const OESaveStateUseQuickSaveSlotsKey = @"UseQuickSaveSlots";
     __block OEDBSaveState *result = nil;
     [context performBlockAndWait:^{
         NSEntityDescription *description = [NSEntityDescription entityForName:@"SaveState" inManagedObjectContext:context];
-        OEDBSaveState *result = [[OEDBSaveState alloc] initWithEntity:description insertIntoManagedObjectContext:context];
-
+        result = [[OEDBSaveState alloc] initWithEntity:description insertIntoManagedObjectContext:context];
         [result setTimestamp:[NSDate date]];
     }];
 

OpenEmu/OEGameDocument.m
@@ -1067,7 +1067,7 @@ typedef enum : NSUInteger
          NSData *PNGData = [bitmapImageRep representationUsingType:NSPNGFileType properties:nil];
 
          __autoreleasing NSError *saveError = nil;
-         if(![PNGData writeToURL:[state screenshotURL] options:NSDataWritingAtomic error:&saveError])
+         if([state screenshotURL] == nil || ![PNGData writeToURL:[state screenshotURL] options:NSDataWritingAtomic error:&saveError])
              NSLog(@"Could not create screenshot at url: %@ with error: %@", [state screenshotURL], saveError);
 
          if(handler != nil) handler();