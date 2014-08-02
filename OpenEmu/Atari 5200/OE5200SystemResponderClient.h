319c36230e2e15b9e9d1e50876a401d583dad033
clobber <brymaster@gmail.com>
2014-08-02 14:34:36 -0500

    Support keypad in Atari 5200 system plugin

--- OpenEmu/Atari 5200/OE5200SystemResponderClient.h
+++ OpenEmu/Atari 5200/OE5200SystemResponderClient.h
@@ -39,6 +39,18 @@ typedef enum _OE5200Button
     OE5200ButtonStart,
 	OE5200ButtonPause,
     OE5200ButtonReset,
+    OE5200Button1,
+    OE5200Button2,
+    OE5200Button3,
+    OE5200Button4,
+    OE5200Button5,
+    OE5200Button6,
+    OE5200Button7,
+    OE5200Button8,
+    OE5200Button9,
+    OE5200Button0,
+    OE5200ButtonAsterisk,
+    OE5200ButtonPound,
 	OE5200ButtonCount
 } OE5200Button;
 
