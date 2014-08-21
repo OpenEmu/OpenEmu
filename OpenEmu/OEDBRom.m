OpenEmu/OpenEmu

commit 5f5c087946975b98a0651c7104ec6c2efb3318cb

Fix some path issues introduce in the last commits.

OpenEmu/OEDBRom.m
@@ -51,7 +51,11 @@
 {
     if(url == nil) return nil;
 
-    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"location == %@", [url absoluteString]];
+    NSURL *romFolderURL =  [[context userInfo] valueForKey:OELibraryDatabaseUserInfoKey];
+
+    url = [url urlRelativeToURL:romFolderURL];
+
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"location == %@", [url relativeString]];
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[self entityName]];
     
     [fetchRequest setFetchLimit:1];
@@ -93,17 +97,8 @@
 
 - (NSURL *)URL
 {
-    NSURL *result = nil;
-    NSString *location = [self location];
-    if([location isAbsolutePath])
-        result = [NSURL URLWithString:location];
-    else
-    {
-        OELibraryDatabase *database = [self libraryDatabase];
-        NSURL *baseURL = [database romsFolderURL];
-        result = [NSURL URLWithString:location relativeToURL:baseURL];
-    }
-    return result;
+    NSURL *romFolderURL = [[self libraryDatabase] romsFolderURL];
+    return [NSURL URLWithString:[self location] relativeToURL:romFolderURL];
 }
 
 - (void)setURL:(NSURL *)url

OpenEmu/OEDBImage.m
@@ -106,8 +106,6 @@
     OELibraryDatabase *database = [OELibraryDatabase defaultDatabase];
     NSURL *coverFolderURL = [database coverFolderURL];
     NSURL *imageURL = [NSURL URLWithString:fileName relativeToURL:coverFolderURL];
-    imageURL = [imageURL URLByStandardizingPath];
-
     if(![data writeToURL:imageURL atomically:YES])
     {
         [[NSFileManager defaultManager] removeItemAtURL:imageURL error:nil];

commit 95fb96ff8633e3b76503c7ea3e8cbc2168c3229a

Fix command-o to import & launch a game. Broken in 5f5c087946975b98a0651c7104ec6c2efb3318cb.

OpenEmu/OEDBRom.m
@@ -51,7 +51,8 @@
 {
     if(url == nil) return nil;
 
-    NSURL *romFolderURL =  [[context userInfo] valueForKey:OELibraryDatabaseUserInfoKey];
+    OELibraryDatabase *library = [[context userInfo] valueForKey:OELibraryDatabaseUserInfoKey];
+    NSURL *romFolderURL = [library romsFolderURL];
 
     url = [url urlRelativeToURL:romFolderURL];
 