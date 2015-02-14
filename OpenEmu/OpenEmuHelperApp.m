52ba6f71a0aa6023dc990e97cc650a44d5cada0c
clobber <brymaster@gmail.com>
2015-02-13 23:46:30 -0600

    Add 320M to hasSlowClientStorage. Fixes #1762 and #1822
    Need to investigate removing use of GL client storage completely but it seems to affect performance a bit

--- OpenEmu/OpenEmuHelperApp.m
+++ OpenEmu/OpenEmuHelperApp.m
@@ -162,7 +162,7 @@
     
     const GLubyte *vendor = glGetString(GL_VENDOR);
     const GLubyte *renderer = glGetString(GL_RENDERER);
-    _hasSlowClientStorage = strstr((const char*)vendor, "Intel") || strstr((const char*)renderer, "NVIDIA GeForce 9600M GT OpenGL Engine") || strstr((const char*)renderer, "NVIDIA GeForce 8600M GT OpenGL Engine") || strstr((const char*)renderer, "NVIDIA GeForce GT 330M OpenGL Engine") != NULL;
+    _hasSlowClientStorage = strstr((const char*)vendor, "Intel") || strstr((const char*)renderer, "NVIDIA GeForce 9600M GT OpenGL Engine") || strstr((const char*)renderer, "NVIDIA GeForce 8600M GT OpenGL Engine") || strstr((const char*)renderer, "NVIDIA GeForce GT 330M OpenGL Engine") || strstr((const char*)renderer, "NVIDIA GeForce 320M OpenGL Engine") != NULL;
 }
 
 - (void)setupIOSurface
