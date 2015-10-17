147c4414543a34c25ee947e1721a904583e1fe85
Preston Sumner <prestonsumner@me.com>
2015-10-17 17:44:43 -0600

    Set medium weight for the grid label font.

--- OpenEmu/OEGridGameCell.m
+++ OpenEmu/OEGridGameCell.m
@@ -314,7 +314,7 @@ static NSDictionary *disabledActions = nil;
     [_foregroundLayer setActions:disabledActions];
 
     // setup title layer
-    NSFont *titleFont = [NSFont labelFontOfSize:[NSFont labelFontSize]];
+    NSFont *titleFont = [NSFont systemFontOfSize:11 weight:NSFontWeightMedium];
     _textLayer = [CATextLayer layer];
     [_textLayer setActions:disabledActions];
 
