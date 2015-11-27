1a0ccf454c99575f07685e90b88db5cd6ddb1531
clobber <brymaster@gmail.com>
2015-11-26 22:58:23 -0600

    Add Atari 8-bit system plugin

---
+++ OpenEmu/Atari 8-bit/OEA8SystemResponder.m
@@ -0,0 +1,85 @@
+/*
+ Copyright (c) 2015, OpenEmu Team
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the OpenEmu Team nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "OEA8SystemResponder.h"
+#import "OEA8SystemResponderClient.h"
+
+@implementation OEA8SystemResponder
+@dynamic client;
+
++ (Protocol *)gameSystemResponderClientProtocol;
+{
+    return @protocol(OEA8SystemResponderClient);
+}
+
+- (void)HIDKeyDown:(OEHIDEvent *)theEvent
+{
+    [super HIDKeyDown:theEvent];
+    [[self client] keyDown:[theEvent keycode] characters:[theEvent characters] charactersIgnoringModifiers:[theEvent charactersIgnoringModifiers] flags:[theEvent modifierFlags]];
+}
+
+- (void)HIDKeyUp:(OEHIDEvent *)theEvent
+{
+    [super HIDKeyUp:theEvent];
+    [[self client] keyUp:[theEvent keycode] characters:[theEvent characters] charactersIgnoringModifiers:[theEvent charactersIgnoringModifiers] flags:[theEvent modifierFlags]];
+}
+
+- (void)pressEmulatorKey:(OESystemKey *)aKey
+{
+    [[self client] didPushA8Button:(OEA8Button)[aKey key] forPlayer:[aKey player]];
+}
+
+- (void)releaseEmulatorKey:(OESystemKey *)aKey
+{
+    [[self client] didReleaseA8Button:(OEA8Button)[aKey key] forPlayer:[aKey player]];
+}
+
+- (void)mouseMovedAtPoint:(OEIntPoint)aPoint
+{
+    [[self client] mouseMovedAtPoint:aPoint];
+}
+
+- (void)mouseDownAtPoint:(OEIntPoint)aPoint
+{
+    [[self client] leftMouseDownAtPoint:aPoint];
+}
+
+- (void)mouseUpAtPoint
+{
+    [[self client] leftMouseUp];
+}
+
+- (void)rightMouseDownAtPoint:(OEIntPoint)aPoint
+{
+    [[self client] rightMouseDownAtPoint:aPoint];
+}
+
+- (void)rightMouseUpAtPoint
+{
+    [[self client] rightMouseUp];
+}
+
+@end
