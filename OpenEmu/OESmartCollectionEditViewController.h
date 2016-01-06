c77ae27f034c426f4842e9e064d340a9c435f649
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2016-01-06 17:29:58 +0100

    Add view controller to manage editing smart collections.

---
+++ OpenEmu/OESmartCollectionEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  OESmartCollectionEditViewController.h
+//  OpenEmu
+//
+//  Created by Christoph Leimbrock on 1/6/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OESmartCollectionEditViewController : NSViewController
+
+@end
---
+++ OpenEmu/OESmartCollectionEditViewController.m
@@ -0,0 +1,21 @@
+//
+//  OESmartCollectionEditViewController.m
+//  OpenEmu
+//
+//  Created by Christoph Leimbrock on 1/6/16.
+//
+//
+
+#import "OESmartCollectionEditViewController.h"
+
+@interface OESmartCollectionEditViewController ()
+
+@end
+
+@implementation OESmartCollectionEditViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
