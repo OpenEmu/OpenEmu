7af924a7c131c9c32dfcf1408855b5d4fcaaeae5
clobber <brymaster@gmail.com>
2017-10-20 19:46:00 -0500

    Fix support for 2 players and add Console Pause/Hold button in SG-1000 system plugin

--- OpenEmu/SG-1000/OESG1000SystemResponder.m
+++ OpenEmu/SG-1000/OESG1000SystemResponder.m
@@ -37,12 +37,12 @@
 
 - (void)pressEmulatorKey:(OESystemKey *)aKey
 {
-    [[self client] didPushSG1000Button:(OESG1000Button)[aKey key]];
+    [[self client] didPushSG1000Button:(OESG1000Button)[aKey key] forPlayer:[aKey player]];
 }
 
 - (void)releaseEmulatorKey:(OESystemKey *)aKey
 {
-    [[self client] didReleaseSG1000Button:(OESG1000Button)[aKey key]];
+    [[self client] didReleaseSG1000Button:(OESG1000Button)[aKey key] forPlayer:[aKey player]];
 }
 
 @end
