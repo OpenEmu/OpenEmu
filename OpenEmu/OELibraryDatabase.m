OpenEmu/OpenEmu

commit 5cde302e08da971af861bc150462f28a2f61a5a4

Trim title of imported multi-disc games

OpenEmu/OELibraryDatabase.m
@@ -942,6 +942,20 @@ static OELibraryDatabase *defaultDatabase = nil;
             NSManagedObjectID *objectID = gameInfo[@"permanentID"];
             NSDictionary *result = [helper gameInfoWithDictionary:gameInfo];
 
+            // Trim the gameTitle for imported m3u's so they look nice
+            NSURL *gameInfoURL = gameInfo[@"URL"];
+            NSString *gameURLWithSuffix = gameInfoURL.lastPathComponent;
+            NSString *resultGameTitle = result[@"gameTitle"];
+            if ([gameURLWithSuffix.pathExtension.lowercaseString isEqualToString:@"m3u"])
+            {
+                // RegEx pattern match the parentheses e.g. " (Disc 1)" and update dictionary with trimmed gameTitle string
+                NSString *newGameTitle = [resultGameTitle stringByReplacingOccurrencesOfString:@"\\ \\(Disc.*\\)" withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, [resultGameTitle length])];
+
+                NSMutableDictionary *mutableDict = [result mutableCopy];
+                [mutableDict setObject:newGameTitle forKey:@"gameTitle"];
+                result = [mutableDict mutableCopy];
+            }
+
             NSMutableDictionary *dict = [@{ @"objectID" : objectID, @"status" : @(OEDBGameStatusOK) } mutableCopy];
 
             if(result != nil)

commit d98cfda6da126ed0934926e057672be12e287744

Fix crash introduced in 5cde302e08da971af861bc150462f28a2f61a5a4 if URL is NSNull

OpenEmu/OELibraryDatabase.m
@@ -951,16 +951,19 @@ static OELibraryDatabase * _Nullable defaultDatabase = nil;
 
             // Trim the gameTitle for imported m3u's so they look nice
             NSURL *gameInfoURL = gameInfo[@"URL"];
-            NSString *gameURLWithSuffix = gameInfoURL.lastPathComponent;
-            NSString *resultGameTitle = result[@"gameTitle"];
-            if (resultGameTitle && [gameURLWithSuffix.pathExtension.lowercaseString isEqualToString:@"m3u"])
+            if (![gameInfoURL isEqual:[NSNull null]])
             {
-                // RegEx pattern match the parentheses e.g. " (Disc 1)" and update dictionary with trimmed gameTitle string
-                NSString *newGameTitle = [resultGameTitle stringByReplacingOccurrencesOfString:@"\\ \\(Disc.*\\)" withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, [resultGameTitle length])];
+                NSString *gameURLWithSuffix = gameInfoURL.lastPathComponent;
+                NSString *resultGameTitle = result[@"gameTitle"];
+                if (resultGameTitle && [gameURLWithSuffix.pathExtension.lowercaseString isEqualToString:@"m3u"])
+                {
+                    // RegEx pattern match the parentheses e.g. " (Disc 1)" and update dictionary with trimmed gameTitle string
+                    NSString *newGameTitle = [resultGameTitle stringByReplacingOccurrencesOfString:@"\\ \\(Disc.*\\)" withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, [resultGameTitle length])];
 
-                NSMutableDictionary *mutableDict = [result mutableCopy];
-                [mutableDict setObject:newGameTitle forKey:@"gameTitle"];
-                result = [mutableDict mutableCopy];
+                    NSMutableDictionary *mutableDict = [result mutableCopy];
+                    [mutableDict setObject:newGameTitle forKey:@"gameTitle"];
+                    result = [mutableDict mutableCopy];
+                }
             }
 
             NSMutableDictionary *dict = [@{ @"objectID" : objectID, @"status" : @(OEDBGameStatusOK) } mutableCopy];