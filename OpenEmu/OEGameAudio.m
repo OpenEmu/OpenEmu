d2fc9b6bb2df07ae05fb8daf3f624f6da04c8400
Stuart Carnie <stuart.carnie@gmail.com>
2019-10-26 21:27:22 -0700

    hack: Delay cleanup after AVAudioEngine stop
    Fighting race conditions when AVAudioEngine is released. Delay the
    cleanup of the AVAudioEngine after it has been stopped.

--- OpenEmu/OEGameAudio.m
+++ OpenEmu/OEGameAudio.m
@@ -251,9 +251,8 @@ static OSStatus AudioConverterFillComplexBufferBlock(AudioConverterRef inAudioCo
     if(outputDeviceID != currentID)
     {
         _outputDeviceID = outputDeviceID;
-        //[self stopAudio];
-        //[self performSelector:@selector(createAudioEngine) withObject:nil afterDelay:0.020];
-        [self createAudioEngine];
+        [self stopAudio];
+        [self performSelector:@selector(createAudioEngine) withObject:nil afterDelay:0.020];
     }
 }
 
