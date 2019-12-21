969182713ae1d494b5b593bc6f5119b40fb16e1f
Stuart Carnie <stuart.carnie@gmail.com>
2019-12-21 15:15:29 -0700

    feat: Use class property

--- OpenEmu/OEShadersModel.swift
+++ OpenEmu/OEShadersModel.swift
@@ -102,7 +102,7 @@ public class OEShadersModel : NSObject {
     @objc
     public var defaultShader: OEShaderModel {
         get {
-            if let name = UserDefaults.oe_application().string(forKey: Preferences.global.key),
+            if let name = UserDefaults.oe_application.string(forKey: Preferences.global.key),
                 let shader = self[name] {
                 return shader
             }
@@ -122,7 +122,7 @@ public class OEShadersModel : NSObject {
     
     @objc
     public func shader(forSystem identifier: String) -> OEShaderModel? {
-        guard let name = UserDefaults.oe_application().string(forKey: Preferences.system(identifier).key) else {
+        guard let name = UserDefaults.oe_application.string(forKey: Preferences.system(identifier).key) else {
             return defaultShader
         }
         return self[name]
@@ -236,7 +236,7 @@ public class OEShadersModel : NSObject {
         
         @objc
         public func parameters(forIdentifier identifier: String) -> [String: Double]? {
-            if let state = UserDefaults.oe_application().string(forKey: Params.system(self.name, identifier).key) {
+            if let state = UserDefaults.oe_application.string(forKey: Params.system(self.name, identifier).key) {
                 var res = [String:Double]()
                 for param in state.split(separator: ";") {
                     let vals = param.split(separator: "=")
