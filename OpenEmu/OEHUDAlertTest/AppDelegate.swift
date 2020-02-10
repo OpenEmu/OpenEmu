b6a0b4d62dcee2a043726a1a852f60a5f6973f24
Daniele Cattaneo <daniele.cattaneo@alice.it>
2020-02-10 21:27:21 +0100

    Add a test application for debugging OEHUDAlert

---
+++ OpenEmu/OEHUDAlertTest/AppDelegate.swift
@@ -0,0 +1,137 @@
+// Copyright (c) 2020, OpenEmu Team
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the OpenEmu Team nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import Cocoa
+
+
+class AlertStatus: NSObject
+{
+    @objc var defaultButtonText = "DEFAULT_BTN"
+    @objc var alternateButtonText = ""
+    @objc var otherButtonText = ""
+    @objc var messageText = ""
+    @objc var headlineText = ""
+    @objc var showsInputField = false
+    @objc var stringValue = ""
+    @objc var showsOtherInputField = false
+    @objc var otherStringValue = ""
+    @objc var inputLabelText = ""
+    @objc var otherInputLabelText = ""
+    @objc var showsProgressbar = false
+    @objc var progress = 0.0
+    @objc var suppressionLabelText = ""
+    @objc var showsSuppressionButton = false
+    
+    
+    func update(_ alert: OEHUDAlert)
+    {
+        alert.messageText = messageText
+        alert.defaultButtonTitle = defaultButtonText
+        alert.alternateButtonTitle = alternateButtonText
+        alert.otherButtonTitle = otherButtonText
+        alert.headlineText = headlineText
+        alert.showsInputField = showsInputField
+        alert.stringValue = stringValue
+        alert.showsOtherInputField = showsOtherInputField
+        alert.otherStringValue = otherStringValue
+        alert.showsInputField = showsInputField
+        alert.inputLabelText = inputLabelText
+        alert.showsOtherInputField = showsOtherInputField
+        alert.otherInputLabelText = otherInputLabelText
+        alert.showsProgressbar = showsProgressbar
+        alert.progress = progress
+        alert.suppressionLabelText = suppressionLabelText
+        alert.showsSuppressionButton = showsSuppressionButton
+    }
+}
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+    @IBOutlet weak var window: NSWindow!
+    @objc var currentStatus = AlertStatus()
+    var nextStatuses = [AlertStatus]()
+    
+    var alert = OEHUDAlert(messageText: "", defaultButton: "", alternateButton: "")
+
+
+    func applicationWillFinishLaunching(_ aNotification: Notification)
+    {
+        if #available(OSX 10.14, *) {
+            NSApp.appearance = NSAppearance(named: .darkAqua)
+        }
+    }
+    
+    func updateAlert()
+    {
+        currentStatus.update(alert)
+    }
+    
+    @IBAction func pushStatus(_ sender: Any?)
+    {
+        nextStatuses.insert(currentStatus, at: 0)
+        self.willChangeValue(for: \.currentStatus)
+        currentStatus = AlertStatus()
+        self.didChangeValue(for: \.currentStatus)
+    }
+
+    @IBAction func runModal(_ sender: Any?)
+    {
+        alert = OEHUDAlert(messageText: "", defaultButton: "", alternateButton: "")
+        updateAlert()
+        if !nextStatuses.isEmpty {
+            alert.setDefaultButtonAction(#selector(alertButtonPressed(_:)), andTarget: self)
+        }
+        alert.runModal()
+    }
+    
+    @IBAction func nsAlert(_ sender: Any?)
+    {
+        let nsalert = NSAlert()
+        nsalert.messageText = currentStatus.headlineText
+        nsalert.informativeText = currentStatus.messageText
+        nsalert.addButton(withTitle: currentStatus.defaultButtonText)
+        if (currentStatus.alternateButtonText.count != 0) {
+            nsalert.addButton(withTitle: currentStatus.alternateButtonText)
+        }
+        if (currentStatus.otherButtonText.count != 0) {
+            nsalert.addButton(withTitle: currentStatus.otherButtonText)
+        }
+        nsalert.runModal()
+    }
+    
+    
+    @objc func alertButtonPressed(_ sender: Any?)
+    {
+        if nextStatuses.isEmpty {
+            NSApp.stopModal(withCode: .alertFirstButtonReturn)
+        } else {
+            currentStatus = nextStatuses[0]
+            nextStatuses.remove(at: 0)
+            updateAlert()
+        }
+    }
+}
+
