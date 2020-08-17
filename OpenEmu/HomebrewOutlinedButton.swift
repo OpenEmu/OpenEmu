26681941f237b2bd7b27ad0a65e717ab9eaf3982
J-rg <j_rg@live.de>
2020-08-17 20:47:51 +0200

    Replace OEOutlinedButtonCell

---
+++ OpenEmu/HomebrewOutlinedButton.swift
@@ -0,0 +1,76 @@
+// Copyright (c) 2020, OpenEmu Team
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the OpenEmu Team nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import Cocoa
+
+@objc(OEHomebrewOutlinedButton)
+final class HomebrewOutlinedButton: NSButton {
+    
+    private let color = NSColor(white: 0.5, alpha: 1)
+    private let buttonShadow: NSShadow = {
+        let shadow = NSShadow()
+        shadow.shadowColor = NSColor(named: "labelShadowColor")
+        shadow.shadowOffset = NSMakeSize(0, -1)
+        return shadow
+    }()
+    
+    override func draw(_ dirtyRect: NSRect) {
+        
+        let frame = dirtyRect.insetBy(dx: 1, dy: 2)
+        
+        NSGraphicsContext.current?.saveGraphicsState()
+        
+        color.setStroke()
+        buttonShadow.set()
+        
+        let bezierPath = NSBezierPath(roundedRect: frame, xRadius: 3, yRadius: 3)
+        bezierPath.lineWidth = 1
+        bezierPath.stroke()
+        
+        NSGraphicsContext.current?.restoreGraphicsState()
+        
+        title.draw(in: frame.insetBy(dx: 0, dy: -0.5), withAttributes: HomebrewOutlinedButton.attributes)
+    }
+    
+    private static let attributes: [NSAttributedString.Key : Any] = {
+        
+        let font = NSFont.systemFont(ofSize: 12)
+        let color = NSColor(white: 0.5, alpha: 1)
+        
+        let shadow = NSShadow()
+        shadow.shadowColor = NSColor(named: "labelShadowColor")
+        shadow.shadowOffset = NSMakeSize(0, -1)
+        
+        let style = NSMutableParagraphStyle()
+        style.alignment = .center
+        
+        let attributes: [NSAttributedString.Key : Any] =
+                                          [.font: font,
+                                .foregroundColor: color,
+                                         .shadow: shadow,
+                                 .paragraphStyle: style]
+        
+        return attributes
+    }()
+}
