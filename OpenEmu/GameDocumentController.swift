e5f60b78dd0b3ce1795d81380cc2ac713d754b09
C.W. Betts <computers57@hotmail.com>
2020-10-16 00:48:43 -0600

    One place where more Swift-like errors might be useful.

--- OpenEmu/GameDocumentController.swift
+++ OpenEmu/GameDocumentController.swift
@@ -154,7 +154,7 @@ class GameDocumentController: NSDocumentController {
                 self.setUpGameDocument(document, display: true, fullScreen: fullScreen, completionHandler: nil)
             }
             
-            if let error = error as NSError?, error.domain == OEGameDocumentErrorDomain, error.code == Int(OEGameDocumentErrorCodes.importRequiredError.rawValue) {
+            if let error = error as? OEGameDocumentErrorCodes, error.code == OEGameDocumentErrorCodes.importRequiredError  {
                 completionHandler(nil, false, nil)
                 return
             }
