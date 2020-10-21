OpenEmu/OpenEmu

commit 43ad33d9b531cdcf254e6a6f01ddc3b234814f32

Remove superfluous subview from sidebar
Remove empty OESidebarController XIB

OpenEmu/OpenEmu.xcodeproj/project.pbxproj
@@ -323,7 +323,6 @@
 		8301557B1456BF7A0093D681 /* OEGameViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = 8301557A1456BF7A0093D681 /* OEGameViewController.m */; };
 		8305A48018FD2908005F1AE4 /* OEGridViewCellIndicationLayer.m in Sources */ = {isa = PBXBuildFile; fileRef = 8305A47F18FD2908005F1AE4 /* OEGridViewCellIndicationLayer.m */; };
 		8306CD101BD3AC180025BC6C /* OELibraryGamesViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = 8306CD0E1BD3AC180025BC6C /* OELibraryGamesViewController.m */; };
-		8306CD261BD3AC3D0025BC6C /* OESidebarController.xib in Resources */ = {isa = PBXBuildFile; fileRef = 8306CD1A1BD3AC3D0025BC6C /* OESidebarController.xib */; };
 		8306CD2B1BD3AD480025BC6C /* OESidebarCell.m in Sources */ = {isa = PBXBuildFile; fileRef = 8306CD161BD3AC3D0025BC6C /* OESidebarCell.m */; };
 		8306CD2C1BD3AD480025BC6C /* OESidebarController.m in Sources */ = {isa = PBXBuildFile; fileRef = 8306CD181BD3AC3D0025BC6C /* OESidebarController.m */; };
 		8306CD2E1BD3AD480025BC6C /* OESidebarGroupItem.m in Sources */ = {isa = PBXBuildFile; fileRef = 8306CD1E1BD3AC3D0025BC6C /* OESidebarGroupItem.m */; };
@@ -1716,7 +1715,6 @@
 		8306CD161BD3AC3D0025BC6C /* OESidebarCell.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OESidebarCell.m; sourceTree = "<group>"; };
 		8306CD171BD3AC3D0025BC6C /* OESidebarController.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OESidebarController.h; sourceTree = "<group>"; };
 		8306CD181BD3AC3D0025BC6C /* OESidebarController.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OESidebarController.m; sourceTree = "<group>"; };
-		8306CD1A1BD3AC3D0025BC6C /* OESidebarController.xib */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = file.xib; path = OESidebarController.xib; sourceTree = "<group>"; };
 		8306CD1D1BD3AC3D0025BC6C /* OESidebarGroupItem.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OESidebarGroupItem.h; sourceTree = "<group>"; };
 		8306CD1E1BD3AC3D0025BC6C /* OESidebarGroupItem.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OESidebarGroupItem.m; sourceTree = "<group>"; };
 		8306CD1F1BD3AC3D0025BC6C /* OESidebarItem.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OESidebarItem.h; sourceTree = "<group>"; };
@@ -3843,7 +3841,6 @@
 				8306CD161BD3AC3D0025BC6C /* OESidebarCell.m */,
 				8306CD171BD3AC3D0025BC6C /* OESidebarController.h */,
 				8306CD181BD3AC3D0025BC6C /* OESidebarController.m */,
-				8306CD1A1BD3AC3D0025BC6C /* OESidebarController.xib */,
 				8306CD1D1BD3AC3D0025BC6C /* OESidebarGroupItem.h */,
 				8306CD1E1BD3AC3D0025BC6C /* OESidebarGroupItem.m */,
 				8306CD1F1BD3AC3D0025BC6C /* OESidebarItem.h */,
@@ -6489,7 +6486,6 @@
 			files = (
 				05E4400B22991527009E6D27 /* Shaders in Resources */,
 				C6C3734316BE7BAF003E57AE /* OE Startup.qtz in Resources */,
-				8306CD261BD3AC3D0025BC6C /* OESidebarController.xib in Resources */,
 				1B32151514DDD49E00C5B50A /* OpenEmu.dae in Resources */,
 				1B32151614DDD49E00C5B50A /* OpenEmuText.dae in Resources */,
 				1BFC26D0116C1CA300B95689 /* OpenEmuHelperApp in Resources */,

OpenEmu/OESidebarController.m
@@ -92,10 +92,6 @@ NSString * const OEMainViewMinWidth = @"mainViewMinWidth";
     }];
 }
 
-- (NSString*)nibName {
-    return @"OESidebarController";
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];

OpenEmu/OESidebarController.xib
@@ -1,16 +1,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<document type="com.apple.InterfaceBuilder3.Cocoa.XIB" version="3.0" toolsVersion="10117" systemVersion="15F34" targetRuntime="MacOSX.Cocoa" propertyAccessControl="none" useAutolayout="YES" customObjectInstantitationMethod="direct">
-    <dependencies>
-        <deployment identifier="macosx"/>
-        <plugIn identifier="com.apple.InterfaceBuilder.CocoaPlugin" version="10117"/>
-    </dependencies>
-    <objects>
-        <customObject id="-2" userLabel="File's Owner"/>
-        <customObject id="-1" userLabel="First Responder" customClass="FirstResponder"/>
-        <customObject id="-3" userLabel="Application" customClass="NSObject"/>
-        <customView id="c22-O7-iKe">
-            <rect key="frame" x="0.0" y="0.0" width="480" height="272"/>
-            <autoresizingMask key="autoresizingMask" flexibleMaxX="YES" flexibleMinY="YES"/>
-        </customView>
-    </objects>
-</document>

commit 44416e5f008c22d1ac4f75bd6b66e0522e2bfbae

Fix a bug introduced in commit 43ad33d9 where the main window resized by itself when the sidebar was dragged to its minimum width.
Enforcement of minimum sidebar panel sizes was done via a custom class, which used delegate methods incompatible with AutoLayout (see https://developer.apple.com/library/archive/releasenotes/AppKit/RN-AppKitOlderNotes/index.html#10_11SplitView)
The custom class was removed and additional autolayout constraints were added to mantain minimum panel size enforcement.
Additionally, custom handling of split panel state saving was replaced with AppKit's builtin autosaving functionality.

OpenEmu/OELibraryController.h
@@ -26,8 +26,6 @@
 
 @import Cocoa;
 
-#import "OELibrarySplitView.h"
-
 @class OELibraryDatabase;
 @class OESidebarController;
 @class OELibrarySplitView;

OpenEmu/OELibraryController.m
@@ -29,7 +29,6 @@
 
 #import "OESidebarItem.h"
 #import "OESidebarController.h"
-#import "OELibrarySplitView.h"
 
 #import "OELibrarySubviewController.h"
 #import "OEROMImporter.h"

OpenEmu/OELibraryGamesViewController.h
@@ -27,6 +27,8 @@
 @import Cocoa;
 #import "OELibrarySubviewController.h"
 
+extern NSNotificationName const OELibrarySplitViewResetSidebarNotification;
+
 @class OESidebarController, OEGameCollectionViewController, OEGameScannerViewController, OELibraryDatabase;
 
 @interface OELibraryGamesViewController : NSViewController <NSToolbarItemValidation>

OpenEmu/OELibraryGamesViewController.m
@@ -28,7 +28,6 @@
 
 #import "OEGameCollectionViewController.h"
 #import "OESidebarController.h"
-#import "OELibrarySplitView.h"
 
 #import "OEDBCollection.h"
 #import "OEDBSystem+CoreDataProperties.h"
@@ -37,14 +36,27 @@
 
 #import "OpenEmu-Swift.h"
 
+#pragma mark - Public constants
+
+NSString * const OESkipDiscGuideMessageKey = @"OESkipDiscGuideMessageKey";
+NSNotificationName const OELibrarySplitViewResetSidebarNotification = @"OELibrarySplitViewResetSidebarNotification";
+
+#pragma mark - Private constants
+
 #define MainMenu_View_GridTag      301
 #define MainMenu_View_ListTag      303
 
-NSString * const OESkipDiscGuideMessageKey = @"OESkipDiscGuideMessageKey";
+static const CGFloat _OESidebarMinWidth  = 105;
+static const CGFloat _OESidebarMaxWidth  = 450;
+static const CGFloat _OEMainViewMinWidth = 495;
+
+#pragma mark - Private class members
 
 @interface OELibraryGamesViewController()<OELibrarySubviewControllerGameSelection>
 @property (nonatomic, readonly, nullable) OELibraryToolbar *toolbar;
 @end
+
+#pragma mark - Implementation
 
 @implementation OELibraryGamesViewController
 @synthesize database = _database;
@@ -79,6 +91,8 @@ NSString * const OESkipDiscGuideMessageKey = @"OESkipDiscGuideMessageKey";
     [self addChildViewController:self.sidebarController];
     [self addChildViewController:self.collectionController];
     [self addChildViewController:self.gameScannerController];
+    
+    [self _setupSplitView];
 }
 
 - (void)viewWillAppear
@@ -112,6 +126,41 @@ NSString * const OESkipDiscGuideMessageKey = @"OESkipDiscGuideMessageKey";
     field.searchMenuTemplate = nil;
     field.enabled = NO;
     field.stringValue = @"";
+}
+
+#pragma mark - Split View
+
+- (void)_setupSplitView
+{
+    [NSUserDefaults.standardUserDefaults registerDefaults:@{
+        @"lastSidebarWidth": @186.0f,
+    }];
+    
+    NSSplitView *librarySplitView = (NSSplitView *)self.view;
+    NSView *sidebar = librarySplitView.arrangedSubviews[0];
+    NSView *gridView = librarySplitView.arrangedSubviews[1];
+    
+    [librarySplitView addConstraints:@[
+        [sidebar.widthAnchor constraintGreaterThanOrEqualToConstant:_OESidebarMinWidth],
+        [sidebar.widthAnchor constraintLessThanOrEqualToConstant:_OESidebarMaxWidth],
+        [gridView.widthAnchor constraintGreaterThanOrEqualToConstant:_OEMainViewMinWidth]
+    ]];
+    
+    if (![NSUserDefaults.standardUserDefaults objectForKey:@"NSSplitView Subview Frames gamesLibrarySplitView"]) {
+        /* read the old property key (OE 2.2.1 and prior) */
+        CGFloat dividerPosition = [NSUserDefaults.standardUserDefaults doubleForKey:@"lastSidebarWidth"];
+        [librarySplitView setPosition:dividerPosition ofDividerAtIndex:0];
+    }
+    librarySplitView.autosaveName = @"libraryGamesSplitView";
+    
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(resetSidebar) name:OELibrarySplitViewResetSidebarNotification object:nil];
+    
+}
+
+- (void)resetSidebar
+{
+    NSSplitView *librarySplitView = (NSSplitView *)self.view;
+    [librarySplitView setPosition:186.0 ofDividerAtIndex:0];
 }
 
 #pragma mark - Validation
@@ -230,6 +279,7 @@ NSString * const OESkipDiscGuideMessageKey = @"OESkipDiscGuideMessageKey";
 }
 
 #pragma mark - Sidebar handling
+
 - (void)_updateCollectionContentsFromSidebar:(id)sender
 {
     id selectedItem = self.sidebarController.selectedSidebarItem;

OpenEmu/OELibraryGamesViewController.xib
@@ -24,9 +24,9 @@
             </connections>
         </viewController>
         <viewController nibName="" id="ZI1-g0-Ovb" customClass="OEGameCollectionViewController"/>
-        <splitView autosaveName="mainSplitView" dividerStyle="thin" vertical="YES" id="swh-F8-vNy" userLabel="Library Split View" customClass="OELibrarySplitView">
+        <splitView autosaveName="mainSplitView" dividerStyle="thin" vertical="YES" id="swh-F8-vNy" userLabel="Library Split View">
             <rect key="frame" x="0.0" y="0.0" width="830" height="600"/>
-            <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
+            <autoresizingMask key="autoresizingMask" flexibleMaxX="YES" flexibleMinY="YES"/>
             <subviews>
                 <visualEffectView fixedFrame="YES" blendingMode="behindWindow" material="sidebar" state="followsWindowActiveState" id="jVR-Tc-HAY">
                     <rect key="frame" x="0.0" y="0.0" width="186" height="600"/>
@@ -172,7 +172,7 @@
                 </customView>
             </subviews>
             <holdingPriorities>
-                <real value="250"/>
+                <real value="251"/>
                 <real value="250"/>
             </holdingPriorities>
             <point key="canvasLocation" x="91" y="106"/>

OpenEmu/OELibrarySplitView.h
@@ -1,41 +1,0 @@
-/*
- Copyright (c) 2012, OpenEmu Team
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-     * Neither the name of the OpenEmu Team nor the
-       names of its contributors may be used to endorse or promote products
-       derived from this software without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-@import Cocoa;
-
-@interface OELibrarySplitView : NSSplitView
-
-- (void)resetSidebar;
-
-/* Return the splitter position as shown on screen.
-   If the sidebar is hidden, return 0.
-   If the sidebar is being toggled, return the current splitter position according to the hide/reveal animation.
-*/
-@property (readonly) CGFloat splitterPosition;
-
-@end
-
-extern NSNotificationName const OELibrarySplitViewResetSidebarNotification;

OpenEmu/OELibrarySplitView.m
@@ -1,245 +1,0 @@
-/*
- Copyright (c) 2012, OpenEmu Team
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-     * Neither the name of the OpenEmu Team nor the
-       names of its contributors may be used to endorse or promote products
-       derived from this software without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#import "OELibrarySplitView.h"
-
-#import "OpenEmu-Swift.h"
-
-#pragma mark - Public variables
-
-NSNotificationName const OELibrarySplitViewResetSidebarNotification = @"OELibrarySplitViewResetSidebarNotification";
-
-#pragma mark - Private variables
-
-static const CGFloat _OESidebarMinWidth  = 105;
-static const CGFloat _OESidebarMaxWidth  = 450;
-static const CGFloat _OEMainViewMinWidth = 495;
-
-static NSString * const _OESidebarWidthKey = @"lastSidebarWidth";
-
-#pragma mark - OELibrarySplitView
-
-@interface OELibrarySplitView () <NSSplitViewDelegate>
-
-@property CGFloat previousWidth;
-
-- (NSView *)OE_sidebarContainerView;
-- (NSView *)OE_mainContainerView;
-- (CGFloat)OE_visibleSidebarSplitterPosition;
-- (void)OE_getSidebarFrame:(NSRect *)sidebarFrame mainFrame:(NSRect *)mainFrame forSplitterAtPosition:(CGFloat)splitterPosition;
-
-@end
-
-@implementation OELibrarySplitView
-
-#pragma mark - Lifecycle
-
-+ (void)initialize
-{
-    if (self != [OELibrarySplitView class])
-        return;
-
-    [[NSUserDefaults standardUserDefaults] registerDefaults:(@{
-                                                             _OESidebarWidthKey   : @186.0f,
-                                                             })];
-}
-
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        [self OE_commonLibrarySplitViewInit];
-    }
-    return self;
-}
-
-- (instancetype)initWithCoder:(NSCoder *)coder
-{
-    self = [super initWithCoder:coder];
-    if (self) {
-        [self OE_commonLibrarySplitViewInit];
-    }
-    return self;
-}
-
-- (instancetype)initWithFrame:(NSRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self OE_commonLibrarySplitViewInit];
-    }
-    return self;
-}
-
-- (void)OE_commonLibrarySplitViewInit {
-    super.delegate = self;
-}
-
-- (void)awakeFromNib
-{
-    [super awakeFromNib];
-
-    NSRect newSidebarFrame, newMainFrame;
-    const CGFloat splitterPosition = [self OE_visibleSidebarSplitterPosition];
-    [self OE_getSidebarFrame:&newSidebarFrame mainFrame:&newMainFrame forSplitterAtPosition:splitterPosition];
-    self.OE_sidebarContainerView.frame = newSidebarFrame;
-    self.OE_mainContainerView.frame = newMainFrame;
-
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(resetSidebar) name:OELibrarySplitViewResetSidebarNotification object:nil];
-
-    [self adjustSubviews];
-}
-
-#pragma mark - Main methods
-
-- (void)OE_getSidebarFrame:(NSRect *)sidebarFrame mainFrame:(NSRect *)mainFrame forSplitterAtPosition:(CGFloat)splitterPosition
-{
-    splitterPosition = MAX(splitterPosition, 0);
-
-    if (sidebarFrame) {
-        *sidebarFrame = NSMakeRect(0.0,
-                                   0.0,
-                                   splitterPosition,
-                                   NSHeight(self.frame));
-    }
-
-    if (mainFrame) {
-        const CGFloat x = splitterPosition;
-        *mainFrame = NSMakeRect(x,
-                                0.0,
-                                NSWidth(self.frame) - x,
-                                NSHeight(self.frame));
-    }
-}
-
-- (CGFloat)OE_visibleSidebarSplitterPosition
-{
-    return [[NSUserDefaults standardUserDefaults] doubleForKey:_OESidebarWidthKey];
-}
-
-- (CGFloat)splitterPosition
-{
-    return NSWidth(self.OE_sidebarContainerView.frame);
-}
-
-- (NSView *)OE_sidebarContainerView
-{
-    return self.subviews.firstObject;
-}
-
-- (NSView *)OE_mainContainerView
-{
-    return (self.subviews.count > 1 ? self.subviews[1] : nil);
-}
-
-- (void)resetSidebar
-{
-    [self setPosition:186.0 ofDividerAtIndex:0];
-}
-
-#pragma mark - Overridden methods
-
-- (void)adjustSubviews
-{
-    const CGFloat currentWidth = NSWidth(self.frame);
-    const CGFloat mainWidth    = NSWidth(self.OE_mainContainerView.frame);
-    const CGFloat sidebarWidth = NSWidth(self.OE_sidebarContainerView.frame);
-
-    CGFloat visualSplitterPosition;
-
-    // We cannot always let NSSplitView adjust the subviews with help from -splitView:shouldAdjustSizeOfSubview:
-    // because of the following scenario:
-    // - sidebar width > _OESidebarMinWidth
-    // - main width > _OEMainViewMinWidth
-    // - user aggressively shrinks the window width in one mouse swipe
-    // When this happens, -splitView:shouldAdjustSizeOfSubview: initially returns YES for the main view since
-    // it can be shrunk, and we prefer not to touch the sidebar width. However, since
-    // -splitView:shouldAdjustSizeOfSubview: is not necessarily sent in a granular enough fashion, it is well
-    /// possible that in the second time that it is sent the window has already been shrunk to a size that
-    // violates _OEMainViewMinWidth. In this case, we force the splitter position to respect _OEMainViewMinWidth
-    
-    const BOOL userIsShrinkingWindow = currentWidth < _previousWidth;
-    const BOOL mainViewCannotBeShrunk = mainWidth <= _OEMainViewMinWidth;
-    const BOOL sidebarCanBeShrunk = sidebarWidth > _OESidebarMinWidth;
-    if (userIsShrinkingWindow && mainViewCannotBeShrunk && sidebarCanBeShrunk) {
-        visualSplitterPosition = currentWidth - _OEMainViewMinWidth;
-    } else {
-        visualSplitterPosition = sidebarWidth;
-    }
-
-    NSRect sidebarFrame, mainFrame;
-    [self OE_getSidebarFrame:&sidebarFrame mainFrame:&mainFrame forSplitterAtPosition:visualSplitterPosition];
-    self.OE_sidebarContainerView.frame = sidebarFrame;
-    self.OE_mainContainerView.frame = mainFrame;
-
-    _previousWidth = NSWidth(self.frame);
-}
-
-- (BOOL)isFlipped
-{
-    return YES;
-}
-
-- (void)viewDidMoveToWindow
-{
-    if (!self.window)
-        return;
-
-    NSView *contentView = self.window.contentView;
-
-    const CGFloat splitViewMinWidth = _OESidebarMinWidth + self.dividerThickness + _OEMainViewMinWidth;
-    const CGFloat leftMargin = [self convertPoint:NSZeroPoint toView:contentView].x;
-    const CGFloat rightMargin = NSMaxX(contentView.frame) - [self convertPoint:NSMakePoint(NSMaxX(self.frame), 0) toView:contentView].x;
-    const CGFloat contentMinWidth   = leftMargin + splitViewMinWidth + rightMargin;
-
-    NSSize contentMinSize = self.window.contentMinSize;
-#ifdef DEBUG_PRINT
-    if (contentMinWidth > contentMinSize.width) {
-        DLog(@"Content mininum width had to be adjusted from %f to %f", contentMinSize.width, contentMinWidth);
-    }
-#endif
-    contentMinSize.width = MAX(contentMinSize.width, contentMinWidth);
-    self.window.contentMinSize = contentMinSize;
-}
-
-#pragma mark - NSSplitViewDelegate protocol methods
-
-- (CGFloat)splitView:(NSSplitView *)splitView constrainMinCoordinate:(CGFloat)proposedMin ofSubviewAt:(NSInteger)dividerIndex
-{
-    return _OESidebarMinWidth;
-}
-
-- (CGFloat)splitView:(NSSplitView *)splitView constrainMaxCoordinate:(CGFloat)proposedMaximumPosition ofSubviewAt:(NSInteger)dividerIndex
-{
-    return MIN(_OESidebarMaxWidth, NSWidth(self.frame) - _OEMainViewMinWidth);
-}
-
-- (void)splitViewDidResizeSubviews:(NSNotification *)notification
-{
-    [NSUserDefaults.standardUserDefaults setDouble:self.splitterPosition forKey:_OESidebarWidthKey];
-}
-
-@end

OpenEmu/OpenEmu.xcodeproj/project.pbxproj
@@ -296,7 +296,6 @@
 		53439AE313B92E59005C0CC8 /* OERatingCell.m in Sources */ = {isa = PBXBuildFile; fileRef = 53439ADA13B92E59005C0CC8 /* OERatingCell.m */; };
 		53439AE613B92E59005C0CC8 /* OETableHeaderView.m in Sources */ = {isa = PBXBuildFile; fileRef = 53439AE013B92E59005C0CC8 /* OETableHeaderView.m */; };
 		53439AE713B92E59005C0CC8 /* OETableView.m in Sources */ = {isa = PBXBuildFile; fileRef = 53439AE213B92E59005C0CC8 /* OETableView.m */; };
-		53439AEB13B92E91005C0CC8 /* OELibrarySplitView.m in Sources */ = {isa = PBXBuildFile; fileRef = 53439AEA13B92E91005C0CC8 /* OELibrarySplitView.m */; };
 		53439B0713B92EEC005C0CC8 /* OEGridViewFieldEditor.m in Sources */ = {isa = PBXBuildFile; fileRef = 53439B0613B92EEC005C0CC8 /* OEGridViewFieldEditor.m */; };
 		53439B6E13B932DB005C0CC8 /* OEPrefLibraryController.m in Sources */ = {isa = PBXBuildFile; fileRef = 53439B6D13B932DB005C0CC8 /* OEPrefLibraryController.m */; };
 		53439B7113B932ED005C0CC8 /* OEPrefGameplayController.m in Sources */ = {isa = PBXBuildFile; fileRef = 53439B7013B932ED005C0CC8 /* OEPrefGameplayController.m */; };
@@ -1736,8 +1735,6 @@
 		53439AE013B92E59005C0CC8 /* OETableHeaderView.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OETableHeaderView.m; sourceTree = "<group>"; };
 		53439AE113B92E59005C0CC8 /* OETableView.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OETableView.h; sourceTree = "<group>"; };
 		53439AE213B92E59005C0CC8 /* OETableView.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OETableView.m; sourceTree = "<group>"; };
-		53439AE913B92E91005C0CC8 /* OELibrarySplitView.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OELibrarySplitView.h; sourceTree = "<group>"; };
-		53439AEA13B92E91005C0CC8 /* OELibrarySplitView.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OELibrarySplitView.m; sourceTree = "<group>"; };
 		53439B0513B92EEC005C0CC8 /* OEGridViewFieldEditor.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OEGridViewFieldEditor.h; sourceTree = "<group>"; };
 		53439B0613B92EEC005C0CC8 /* OEGridViewFieldEditor.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = OEGridViewFieldEditor.m; sourceTree = "<group>"; };
 		53439B6813B932C5005C0CC8 /* OEPreferencePane.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OEPreferencePane.h; sourceTree = "<group>"; };
@@ -3859,8 +3856,6 @@
 			children = (
 				83E8927B197490B3004D4489 /* OEDisabledScrollView.h */,
 				83E8927C197490B3004D4489 /* OEDisabledScrollView.m */,
-				53439AE913B92E91005C0CC8 /* OELibrarySplitView.h */,
-				53439AEA13B92E91005C0CC8 /* OELibrarySplitView.m */,
 			);
 			name = Controls;
 			sourceTree = "<group>";
@@ -7652,7 +7647,6 @@
 				05A002D623DE5389007F0DB7 /* SidebarHeaderView.swift in Sources */,
 				53439AE613B92E59005C0CC8 /* OETableHeaderView.m in Sources */,
 				53439AE713B92E59005C0CC8 /* OETableView.m in Sources */,
-				53439AEB13B92E91005C0CC8 /* OELibrarySplitView.m in Sources */,
 				8F58592F24F4340D009B8502 /* HomebrewOutlinedButton.swift in Sources */,
 				5A9606D21C0A5A570089B494 /* NSManagedObjectContext+LibraryDatabase.swift in Sources */,
 				5AFD06BD2058893B00DFE765 /* GameScannerViewController.swift in Sources */,