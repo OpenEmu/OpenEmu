fd09bbd5210a7079b7f045a61f85cb6a2b2870be
J-rg <j_rg@live.de>
2020-10-24 17:10:30 +0200

    Group grid size slider and in-/decrease buttons

--- OpenEmu/LibraryToolbarDelegate.swift
+++ OpenEmu/LibraryToolbarDelegate.swift
@@ -28,10 +28,8 @@ import Cocoa
 
 extension NSToolbarItem.Identifier {
     public static let oeGridSize = Self("OEToolbarItemIdentifierGridSize")
-    public static let oeDecreaseGridSize = Self("OEToolbarItemIdentifierDecreaseGridSize")
-    public static let oeIncreaseGridSize = Self("OEToolbarItemIdentifierIncreaseGridSize")
     public static let oeViewMode = Self("OEToolbarItemIdentifierViewMode")
-    public static let oeSearch = Self("OEToolbarItemIdentifierSearch")
+    public static let oeSearch   = Self("OEToolbarItemIdentifierSearch")
     public static let oeCategory = Self("OEToolbarItemIdentifierCategory")
 }
 
@@ -45,8 +43,6 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
     func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
         return [.flexibleSpace,
                 .oeGridSize,
-                .oeDecreaseGridSize,
-                .oeIncreaseGridSize,
                 .oeViewMode,
                 .oeSearch,
                 .oeCategory]
@@ -56,9 +52,7 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
     func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
         cacheItems(toolbar: toolbar as! LibraryToolbar)
         return [.oeViewMode,
-                .oeDecreaseGridSize,
                 .oeGridSize,
-                .oeIncreaseGridSize,
                 .oeCategory,
                 .flexibleSpace,
                 .oeSearch]
@@ -72,8 +66,6 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
     
     func cacheItems(toolbar: LibraryToolbar) {
         gridSizeToolbarItem(toolbar: toolbar)
-        decreaseGridSizeToolbarItem(toolbar: toolbar)
-        increaseGridSizeToolbarItem(toolbar: toolbar)
         viewModeToolbarItem(toolbar: toolbar)
         categoryToolbarItem(toolbar: toolbar)
         searchToolbarItem(toolbar: toolbar)
@@ -85,10 +77,6 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
         switch itemIdentifier {
         case .oeGridSize:
             return gridSizeToolbarItem(toolbar: toolbar as! LibraryToolbar)
-        case .oeDecreaseGridSize:
-            return decreaseGridSizeToolbarItem(toolbar: toolbar as! LibraryToolbar)
-        case .oeIncreaseGridSize:
-            return increaseGridSizeToolbarItem(toolbar: toolbar as! LibraryToolbar)
         case .oeViewMode:
             return viewModeToolbarItem(toolbar: toolbar as! LibraryToolbar)
         case .oeCategory:
@@ -106,59 +94,39 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
         if let item = itemCache[.oeGridSize] {
             return item
         }
-        let item = NSToolbarItem(itemIdentifier: .oeGridSize)
         
         let slider = NSSlider(value: 1.0, minValue: 0.5, maxValue: 2.5, target: toolbarOwner, action: #selector(LibraryController.changeGridSize(_:)))
-        slider.isContinuous = true
-        if #available(macOS 11.0, *) {
-            slider.controlSize = .mini
-        } else {
-            slider.controlSize = .small
-        }
+        slider.controlSize = .small
         
         NSLayoutConstraint(item: slider, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 64).isActive = true
         
-        item.view = slider
-        item.label = NSLocalizedString("Grid Size", comment:"Grid size toolbar button label, main window")
+        let decreaseButton = NSButton(image: NSImage(named: "grid_slider_min")!, target: toolbarOwner, action: #selector(LibraryController.decreaseGridSize(_:)))
+        decreaseButton.isBordered = false
+        decreaseButton.toolTip = NSLocalizedString("Decrease Grid Size", comment:"Toolbar, tooltip")
+        
+        let increaseButton = NSButton(image: NSImage(named: "grid_slider_max")!, target: toolbarOwner, action: #selector(LibraryController.increaseGridSize(_:)))
+        increaseButton.isBordered = false
+        increaseButton.toolTip = NSLocalizedString("Increase Grid Size", comment:"Toolbar, tooltip")
+        
         toolbar.gridSizeSlider = slider
-        itemCache[item.itemIdentifier] = item
-        return item;
-    }
-    
-    
-    @discardableResult
-    func decreaseGridSizeToolbarItem(toolbar: LibraryToolbar) -> NSToolbarItem {
-        if let item = itemCache[.oeDecreaseGridSize] {
-            return item
-        }
-        let item = NSToolbarItem(itemIdentifier: .oeDecreaseGridSize)
+        toolbar.decreaseGridSizeButton = decreaseButton
+        toolbar.increaseGridSizeButton = increaseButton
         
-        let minHint = NSButton(image: NSImage(named: "grid_slider_min")!, target: toolbarOwner, action: #selector(LibraryController.decreaseGridSize(_:)))
-        minHint.isBordered = false
+        let item = NSToolbarItem()
+        item.view = slider
         
-        item.view = minHint
-        item.label = NSLocalizedString("Decrease Grid Size", comment:"Grid size toolbar button label, main window")
-        toolbar.decreaseGridSizeButton = minHint
-        itemCache[item.itemIdentifier] = item
-        return item;
-    }
-    
-    
-    @discardableResult
-    func increaseGridSizeToolbarItem(toolbar: LibraryToolbar) -> NSToolbarItem {
-        if let item = itemCache[.oeIncreaseGridSize] {
-            return item
-        }
-        let item = NSToolbarItem(itemIdentifier: .oeIncreaseGridSize)
+        let minHint = NSToolbarItem()
+        minHint.view = decreaseButton
         
-        let maxHint = NSButton(image: NSImage(named: "grid_slider_max")!, target: toolbarOwner, action: #selector(LibraryController.increaseGridSize(_:)))
-        maxHint.isBordered = false
+        let maxHint = NSToolbarItem()
+        maxHint.view = increaseButton
         
-        item.view = maxHint
-        item.label = NSLocalizedString("Increase Grid Size", comment:"Grid size toolbar button label, main window")
-        toolbar.increaseGridSizeButton = maxHint
-        itemCache[item.itemIdentifier] = item
-        return item;
+        let group = NSToolbarItemGroup(itemIdentifier: .oeGridSize)
+        group.subitems = [minHint, item, maxHint]
+        group.label = NSLocalizedString("Grid Size", comment:"Toolbar, grid size slider label")
+        
+        itemCache[group.itemIdentifier] = group
+        return group;
     }
     
     
@@ -167,21 +135,23 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
         if let item = itemCache[.oeViewMode] {
             return item
         }
-        let item = NSToolbarItem(itemIdentifier: .oeViewMode)
         
         let images = [NSImage(named: NSImage.iconViewTemplateName)!,
                       NSImage(named: NSImage.listViewTemplateName)!]
         
         let segmControl = NSSegmentedControl(images: images, trackingMode: .selectOne, target: toolbarOwner, action: #selector(LibraryController.switchToView(_:)))
-        segmControl.setToolTip(NSLocalizedString("Switch To Grid View", comment: "Tooltip"), forSegment: 0)
-        segmControl.setToolTip(NSLocalizedString("Switch To List View", comment: "Tooltip"), forSegment: 1)
+        segmControl.setToolTip(NSLocalizedString("Switch To Grid View", comment: "Toolbar, tooltip"), forSegment: 0)
+        segmControl.setToolTip(NSLocalizedString("Switch To List View", comment: "Toolbar, tooltip"), forSegment: 1)
         segmControl.setWidth(26, forSegment: 0)
         segmControl.setWidth(26, forSegment: 1)
         
+        toolbar.viewModeSelector = segmControl
+        
+        let item = NSToolbarItem(itemIdentifier: .oeViewMode)
         item.view = segmControl
         item.visibilityPriority = .low
-        item.label = NSLocalizedString("View Mode", comment:"View mode toolbar button label, main window")
-        toolbar.viewModeSelector = segmControl
+        item.label = NSLocalizedString("View Mode", comment:"Toolbar, view mode selector label")
+        
         itemCache[item.itemIdentifier] = item
         return item;
     }
@@ -192,7 +162,6 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
         if let item = itemCache[.oeCategory] {
             return item
         }
-        let item = NSToolbarItem(itemIdentifier: .oeCategory)
         
         let titles = [NSLocalizedString("Toolbar: Library", value: "Library", comment: "toolbar, category label"),
                       NSLocalizedString("Toolbar: Save States", value: "Save States", comment: "toolbar, category label"),
@@ -201,57 +170,54 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
         
         let segmControl = NSSegmentedControl(labels: titles, trackingMode: .selectOne, target: toolbarOwner, action: #selector(LibraryController.switchCategory(_:)))
         
-        item.view = segmControl
-        item.label = NSLocalizedString("Category", comment:"Category selector toolbar label, main window")
         toolbar.categorySelector = segmControl
         
-        let libraryMenuItem = NSMenuItem()
-        libraryMenuItem.title = NSLocalizedString("Toolbar: Library", value: "Library", comment: "")
-        libraryMenuItem.target = toolbarOwner
-        libraryMenuItem.tag = 100
-        libraryMenuItem.action = #selector(LibraryController.switchCategoryFromMenu(_:))
-        
-        let savesStatesMenuItem = NSMenuItem()
-        savesStatesMenuItem.title = NSLocalizedString("Toolbar: Save States", value: "Save States", comment: "")
-        savesStatesMenuItem.target = toolbarOwner
-        savesStatesMenuItem.tag = 101
-        savesStatesMenuItem.action = #selector(LibraryController.switchCategoryFromMenu(_:))
-        
-        let screenshotsMenuItem = NSMenuItem()
-        screenshotsMenuItem.title = NSLocalizedString("Toolbar: Screenshots", value: "Screenshots", comment: "")
-        screenshotsMenuItem.target = toolbarOwner
-        screenshotsMenuItem.tag = 102
-        screenshotsMenuItem.action = #selector(LibraryController.switchCategoryFromMenu(_:))
-        
-        let homebrewMenuItem = NSMenuItem()
-        homebrewMenuItem.title = NSLocalizedString("Toolbar: Homebrew", value: "Homebrew", comment: "")
-        homebrewMenuItem.target = toolbarOwner
-        homebrewMenuItem.tag = 103
-        homebrewMenuItem.action = #selector(LibraryController.switchCategoryFromMenu(_:))
-        
-        let menu = NSMenu()
-        menu.addItem(libraryMenuItem)
-        menu.addItem(savesStatesMenuItem)
-        menu.addItem(screenshotsMenuItem)
-        menu.addItem(homebrewMenuItem)
-        
-        let menuFormRepresentation = NSMenuItem()
-        menuFormRepresentation.title = NSLocalizedString("Category", comment:"")
-        menuFormRepresentation.submenu = menu
-        
-        item.menuFormRepresentation = menuFormRepresentation
+        let item = NSToolbarItem(itemIdentifier: .oeCategory)
+        item.view = segmControl
+        item.menuFormRepresentation = categoryMenu
+        item.label = NSLocalizedString("Category", comment:"Toolbar, category selector label")
         
         itemCache[item.itemIdentifier] = item
         return item;
     }
     
+    let categoryMenu: NSMenuItem = {
+        
+        let library = NSMenuItem()
+        library.title = NSLocalizedString("Toolbar: Library", value: "Library", comment: "")
+        library.tag = 100
+        library.action = #selector(LibraryController.switchCategoryFromMenu(_:))
+        
+        let savesStates = NSMenuItem()
+        savesStates.title = NSLocalizedString("Toolbar: Save States", value: "Save States", comment: "")
+        savesStates.tag = 101
+        savesStates.action = #selector(LibraryController.switchCategoryFromMenu(_:))
+        
+        let screenshots = NSMenuItem()
+        screenshots.title = NSLocalizedString("Toolbar: Screenshots", value: "Screenshots", comment: "")
+        screenshots.tag = 102
+        screenshots.action = #selector(LibraryController.switchCategoryFromMenu(_:))
+        
+        let homebrew = NSMenuItem()
+        homebrew.title = NSLocalizedString("Toolbar: Homebrew", value: "Homebrew", comment: "")
+        homebrew.tag = 103
+        homebrew.action = #selector(LibraryController.switchCategoryFromMenu(_:))
+        
+        let menu = NSMenu()
+        menu.items = [library, savesStates, screenshots, homebrew]
+        
+        let categoryMenu = NSMenuItem()
+        categoryMenu.title = NSLocalizedString("Category", comment:"")
+        categoryMenu.submenu = menu
+        
+        return categoryMenu
+    }()
     
     @discardableResult
     func searchToolbarItem(toolbar: LibraryToolbar) -> NSToolbarItem {
         if let item = itemCache[.oeSearch] {
             return item
         }
-        let item = NSToolbarItem(itemIdentifier: .oeSearch)
         
         let searchField = OESearchField()
         searchField.lineBreakMode = .byClipping
@@ -265,11 +231,13 @@ class LibraryToolbarDelegate: NSObject, NSToolbarDelegate {
         
         NSLayoutConstraint(item: searchField, attribute: .width, relatedBy: .lessThanOrEqual, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 166).isActive = true
         
-        item.view = searchField;
-        item.label = NSLocalizedString("Search", comment:"Search field toolbar label, main window")
         toolbar.searchField = searchField
+        
+        let item = NSToolbarItem(itemIdentifier: .oeSearch)
+        item.view = searchField;
+        item.label = NSLocalizedString("Search", comment:"Toolbar, search field label")
+        
         itemCache[item.itemIdentifier] = item
         return item;
     }
 }
-
