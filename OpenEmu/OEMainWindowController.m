OpenEmu/OpenEmu

commit 06facb046500afedec3f6df954357bd80b300e18

Refactor view controller switching in OEMainWindowController to fix a bug where, after the main window is forcibly resized, the title bar overlaps the game view after starting a game until the first resize.
Now we always use a full size content view, and the placeholderView is manually adjusted instead.

OpenEmu/OEMainWindowController.m
@@ -147,7 +147,6 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
         // Adjust visual properties of the window.
         window.titlebarAppearsTransparent = YES;
         window.toolbar.visible = NO;
-        window.styleMask |= NSWindowStyleMaskFullSizeContentView;
         
         [window center];
         
@@ -209,21 +208,10 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
     [newController viewWillAppear];
 
     NSView *viewToReplace = _currentContentController.view;
-    newController.view.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-    newController.view.frame = placeHolderView.bounds;
     
     if(viewToReplace)
-        [placeHolderView replaceSubview:viewToReplace with:newController.view];
-    else
-        [placeHolderView addSubview:newController.view];
-    
-    [self.window makeFirstResponder:newController.view];
-    
+        [viewToReplace removeFromSuperview];
     [self->_currentContentController viewDidDisappear];
-    [newController viewDidAppear];
-    self->_currentContentController = newController;
-    
-    [viewToReplace removeFromSuperview];
     
     if(newController == self->_gameDocument.gameViewController)
     {
@@ -231,10 +219,8 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
         window.toolbar.visible = NO;
         window.titleVisibility = NSWindowTitleVisible;
         
-        // Disable the full size content view window style mask attribute.
-        NSRect windowFrame = window.frame;
-        window.styleMask &= ~NSWindowStyleMaskFullSizeContentView;
-        [window setFrame:windowFrame display:NO];
+        // Change the placeholder view to not overlap the title bar
+        placeHolderView.frame = window.contentLayoutRect;
         
         self->_gameDocument.gameWindowController = self;
     }
@@ -244,15 +230,24 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
         
         if(newController == self.libraryController)
         {
-            // Enable the full size content view window style mask attribute.
-            NSRect windowFrame = window.frame;
-            window.styleMask |= NSWindowStyleMaskFullSizeContentView;
-            [window setFrame:windowFrame display:NO];
-            
             // Adjust visual properties of the window.
             window.toolbar.visible = YES;
+        
+            // Change the placeholder view to overlap the title bar
+            placeHolderView.frame = window.contentView.frame;
         }
     }
+    
+    // Set the size of the new controller *before* it is added to the
+    // view hierarchy. This is important because things like NSSplitter
+    // sizes are loaded as soon as the view is added to a subview.
+    newController.view.frame = placeHolderView.bounds;
+    newController.view.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+    [placeHolderView addSubview:newController.view];
+    [window makeFirstResponder:newController.view];
+    
+    self->_currentContentController = newController;
+    [newController viewDidAppear];
     
     // If a game is playing in the library window, unpause emulation immediately.
     if (newController == _gameDocument.gameViewController) {

commit 7245676cde1d3613e172501b8b363e5639b60b37

Use autolayout to constrain the frame of the "placeholder" view in the main window.
Fixes a bug introduced in 06facb0 where, in non-popout fullscreen, a titlebar-sized strip exposes the window background at the top

OpenEmu/OEMainWindowController.m
@@ -60,6 +60,8 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
     BOOL            _resumePlayingAfterFullScreenTransition;
     
     BOOL _isLaunchingGame;
+    
+    NSLayoutConstraint *_lastPlaceholderViewTopConstraint;
 }
 
 @end
@@ -92,12 +94,29 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
 {
     [super awakeFromNib];
     
+    [self setUpPlaceholderView];
     [self setUpLibraryController];
     [self setUpWindow];
     [self setUpCurrentContentController];
     [self setUpViewMenuItemBindings];
     
     _isLaunchingGame = NO;
+}
+
+- (void)setUpPlaceholderView
+{
+    self.placeholderView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    NSLayoutGuide *windowGuide = self.window.contentLayoutGuide;
+    _lastPlaceholderViewTopConstraint =
+        [windowGuide.topAnchor constraintEqualToAnchor:self.placeholderView.topAnchor];
+        
+    [NSLayoutConstraint activateConstraints:@[
+        [windowGuide.leftAnchor constraintEqualToAnchor:self.placeholderView.leftAnchor],
+        [windowGuide.bottomAnchor constraintEqualToAnchor:self.placeholderView.bottomAnchor],
+        [windowGuide.rightAnchor constraintEqualToAnchor:self.placeholderView.rightAnchor],
+        _lastPlaceholderViewTopConstraint
+    ]];
 }
 
 - (void)setUpLibraryController
@@ -205,6 +224,7 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
     [self->_currentContentController viewDidDisappear];
     
     // Adjust visual properties of the window.
+    
     // HACK: toolbar.allowsUserCustomization=NO prevents re-enabling the toolbar
     // by right clicking on the title bar.
     // The correct fix would be to set window.toolbar to nil, but because the
@@ -214,14 +234,14 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
     // be loaded too late, and it would require basically a rewrite of the
     // toolbar handling code... :/
     // TODO: fix toolbar handling code to remove the hack
+    
+    BOOL overlapsTitleBar = NO;
     if (newController == _gameDocument.gameViewController) {
         window.toolbar.visible = NO;
         window.toolbar.allowsUserCustomization = NO;
         window.titleVisibility = NSWindowTitleVisible;
         window.titlebarAppearsTransparent = NO;
-        
-        // content does not overlap the title bar
-        placeHolderView.frame = window.contentLayoutRect;
+        overlapsTitleBar = NO;
         
     } else if (newController == self.libraryController) {
         window.toolbar.visible = YES;
@@ -229,9 +249,7 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
         window.titleVisibility = NSWindowTitleHidden;
         window.titlebarAppearsTransparent = NO;
         window.styleMask |= NSWindowStyleMaskClosable;
-        
-        // content overlaps the title bar
-        placeHolderView.frame = window.contentView.frame;
+        overlapsTitleBar = YES;
         
     } else if ([newController isKindOfClass:[OESetupAssistant class]]) {
         window.toolbar.visible = NO;
@@ -239,9 +257,7 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
         window.titleVisibility = NSWindowTitleHidden;
         window.titlebarAppearsTransparent = YES;
         window.styleMask &= ~NSWindowStyleMaskClosable;
-        
-        // content overlaps the title bar
-        placeHolderView.frame = window.contentView.frame;
+        overlapsTitleBar = YES;
         
     } else {
         // probably unused
@@ -254,6 +270,14 @@ NSString *const OEDefaultWindowTitle       = @"OpenEmu";
     newController.view.frame = placeHolderView.bounds;
     newController.view.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
     [placeHolderView addSubview:newController.view];
+    
+    NSLayoutGuide *windowGuide = self.window.contentLayoutGuide;
+    _lastPlaceholderViewTopConstraint.active = NO;
+    _lastPlaceholderViewTopConstraint = overlapsTitleBar ?
+        [self.placeholderView.topAnchor constraintEqualToAnchor:self.window.contentView.topAnchor] :
+        [self.placeholderView.topAnchor constraintEqualToAnchor:windowGuide.topAnchor];
+    _lastPlaceholderViewTopConstraint.active = YES;
+
     [window makeFirstResponder:newController.view];
     
     _currentContentController = newController;