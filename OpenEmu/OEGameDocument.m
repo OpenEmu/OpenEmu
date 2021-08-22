OpenEmu/OpenEmu

commit f173419b431efd75058208dad54485a8834f62b0

Port OEGameViewController to Swift

OpenEmu/OEGameDocument.m
@@ -35,7 +35,6 @@
 #import "OEDBGame.h"
 #import "OEDBSaveState.h"
 #import "OEDBSystem.h"
-#import "OEGameViewController.h"
 #import "OELibraryDatabase.h"
 #import "OEPopoutGameWindowController.h"
 #import "OEDownload.h"
@@ -50,7 +49,17 @@
 
 #import "OpenEmu-Swift.h"
 
-NSString * const OEScreenshotAspectRatioCorrectionDisabled = @"disableScreenshotAspectRatioCorrection";
+NSString *const OEGameVolumeKey = @"volume";
+NSString *const OEGameDefaultVideoShaderKey = @"videoShader";
+NSString *const OEGameSystemVideoShaderKeyFormat = @"videoShader.%@";
+NSString *const OEGameCoreDisplayModeKeyFormat = @"displayMode.%@";
+NSString *const OEBackgroundPauseKey = @"backgroundPause";
+NSString *const OEBackgroundControllerPlayKey = @"backgroundControllerPlay";
+NSString *const OETakeNativeScreenshots = @"takeNativeScreenshots";
+
+NSString *const OEScreenshotFileFormatKey = @"screenshotFormat";
+NSString *const OEScreenshotPropertiesKey = @"screenshotProperties";
+NSString *const OEScreenshotAspectRatioCorrectionDisabled = @"disableScreenshotAspectRatioCorrection";
 
 NSString *const OEGameCoreManagerModePreferenceKey = @"OEGameCoreManagerModePreference";
 NSString *const OEGameDocumentErrorDomain = @"OEGameDocumentErrorDomain";
@@ -108,6 +117,17 @@ typedef NS_ENUM(NSUInteger, OEEmulationStatus)
 @end
 
 @implementation OEGameDocument
+
++ (void)initialize
+{
+    if([self class] == [OEGameDocument class])
+    {
+        [[NSUserDefaults standardUserDefaults] registerDefaults:@{
+                                                                  OEScreenshotFileFormatKey : @(NSBitmapImageFileTypePNG),
+                                                                  OEScreenshotPropertiesKey : @{},
+                                                                  }];
+    }
+}
 
 - (id)initWithRom:(OEDBRom *)rom core:(OECorePlugin *)core error:(NSError **)outError
 {

OpenEmu/GameViewController+TouchBar.swift
@@ -53,9 +53,9 @@ private extension NSTouchBarItem.Identifier {
     static let toggleFullScreen = NSTouchBarItem.Identifier("org.openemu.GameViewControllerTouchBar.toggleFullScreen")
 }
 
-extension OEGameViewController {
+extension GameViewController {
     
-    open override func makeTouchBar() -> NSTouchBar? {
+    override func makeTouchBar() -> NSTouchBar? {
         
         let touchBar = NSTouchBar()
         
@@ -77,9 +77,9 @@ extension OEGameViewController {
     }
 }
 
-extension OEGameViewController: NSTouchBarDelegate {
+extension GameViewController: NSTouchBarDelegate {
     
-    public func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
+    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
         
         switch identifier {
             

OpenEmu/GameViewController.swift
@@ -1,0 +1,339 @@
+// Copyright (c) 2020, OpenEmu Team
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the OpenEmu Team nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import Cocoa
+
+// TODO: Messages to remote layer:
+// - Change bounds
+// - Start Syphon
+// - Native screenshot
+//
+// Messages from remote layer:
+// - Default screen size/aspect size - DONE?
+
+@objc(OEGameViewController)
+@objcMembers
+final class GameViewController: NSViewController {
+    
+    /// arbitrary default screen size with 4:3 ratio
+    private let defaultSize = CGSize(width: 400, height: 300)
+    private(set) var defaultScreenSize = CGSize.zero
+    private var aspectSize = OEIntSize()
+    private var screenSize = OEIntSize()
+    
+    private var scaledView: OEScaledGameLayerView!
+    private(set) var gameView: OEGameLayerView!
+    private var notificationView: OEGameLayerNotificationView!
+    
+    var controlsWindow: OEGameControlsBar!
+    weak var document: OEGameDocument!
+    weak var integralScalingDelegate: GameIntegralScalingDelegate?
+    
+    private var shaderControl: ShaderControl!
+    private var shaderWindowController: ShaderParametersWindowController!
+    
+    private var token: NSObjectProtocol?
+    
+    var displayModes: [[String: AnyObject]] = []
+    var discCount = 0
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(document: OEGameDocument) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.document = document
+        
+        defaultScreenSize = defaultSize
+        
+        controlsWindow = OEGameControlsBar(gameViewController: self)
+        controlsWindow.isReleasedWhenClosed = false
+        
+        shaderControl = ShaderControl(systemIdentifier: document.systemIdentifier,
+                                      helper: document.gameCoreHelper)
+        shaderControl.shader = OEShadersModel.shared.shader(forSystem: document.systemIdentifier)
+        shaderWindowController = ShaderParametersWindowController(control: shaderControl)
+        
+        scaledView = OEScaledGameLayerView(frame: NSRect(origin: .zero, size: NSSize(width: 1, height: 1)))
+        view = scaledView
+        
+        gameView = OEGameLayerView(frame: view.bounds)
+        gameView.delegate = self
+        scaledView.contentView = gameView
+        scaledView.setContentViewSizeFill(animated: false)
+        
+        notificationView = OEGameLayerNotificationView(frame: NSRect(x: 0, y: 0, width: 28, height: 28))
+        notificationView.translatesAutoresizingMaskIntoConstraints = false
+        notificationView.cell?.setAccessibilityElement(false)
+        view.addSubview(notificationView)
+        
+        NSLayoutConstraint.activate([
+            notificationView.widthAnchor.constraint(equalToConstant: 28),
+            notificationView.heightAnchor.constraint(equalToConstant: 28),
+            notificationView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            notificationView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10)
+        ])
+        
+        token = NotificationCenter.default.addObserver(forName: NSView.frameDidChangeNotification, object: gameView, queue: .main) { [weak self] _ in
+            guard let self = self else { return }
+            
+            self.controlsWindow.repositionOnGameWindow()
+        }
+    }
+    
+    deinit {
+        gameView.delegate = nil
+        gameView = nil
+        
+        controlsWindow.gameWindow = nil
+        controlsWindow.close()
+        controlsWindow = nil
+        
+        shaderWindowController.close()
+        shaderWindowController = nil
+        
+        if token != nil {
+            NotificationCenter.default.removeObserver(token!)
+            token = nil
+        }
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        guard let window = rootWindow else { return }
+        
+        controlsWindow.gameWindow = window
+        controlsWindow.repositionOnGameWindow()
+        
+        window.makeFirstResponder(gameView)
+    }
+    
+    override func viewWillDisappear() {
+        super.viewWillDisappear()
+        
+        controlsWindow.hide(animated: false)
+        controlsWindow.gameWindow = nil
+        rootWindow?.removeChildWindow(controlsWindow)
+    }
+    
+    private var rootWindow: NSWindow? {
+        var window = gameView.window
+        while window?.parent != nil {
+            window = window?.parent
+        }
+        return window
+    }
+    
+    // MARK: - Game View Control
+    
+    func gameViewSetIntegralSize(_ size: NSSize, animated: Bool) {
+        scaledView.setContentViewSize(size, animated: animated)
+    }
+    
+    func gameViewFillSuperView() {
+        scaledView.setContentViewSizeFill(animated: false)
+    }
+    
+    override func viewDidLayout() {
+        document.setOutputBounds(gameView.bounds)
+    }
+    
+    // MARK: - Controlling Emulation
+    
+    var supportsCheats: Bool {
+        return document.supportsCheats()
+    }
+    
+    var supportsSaveStates: Bool {
+        return document.supportsSaveStates()
+    }
+    
+    var supportsMultipleDiscs: Bool {
+        return document.supportsMultipleDiscs()
+    }
+    
+    var supportsFileInsertion: Bool {
+        return document.supportsFileInsertion()
+    }
+    
+    var supportsDisplayModeChange: Bool {
+        return document.supportsDisplayModeChange()
+    }
+    
+    var coreIdentifier: String {
+        return document.coreIdentifier
+    }
+    
+    var systemIdentifier: String {
+        return document.systemIdentifier
+    }
+    
+    @IBAction func takeScreenshot(_ sender: Any?) {
+        document.takeScreenshot(sender)
+    }
+    
+    func reflectVolume(_ volume: CGFloat) {
+        controlsWindow.reflectVolume(volume)
+    }
+    
+    func reflectEmulationPaused(_ paused: Bool) {
+        controlsWindow.reflectEmulationRunning(!paused)
+    }
+    
+    func toggleControlsVisibility(_ sender: NSMenuItem) {
+        sender.state = sender.state == .off ? .on : .off
+        controlsWindow.canShow = sender.state == .off
+        if sender.state == .on {
+            controlsWindow.hide(animated: false)
+        }
+    }
+    
+    // MARK: - HUD Bar Actions
+    
+    func selectShader(_ sender: NSMenuItem) {
+        
+        let shaderName = sender.title
+        
+        if let shader = OEShadersModel.shared.shader(withName: shaderName) {
+            let params = shader.parameters(forIdentifier: document.systemIdentifier)
+            
+            document.setShaderURL(shader.url, parameters: params as [String: NSNumber]?) { success, error in
+                if success {
+                    self.didLoadShader(shader)
+                }
+                else if let error = error {
+                    let alert = NSAlert(error: error)
+                    alert.runModal()
+                }
+            }
+        }
+        
+        UserDefaults.standard.set(shaderName, forKey: String(format: OEGameSystemVideoShaderKeyFormat, document.systemIdentifier))
+    }
+    
+    func didLoadShader(_ shader: OEShadersModel.OEShaderModel) {
+        shaderControl.shader = shader
+    }
+    
+    func configureShader(_ sender: Any?) {
+        shaderWindowController.showWindow(sender)
+    }
+    
+    // MARK: - OEGameCoreOwner Methods
+    
+    var remoteContextID: CAContextID {
+        set {
+            gameView.remoteContextID = newValue
+        }
+        get {
+            return gameView.remoteContextID
+        }
+    }
+    
+    func setScreenSize(_ newScreenSize: OEIntSize, aspectSize newAspectSize: OEIntSize) {
+        
+        screenSize = newScreenSize
+        aspectSize = newAspectSize
+        // Should never happen
+        if newScreenSize.isEmpty && newAspectSize.isEmpty {
+            defaultScreenSize = defaultSize
+        }
+        else {
+            // Some cores may initially report a 0x0 screenRect on launch, so use aspectSize instead.
+            if newScreenSize.isEmpty {
+                screenSize = aspectSize
+            }
+            let correct = screenSize.corrected(forAspectSize: aspectSize)
+            defaultScreenSize = CGSize(width: Int(correct.width), height: Int(correct.height))
+        }
+        
+        gameView.setScreenSize(screenSize, aspectSize: aspectSize)
+    }
+}
+
+// MARK: - NSMenuItemValidation
+
+extension GameViewController: NSMenuItemValidation {
+    
+    func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        let action = menuItem.action
+        if action == #selector(toggleControlsVisibility(_:)) {
+            if controlsWindow.canShow {
+                menuItem.state = .off
+            } else {
+                menuItem.state = .on
+            }
+        }
+        return true
+    }
+}
+
+// MARK: - OEGameViewDelegate
+
+extension GameViewController: OEGameViewDelegate {
+    
+    func gameView(_ gameView: OEGameLayerView, didReceiveMouseEvent event: OEEvent) {
+        document.didReceiveMouseEvent(event)
+    }
+    
+    func gameView(_ gameView: OEGameLayerView, updateBounds newBounds: CGRect) {
+        document.updateBounds(newBounds)
+    }
+    
+    func gameView(_ gameView: OEGameLayerView, updateBackingScaleFactor newScaleFactor: CGFloat) {
+        document.updateBackingScaleFactor(newScaleFactor)
+    }
+}
+
+// MARK: - Notifications
+
+extension GameViewController {
+    
+    func showQuickSaveNotification() {
+        notificationView.showQuickSave()
+    }
+    
+    func showScreenShotNotification() {
+        notificationView.showScreenShot()
+    }
+    
+    func showFastForwardNotification(_ enable: Bool) {
+        notificationView.showFastForward(enabled: enable)
+    }
+    
+    func showRewindNotification(_ enable: Bool) {
+        notificationView.showRewind(enabled: enable)
+    }
+    
+    func showStepForwardNotification() {
+        notificationView.showStepForward()
+    }
+    
+    func showStepBackwardNotification() {
+        notificationView.showStepBackward()
+    }
+}

OpenEmu/OECollectionViewController.m
@@ -27,7 +27,6 @@
 #import "OECollectionViewController.h"
 #import "OEGameControlsBar.h"
 #import "OEMainWindowController.h"
-#import "OEGameViewController.h"
 #import "OEGameDocument.h"
 
 #import "OEROMImporter.h"

OpenEmu/OEDBDataSourceAdditions.m
@@ -28,7 +28,6 @@
 #import "OEDBImage.h"
 #import "OEDBRom.h"
 #import "OEGameDocument.h"
-#import "OEGameViewController.h"
 #import "NSArray+OEAdditions.h"
 @import Quartz;
 

OpenEmu/OEGameControlsBar.m
@@ -30,7 +30,6 @@
 
 @import OpenEmuKit;
 #import "OEGameDocument.h"
-#import "OEGameViewController.h"
 #import "OEPopoutGameWindowController.h"
 
 #import "OEDBSaveState.h"

OpenEmu/OEGameDocument.h
@@ -25,9 +25,18 @@
  */
 
 @import Cocoa;
-#import "OEGameViewController.h"
 
-extern NSString * const OEScreenshotAspectRatioCorrectionDisabled;
+extern NSString *const OEGameVolumeKey;
+extern NSString *const OEGameDefaultVideoShaderKey;
+extern NSString *const OEGameSystemVideoShaderKeyFormat;
+extern NSString *const OEGameCoreDisplayModeKeyFormat;
+extern NSString *const OEBackgroundPauseKey;
+extern NSString *const OEBackgroundControllerPlayKey;
+extern NSString *const OETakeNativeScreenshots;
+
+extern NSString *const OEScreenshotFileFormatKey;
+extern NSString *const OEScreenshotPropertiesKey;
+extern NSString *const OEScreenshotAspectRatioCorrectionDisabled;
 
 extern NSString *const OEGameCoreManagerModePreferenceKey;
 extern NSString *const OEGameDocumentErrorDomain;
@@ -51,6 +60,7 @@ typedef NS_ERROR_ENUM(OEGameDocumentErrorDomain, OEGameDocumentErrorCodes)
 @class OEGameViewController;
 @class OESystemPlugin;
 @class OEEvent;
+@protocol OEGameCoreHelper;
 
 @interface OEGameDocument : NSDocument
 

OpenEmu/OEGameViewController.h
@@ -1,126 +1,0 @@
-/*
- Copyright (c) 2011, OpenEmu Team
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-     * Neither the name of the OpenEmu Team nor the
-       names of its contributors may be used to endorse or promote products
-       derived from this software without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-@import Cocoa;
-@import OpenEmuKit;
-
-extern NSNotificationName const OEGameViewControllerEmulationWillFinishNotification;
-extern NSNotificationName const OEGameViewControllerEmulationDidFinishNotification;
-extern NSString *const OEGameVolumeKey;
-extern NSString *const OEGameDefaultVideoShaderKey;
-extern NSString *const OEGameSystemVideoShaderKeyFormat;
-extern NSString *const OEGameCoreDisplayModeKeyFormat;
-extern NSString *const OEGameCoresInBackgroundKey;
-extern NSString *const OEGameViewControllerROMKey;
-extern NSString *const OEBackgroundPauseKey;
-extern NSString *const OEBackgroundControllerPlayKey;
-extern NSString *const OEGameViewBackgroundColorKey;
-extern NSString *const OEPopoutGameWindowAlwaysOnTopKey;
-extern NSString *const OEPopoutGameWindowIntegerScalingOnlyKey;
-extern NSString *const OEPopoutGameWindowTreatScaleFactorAsPixels;
-extern NSString *const OETakeNativeScreenshots;
-
-extern NSString *const OEScreenshotFileFormatKey;
-extern NSString *const OEScreenshotPropertiesKey;
-
-
-@class OEDBRom;
-@class OEDBGame;
-@class OEDBSaveState;
-
-@class OEGameLayerView;
-
-@class OEGameControlsBar;
-@class OEGameDocument;
-@class OEDBSaveState;
-
-@class OECorePlugin;
-@class OEGameViewController;
-
-@protocol OEGameIntegralScalingDelegate;
-
-@interface OEGameViewController : NSViewController <NSMenuItemValidation>
-
-#pragma mark -
-
-@property(strong) OEGameControlsBar *controlsWindow;
-
-@property(unsafe_unretained) id<OEGameIntegralScalingDelegate> integralScalingDelegate;
-
-@property(weak) OEGameDocument *document;
-
-@property(readonly) BOOL supportsCheats;
-@property(readonly) BOOL supportsSaveStates;
-@property(readonly) BOOL supportsMultipleDiscs;
-@property(readonly) BOOL supportsFileInsertion;
-@property(readonly) BOOL supportsDisplayModeChange;
-@property(readonly) NSString *coreIdentifier;
-@property(readonly) NSString *systemIdentifier;
-
-#pragma mark - Initialization
-
-- (instancetype)initWithDocument:(OEGameDocument *)document;
-
-#pragma mark - Game View control
-
-@property (nonatomic, readonly) OEGameLayerView *gameView;
-- (void)gameViewSetIntegralSize:(NSSize)size animated:(BOOL)animated;
-- (void)gameViewFillSuperView;
-
-- (void)setScreenSize:(OEIntSize)newScreenSize aspectSize:(OEIntSize)newAspectSize;
-
-- (void)reflectVolume:(float)volume;
-- (void)reflectEmulationPaused:(BOOL)paused;
-
-#pragma mark - HUD Bar Actions
-// switchCore:: expects sender or [sender representedObject] to be an OECorePlugin object and prompts the user for confirmation
-- (void)selectShader:(id)sender;
-- (void)configureShader:(id)sender;
-- (void)toggleControlsVisibility:(id)sender;
-
-#pragma mark - Screenshots support
-- (IBAction)takeScreenshot:(id)sender;
-
-#pragma mark - Info
-@property (readonly) NSSize defaultScreenSize;
-@property (readonly) OEIntSize aspectSize;
-@property (readonly) OEIntSize screenSize;
-@property (nonatomic) NSUInteger discCount;
-@property (nonatomic) NSArray <NSDictionary <NSString *, id> *> *displayModes;
-@property (nonatomic) NSUInteger remoteContextID;
-
-@end
-
-@interface OEGameViewController (Notifications)
-
-- (void)showQuickSaveNotification;
-- (void)showScreenShotNotification;
-- (void)showFastForwardNotification:(BOOL)enable;
-- (void)showRewindNotification:(BOOL)enable;
-- (void)showStepForwardNotification;
-- (void)showStepBackwardNotification;
-
-@end

commit 22ce5d66366952eddc2a39568903f66b177800a7

Fix crash when starting a game and not downloading the core
Starting a game without a core and then opting not to download the appropriate core would still create a GameViewController, which since f173419b caused a crash because document.gameCoreHelper was nil.

OpenEmu/OEGameDocument.m
@@ -303,6 +303,7 @@ typedef NS_ENUM(NSUInteger, OEEmulationStatus)
             }
         }];
         *outError = blockError;
+        return NO;
     }
     
     _gameCoreManager = [self _newGameCoreManagerWithCorePlugin:_corePlugin];