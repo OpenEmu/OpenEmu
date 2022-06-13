OpenEmu/OpenEmu

commit 377acd34ff9c9cf2997334a00cb02cf4457b9b79

Port OEGameInfoHelper to Swift

OpenEmu/GameInfoHelper.swift
@@ -1,0 +1,245 @@
+// Copyright (c) 2021, OpenEmu Team
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the OpenEmu Team nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import Foundation
+import OpenEmuSystem
+
+@objc(OEGameInfoHelper)
+final class GameInfoHelper: NSObject {
+    
+    @objc(sharedHelper)
+    static let shared = GameInfoHelper()
+    
+    var database: OpenVGDB? {
+        return OpenVGDB.shared.isAvailable ? OpenVGDB.shared : nil
+    }
+    
+    @objc
+    func gameInfo(withDictionary gameInfo: [String : Any]) -> [String : Any] {
+        
+        DispatchQueue(label: "org.openemu.OpenEmu.GameInfoHelper").sync {
+            
+            guard let database = database else {
+                return [:]
+            }
+            #if swift(>=5.5)
+            lazy var resultDict: [String : Any] = [:]
+            #else
+            var resultDict: [String : Any] = [:]
+            #endif
+            
+            let systemIdentifier = gameInfo["systemIdentifier"] as! String
+            var header = gameInfo["header"] as? String
+            var serial = gameInfo["serial"] as? String
+            let md5 = gameInfo["md5"] as! String
+            let url = gameInfo["URL"] as! URL
+            let archiveFileIndex = gameInfo["archiveFileIndex"] as? NSNumber
+            
+            var isSystemWithHashlessROM = hashlessROMCheck(forSystem: systemIdentifier)
+            var isSystemWithROMHeader = headerROMCheck(forSystem: systemIdentifier)
+            var isSystemWithROMSerial = serialROMCheck(forSystem: systemIdentifier)
+            var headerSize = sizeOfROMHeader(forSystem: systemIdentifier)
+            
+            let DBMD5Key = "romHashMD5"
+            let DBROMExtensionlessFileNameKey = "romExtensionlessFileName"
+            let DBROMHeaderKey = "romHeader"
+            let DBROMSerialKey = "romSerial"
+            
+            var key: String?
+            var value: String?
+            
+            let determineQueryParams: (() -> Void) = {
+                
+                if value != nil {
+                    return
+                }
+                
+                // check if the system is 'hashless' in the db and instead match by filename (Arcade)
+                if isSystemWithHashlessROM {
+                    key = DBROMExtensionlessFileNameKey
+                    value = (url.lastPathComponent as NSString).deletingPathExtension.lowercased()
+                }
+                // check if the system has headers in the db and instead match by header
+                else if isSystemWithROMHeader {
+                    key = DBROMHeaderKey
+                    value = header?.uppercased()
+                }
+                // check if the system has serials in the db and instead match by serial
+                else if isSystemWithROMSerial {
+                    key = DBROMSerialKey
+                    value = serial?.uppercased()
+                }
+                // if rom has no header we can use the hash we calculated at import
+                else if headerSize == 0 && !md5.isEmpty {
+                    key = DBMD5Key
+                    value = md5.uppercased()
+                }
+            }
+            
+            determineQueryParams()
+            
+            if value == nil {
+                
+                var removeFile = false
+                var romURL: URL
+                if let archiveFileIndex = archiveFileIndex,
+                   let archiveURL = OEArchiveHelper.url(ofExtractedFile: url, archiveFileIndex: archiveFileIndex) {
+                    romURL = archiveURL
+                    removeFile = true
+                } else {
+                    // rom is no archive, use original file URL
+                    romURL = url
+                }
+                
+                var file: OEFile
+                do {
+                    file = try OEFile(url: romURL)
+                } catch {
+                    return [:]
+                }
+                
+                let headerFound = OEDBSystem.header(for: file, forSystem: systemIdentifier)
+                let serialFound = OEDBSystem.serial(for: file, forSystem: systemIdentifier)
+                
+                if headerFound == nil && serialFound == nil {
+                    
+                    var md5: NSString?
+                    try? FileManager.default.hashFile(at: romURL, headerSize: headerSize, md5: &md5)
+                    
+                    if let md5 = md5 as String? {
+                        key = DBMD5Key
+                        value = md5.uppercased()
+                        resultDict["md5"] = value
+                    }
+                }
+                else {
+                    if let headerFound = headerFound {
+                        header = headerFound
+                        resultDict["header"] = headerFound
+                    }
+                    if let serialFound = serialFound {
+                        serial = serialFound
+                        resultDict["serial"] = serialFound
+                    }
+                    
+                    determineQueryParams()
+                }
+                
+                if removeFile {
+                    try? FileManager.default.removeItem(at: romURL)
+                }
+            }
+            
+            if value == nil {
+                // Still nothing to look up, force determineQueryParams to use hashes
+                isSystemWithHashlessROM = false
+                isSystemWithROMHeader = false
+                isSystemWithROMSerial = false
+                headerSize = 0
+                
+                determineQueryParams()
+            }
+            
+            guard let key = key, let value = value else {
+                return [:]
+            }
+            
+            let sql = """
+                SELECT DISTINCT releaseTitleName as 'gameTitle', releaseCoverFront as 'boxImageURL', releaseDescription as 'gameDescription', regionName as 'region'\
+                FROM ROMs rom LEFT JOIN RELEASES release USING (romID) LEFT JOIN REGIONS region on (regionLocalizedID=region.regionID)\
+                WHERE \(key) = '\(value)'
+            """
+            
+            let results = (try? database.executeQuery(sql)) ?? []
+            var result: [String : Any]?
+            
+            if results.count > 1 {
+                
+                // the database holds multiple regions for this rom (probably WORLD rom)
+                // so we pick the preferred region if it's available or just any if not
+                var preferredRegion = OELocalizationHelper.shared.regionName
+                // TODO: Associate regionName's in the database with -[OELocalizationHelper regionName]'s
+                if preferredRegion == "North America" {
+                    preferredRegion = "USA"
+                }
+                
+                for dict in results {
+                    if dict["region"] as? String == preferredRegion {
+                        result = dict
+                        break
+                    }
+                }
+                
+                // preferred region not found, just pick one
+                if result == nil {
+                    result = results.last
+                }
+            }
+            else {
+                result = results.last
+            }
+            
+            if var result = result {
+                // remove the region key so the result can be directly passed to OEDBGame
+                result.removeValue(forKey: "region")
+                resultDict.merge(result) { (_, new) in new }
+            }
+            
+            return resultDict
+        }
+    }
+    
+    func hashlessROMCheck(forSystem system: String) -> Bool {
+        guard let database = database else { return false }
+        
+        let sql = "select systemhashless as 'hashless' from systems where systemoeid = '\(system)'"
+        let result = try? database.executeQuery(sql)
+        return result?.last?["hashless"] as? Int32 == 1
+    }
+    
+    func headerROMCheck(forSystem system: String) -> Bool {
+        guard let database = database else { return false }
+        
+        let sql = "select systemheader as 'header' from systems where systemoeid = '\(system)'"
+        let result = try? database.executeQuery(sql)
+        return result?.last?["header"] as? Int32 == 1
+    }
+    
+    func serialROMCheck(forSystem system: String) -> Bool {
+        guard let database = database else { return false }
+        
+        let sql = "select systemserial as 'serial' from systems where systemoeid = '\(system)'"
+        let result = try? database.executeQuery(sql)
+        // NOTE: As of OpenVGDB 28, the “systemSerial” column is of type “TEXT”.
+        return (result?.last?["serial"] as? String) == "1" || (result?.last?["serial"] as? Int32) == 1
+    }
+    
+    func sizeOfROMHeader(forSystem system: String) -> Int32 {
+        guard let database = database else { return 0 }
+        
+        let sql = "select systemheadersizebytes as 'size' from systems where systemoeid = '\(system)'"
+        let result = try? database.executeQuery(sql)
+        return result?.last?["size"] as? Int32 ?? 0
+    }
+}

OpenEmu/GameScannerViewController.swift
@@ -56,9 +56,9 @@ final class GameScannerViewController: NSViewController {
         super.init(coder: coder)
         
         let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(gameInfoHelperWillUpdate(_:)), name: .OEGameInfoHelperWillUpdate, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(gameInfoHelperDidChangeUpdateProgress(_:)), name: .OEGameInfoHelperDidChangeUpdateProgress, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(gameInfoHelperDidUpdate(_:)), name: .OEGameInfoHelperDidUpdate, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(gameInfoHelperWillUpdate(_:)), name: .GameInfoHelperWillUpdate, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(gameInfoHelperDidChangeUpdateProgress(_:)), name: .GameInfoHelperDidChangeUpdateProgress, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(gameInfoHelperDidUpdate(_:)), name: .GameInfoHelperDidUpdate, object: nil)
         notificationCenter.addObserver(self, selector: #selector(toggleGameScannerView), name: GameScannerViewController.OEGameScannerToggleNotification, object: nil)
     }
     
@@ -97,7 +97,7 @@ final class GameScannerViewController: NSViewController {
         importer.delegate = self
         
         // Show game scanner if importer is running already or game info is downloading.
-        if importer.status == .running || OEGameInfoHelper.shared.isUpdating {
+        if importer.status == .running || OpenVGDB.shared.isUpdating {
             updateProgress()
             showGameScannerView(animated: true)
         }
@@ -182,11 +182,11 @@ final class GameScannerViewController: NSViewController {
         
         headlineLabel.stringValue = NSLocalizedString("Game Scanner", comment: "")
         
-        let infoHelper = OEGameInfoHelper.shared
-        if infoHelper.isUpdating {
+        let helper = OpenVGDB.shared
+        if helper.isUpdating {
             
             progressIndicator.minValue = 0
-            progressIndicator.doubleValue = Double(infoHelper.downloadProgress)
+            progressIndicator.doubleValue = helper.downloadProgress
             progressIndicator.maxValue = 1
             progressIndicator.isIndeterminate = false
             progressIndicator.startAnimation(self)
@@ -511,7 +511,7 @@ extension GameScannerViewController: ROMImporterDelegate {
         DLog("")
         
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
-            if self.importer.totalNumberOfItems == self.importer.numberOfProcessedItems && !OEGameInfoHelper.shared.isUpdating {
+            if self.importer.totalNumberOfItems == self.importer.numberOfProcessedItems && !OpenVGDB.shared.isUpdating {
                 self.hideGameScannerView(animated: true)
             }
         }

OpenEmu/OEArchiveHelper.h
@@ -28,35 +28,9 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-extern NSString * const OEOpenVGDBVersionKey;
-extern NSString * const OEOpenVGDBUpdateCheckKey;
-extern NSString * const OEOpenVGDBUpdateIntervalKey;
+@interface OEArchiveHelper : NSObject
 
-extern NSNotificationName const OEGameInfoHelperWillUpdateNotification;
-extern NSNotificationName const OEGameInfoHelperDidChangeUpdateProgressNotification;
-extern NSNotificationName const OEGameInfoHelperDidUpdateNotification;
-
-@interface OEGameInfoHelper : NSObject
-
-@property(class, readonly) OEGameInfoHelper *sharedHelper;
-
-@property (readonly) CGFloat downloadProgress;
-@property (nullable, copy) NSString *downloadVersion;
-@property (readonly, getter=isUpdating) BOOL updating;
-
-- (NSDictionary * _Nullable)gameInfoWithDictionary:(NSDictionary *)gameInfo;
-
-- (BOOL)hashlessROMCheckForSystem:(NSString *)system;
-- (BOOL)headerROMCheckForSystem:(NSString *)system;
-- (BOOL)serialROMCheckForSystem:(NSString *)system;
-- (int)sizeOfROMHeaderForSystem:(NSString *)system;
-
-// Checks for updates and passes URL of new release and version if any newer DB is found.
-- (void)checkForUpdatesWithHandler:(void (^)(NSURL * _Nullable newURL, NSString * _Nullable newVersion))handler;
-- (void)installVersion:(NSString *)versionTag withDownloadURL:(NSURL *)url;
-- (void)cancelUpdate;
-
-- (id)executeQuery:(NSString *)sql error:(NSError **)error;
++ (nullable NSURL *)urlOfExtractedFile:(NSURL *)url archiveFileIndex:(NSNumber *)archiveFileIndex;
 
 @end
 

OpenEmu/OEArchiveHelper.m
@@ -1,0 +1,102 @@
+/*
+ Copyright (c) 2020, OpenEmu Team
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the OpenEmu Team nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "OEArchiveHelper.h"
+#import <XADMaster/XADArchive.h>
+
+@implementation OEArchiveHelper
+
++ (nullable NSURL *)urlOfExtractedFile:(NSURL *)url archiveFileIndex:(NSNumber *)archiveFileIndex
+{
+    
+    if (!archiveFileIndex) {
+        return nil;
+    }
+
+    NSString *path = url.path;
+
+    if (!path || ![[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        return nil;
+    }
+    
+    XADArchive *archive;
+    @try {
+        archive = [XADArchive archiveForFile:path];
+    } @catch (NSException *exception) {
+        archive = nil;
+    }
+
+    int entryIndex = [archiveFileIndex intValue];
+    if (archive && archive.numberOfEntries > entryIndex) {
+        
+        NSString *formatName = archive.formatName;
+        if ([formatName isEqualToString:@"MacBinary"]) {
+            return nil;
+        }
+
+        if ([formatName isEqualToString:@"LZMA_Alone"]) {
+            return nil;
+        }
+
+        if ([formatName isEqualToString:@"ISO 9660"]) {
+            return nil;
+        }
+
+        if (![archive entryHasSize:entryIndex] || [archive entryIsEncrypted:entryIndex] || [archive entryIsDirectory:entryIndex] || [archive entryIsArchive:entryIndex]) {
+            return nil;
+        }
+
+        NSString *folder = temporaryDirectoryForDecompressionOfPath(path);
+        NSString *name = [archive nameOfEntry:entryIndex];
+        if (name.pathExtension.length == 0 && path.pathExtension.length > 0) {
+            // this won't do. Re-add the archive's extension in case it's .smc or the like
+            name = [name stringByAppendingPathExtension:path.pathExtension];
+        }
+        NSString *tmpPath = [folder stringByAppendingPathComponent:name];
+
+        BOOL isdir;
+        NSURL *tmpURL = [NSURL fileURLWithPath:tmpPath];
+        NSFileManager *fm = [NSFileManager new];
+        if ([fm fileExistsAtPath:tmpPath isDirectory:&isdir] && !isdir) {
+            return tmpURL;
+        }
+
+        BOOL success = YES;
+        @try {
+            success = [archive _extractEntry:entryIndex as:tmpPath deferDirectories:NO dataFork:YES resourceFork:NO];
+        } @catch (NSException *exception) {
+            success = NO;
+        }
+        if (success) {
+            return tmpURL;
+        } else {
+            [fm removeItemAtPath:folder error:nil];
+        }
+    }
+    return nil;
+}
+
+@end

OpenEmu/OEDBGame.m
@@ -32,8 +32,6 @@
 #import "OEDBRom+CoreDataProperties.h"
 #import "OEDBImage.h"
 
-#import "OEGameInfoHelper.h"
-
 #import "NSFileManager+OEHashingAdditions.h"
 
 #import "OpenEmu-Swift.h"

OpenEmu/OEDBSystem.h
@@ -50,8 +50,8 @@ extern NSNotificationName const OEDBSystemAvailabilityDidChangeNotification;
 + (NSArray <OEDBSystem *> *)systemsForFile:(OEFile *)file inContext:(NSManagedObjectContext *)context error:(NSError**)error;
 + (NSArray <OEDBSystem *> * _Nullable)systemsForFileWithURL:(NSURL *)fileURL inContext:(NSManagedObjectContext *)context error:(NSError**)error;
 
-+ (NSString *)headerForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier;
-+ (NSString *)serialForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier;
++ (nullable NSString *)headerForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier;
++ (nullable NSString *)serialForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier;
 
 + (instancetype)systemForPlugin:(OESystemPlugin *)plugin inContext:(NSManagedObjectContext *)context;
 + (instancetype)systemForPluginIdentifier:(NSString *)identifier inContext:(NSManagedObjectContext *)context;

OpenEmu/OEDBSystem.m
@@ -172,7 +172,7 @@ typedef NS_ENUM(NSInteger, OEDBSystemErrorCode) {
     return [self systemsForFile:file inContext:context error:error];
 }
 
-+ (NSString *)headerForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier
++ (nullable NSString *)headerForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier
 {
     OESystemPlugin *systemPlugin = [OESystemPlugin systemPluginForIdentifier:identifier];
     NSString *header = [systemPlugin.controller headerLookupForFile:file];
@@ -180,7 +180,7 @@ typedef NS_ENUM(NSInteger, OEDBSystemErrorCode) {
     return header;
 }
 
-+ (NSString *)serialForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier
++ (nullable NSString *)serialForFile:(__kindof OEFile *)file forSystem:(NSString *)identifier
 {
     OESystemPlugin *systemPlugin = [OESystemPlugin systemPluginForIdentifier:identifier];
     NSString *serial = [systemPlugin.controller serialLookupForFile:file];

commit e3a7b9eb2d9d406d7af7a5ee0818cb5b7f3e2d55

Fix crash when looking for game info for homebrew games
Regression from 377acd3

OpenEmu/GameInfoHelper.swift
@@ -52,8 +52,8 @@ final class GameInfoHelper: NSObject {
             let systemIdentifier = gameInfo["systemIdentifier"] as! String
             var header = gameInfo["header"] as? String
             var serial = gameInfo["serial"] as? String
-            let md5 = gameInfo["md5"] as! String
-            let url = gameInfo["URL"] as! URL
+            let md5 = gameInfo["md5"] as? String
+            let url = gameInfo["URL"] as? URL
             let archiveFileIndex = gameInfo["archiveFileIndex"] as? NSNumber
             
             var isSystemWithHashlessROM = hashlessROMCheck(forSystem: systemIdentifier)
@@ -76,7 +76,8 @@ final class GameInfoHelper: NSObject {
                 }
                 
                 // check if the system is 'hashless' in the db and instead match by filename (Arcade)
-                if isSystemWithHashlessROM {
+                if isSystemWithHashlessROM,
+                   let url = url {
                     key = DBROMExtensionlessFileNameKey
                     value = (url.lastPathComponent as NSString).deletingPathExtension.lowercased()
                 }
@@ -91,7 +92,7 @@ final class GameInfoHelper: NSObject {
                     value = serial?.uppercased()
                 }
                 // if rom has no header we can use the hash we calculated at import
-                else if headerSize == 0 && !md5.isEmpty {
+                else if headerSize == 0, let md5 = md5 {
                     key = DBMD5Key
                     value = md5.uppercased()
                 }
@@ -99,7 +100,8 @@ final class GameInfoHelper: NSObject {
             
             determineQueryParams()
             
-            if value == nil {
+            if value == nil,
+               let url = url {
                 
                 var removeFile = false
                 var romURL: URL