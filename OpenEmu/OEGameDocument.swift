OpenEmu/OpenEmu

commit 63c69eb2efe9949714a25866e56a5a14c9756552

Miscellaneous minor changes to previous Swift ports

OpenEmu/OEGameDocument.swift
@@ -107,7 +107,7 @@ final class OEGameDocument: NSDocument {
                 return 9
             case .couldNotLoadROM:
                 return 10
-            case .gameCoreCrashed(_, _, _):
+            case .gameCoreCrashed:
                 return 11
             case .invalidSaveState:
                 return 12
@@ -217,8 +217,8 @@ final class OEGameDocument: NSDocument {
         }
     }
     
-    convenience init(rom: OEDBRom, core: OECorePlugin?) throws {
-        self.init()
+    init(rom: OEDBRom, core: OECorePlugin?) throws {
+        super.init()
         do {
             try setUpDocument(with: rom, using: core)
         } catch {
@@ -226,8 +226,8 @@ final class OEGameDocument: NSDocument {
         }
     }
     
-    convenience init(game: OEDBGame, core: OECorePlugin?) throws {
-        self.init()
+    init(game: OEDBGame, core: OECorePlugin?) throws {
+        super.init()
         do {
             try setUpDocument(with: game.defaultROM, using: core)
         } catch {
@@ -235,8 +235,8 @@ final class OEGameDocument: NSDocument {
         }
     }
     
-    convenience init(saveState state: OEDBSaveState) throws {
-        self.init()
+    init(saveState state: OEDBSaveState) throws {
+        super.init()
         do {
             try setUpDocument(with: state)
         } catch {
@@ -299,7 +299,7 @@ final class OEGameDocument: NSDocument {
                     
                     if alert.runModal() == .alertFirstButtonReturn || error?.code == NSUserCancelledError {
                         // User canceld
-                        let error = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
+                        let error = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError)
                         throw error
                     }
                     else {
@@ -317,7 +317,7 @@ final class OEGameDocument: NSDocument {
                 }
                 else {
                     // User canceld
-                    let error = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
+                    let error = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError)
                     throw error
                 }
             }
@@ -382,7 +382,7 @@ final class OEGameDocument: NSDocument {
             // including untitled (new) documents.
             var displayName = rom.game?.displayName ?? ""
             #if DEBUG
-            displayName.append(" (DEBUG BUILD)")
+            displayName += " (DEBUG BUILD)"
             #endif
             return displayName
         }
@@ -402,7 +402,7 @@ final class OEGameDocument: NSDocument {
     
     override func data(ofType typeName: String) throws -> Data {
         DLog("\(typeName)")
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr)
     }
     
     override func read(from url: URL, ofType typeName: String) throws {
@@ -652,7 +652,7 @@ final class OEGameDocument: NSDocument {
         let lastDisplayModeInfo = UserDefaults.standard.object(forKey: String(format: OEGameCoreDisplayModeKeyFormat, corePlugin.bundleIdentifier)) as? [String : Any]
         // if file is in an archive append :entryIndex to path, so the core manager can figure out which entry to load
         if let index = rom.archiveFileIndex as? Int {
-            path.append(":\(index)")
+            path += ":\(index)"
         }
         
         // Never extract arcade roms and .md roms (XADMaster identifies some as LZMA archives)
@@ -1592,10 +1592,10 @@ final class OEGameDocument: NSDocument {
         }
         
         if alert.runModal() == .alertFirstButtonReturn {
-            if alert.stringValue == "" {
-                saveState(name: proposedName, completionHandler: nil)
+            if alert.stringValue.isEmpty {
+                saveState(name: proposedName)
             } else {
-                saveState(name: alert.stringValue, completionHandler: nil)
+                saveState(name: alert.stringValue)
             }
         }
         

OpenEmu/BlankSlateSpinnerView.swift
@@ -25,7 +25,7 @@
 import Cocoa
 import QuartzCore
 
-final class BlankSlateSpinnerView: NSView, CALayerDelegate, NSViewLayerContentScaleDelegate {
+final class BlankSlateSpinnerView: NSView, CALayerDelegate {
     
     private var spinnerLayer: CALayer?
     
@@ -33,8 +33,8 @@ final class BlankSlateSpinnerView: NSView, CALayerDelegate, NSViewLayerContentSc
         fatalError("init(coder:) has not been implemented")
     }
     
-    override init(frame: NSRect) {
-        super.init(frame: frame)
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
         wantsLayer = true
     }
     
@@ -57,8 +57,7 @@ final class BlankSlateSpinnerView: NSView, CALayerDelegate, NSViewLayerContentSc
     
     // MARK: - Animation
     
-    private var spinnerAnimation: CAAnimation {
-        
+    private var spinnerAnimation: CAKeyframeAnimation {
         let stepCount = 12
         var spinnerValues = [Double]()
         spinnerValues.reserveCapacity(stepCount)
@@ -69,22 +68,22 @@ final class BlankSlateSpinnerView: NSView, CALayerDelegate, NSViewLayerContentSc
         
         let animation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
         animation.calculationMode = .discrete
-        animation.duration = 1.0
-        animation.repeatCount = .infinity
+        animation.duration = 1
+        animation.repeatCount = .greatestFiniteMagnitude
         animation.isRemovedOnCompletion = false
         animation.values = spinnerValues
         
         return animation
     }
     
-    // MARK: - Layer Delegate
+    // MARK: - CALayer Delegate
     
     func layoutSublayers(of layer: CALayer) {
-        if layer == self.layer, spinnerLayer != nil {
-            spinnerLayer!.frame = layer.bounds
-            spinnerLayer!.frame.size.height = 124
-            spinnerLayer!.frame.origin.y -= (spinnerLayer!.frame.height - layer.frame.height) / 2
-            spinnerLayer!.contents = NSImage(named: "blank_slate_spinner")?.withTintColor(NSColor(named: "blank_slate_box_text")!)
+        if layer == self.layer, let spinnerLayer = spinnerLayer {
+            spinnerLayer.frame = layer.bounds
+            spinnerLayer.frame.size.height = 124
+            spinnerLayer.frame.origin.y -= (spinnerLayer.frame.height - layer.frame.height) / 2
+            spinnerLayer.contents = NSImage(named: "blank_slate_spinner")?.withTintColor(NSColor(named: "blank_slate_box_text")!)
         }
     }
     

OpenEmu/BlankSlateView.swift
@@ -236,11 +236,8 @@ class BlankSlateView: NSView {
         containerView?.addSubview(textView)
         
         // Get core plugins that can handle the system
-        let pluginFilter = NSPredicate(block: { evaluatedPlugin, bindings in
-            return (evaluatedPlugin as! OECorePlugin).systemIdentifiers.contains(plugin.systemIdentifier)
-        })
+        let pluginsForSystem = OECorePlugin.allPlugins.filter { $0.systemIdentifiers.contains(plugin.systemIdentifier) }
         
-        let pluginsForSystem = (OECorePlugin.allPlugins as NSArray).filtered(using: pluginFilter)
         let extraspace = CGFloat(max(0, pluginsForSystem.count - 2))
         
         let coreIconRect = NSRect(x: CoreX,
@@ -269,8 +266,6 @@ class BlankSlateView: NSView {
         
         for (idx, core) in pluginsForSystem.enumerated() {
             
-            guard let core = core as? OECorePlugin else { continue }
-            
             let projectURL = core.infoDictionary["OEProjectURL"] as? String
             let name = core.displayName ?? ""
             
@@ -281,7 +276,7 @@ class BlankSlateView: NSView {
                           height: 20)
             
             if #available(macOS 11.0, *) {
-                frame.origin.x += 2;
+                frame.origin.x += 2
             }
             
             let gotoButton = TextButton(frame: frame)
@@ -421,9 +416,10 @@ extension BlankSlateView: CALayerDelegate {
     
     func layoutSublayers(of layer: CALayer) {
         CATransaction.begin()
+        defer { CATransaction.commit() }
         CATransaction.setDisableActions(true)
         
-        let bounds = self.bounds
+        let bounds = bounds
         let contentLayoutRect = window!.contentLayoutRect
         
         for layer in layer.sublayers ?? [] {
@@ -438,8 +434,6 @@ extension BlankSlateView: CALayerDelegate {
                 layer.frame = bounds
             }
         }
-        
-        CATransaction.commit()
     }
 }
 

OpenEmu/ControllerImageView.swift
@@ -108,9 +108,9 @@ final class ControllerImageView: NSView {
         ringAnimation.delegate = self
         
         animations = [
-            "ringAlpha": ringAnimation,
-            "ringPosition": ringAnimation,
-            "overlayAlpha": overlayAnimation,
+            #keyPath(ringAlpha): ringAnimation,
+            #keyPath(ringPosition): ringAnimation,
+            #keyPath(overlayAlpha): overlayAnimation,
         ]
     }
     

OpenEmu/ControlsButtonSetupView.swift
@@ -87,14 +87,14 @@ final class ControlsButtonSetupView: NSView {
         orderedKeys = parser.orderedKeys
         for (key, button) in keyToButtonMap {
             button.bind(
-                NSBindingName("title"),
+                .title,
                 to: self,
                 withKeyPath: "bindingsProvider.\(key)",
                 options: [.nullPlaceholder: ""]
             )
         }
         
-        setupSubviews()
+        setUpSubviews()
         layoutSubviews()
     }
     
@@ -124,7 +124,7 @@ final class ControlsButtonSetupView: NSView {
     
     private var sectionTitleHeight: CGFloat = 25
     
-    func setupSubviews() {
+    func setUpSubviews() {
         // remove all subviews if any
         subviews.forEach { $0.removeFromSuperview() }
         
@@ -211,7 +211,7 @@ final class ControlsButtonSetupView: NSView {
                     let buttonRect = NSRect(x: width - buttonWidth, y: y - itemHeight, width: buttonWidth - rightGap, height: itemHeight)
                     button.frame = buttonRect.integral
                     
-                    var labelRect = NSIntegralRect(NSRect(x: leftGap, y: buttonRect.origin.y + buttonRect.size.height / 2 + 1, width: width - leftGap - labelButtonSpacing - buttonWidth, height: 100000))
+                    var labelRect = NSRect(x: leftGap, y: buttonRect.origin.y + buttonRect.size.height / 2 + 1, width: width - leftGap - labelButtonSpacing - buttonWidth, height: 100000).integral
                     
                     var labelFitSize = label.cell?.cellSize(forBounds: labelRect) ?? .zero
                     if labelFitSize.height > 30 {
@@ -291,12 +291,12 @@ final class ControlsButtonSetupView: NSView {
                 minY = visibleSectionRect.maxY
                 
                 if minY >= visibleRect.maxY - sectionTitleHeight - 1 {
-                    sectionHeader.pinned = true
+                    sectionHeader.isPinned = true
                 } else {
-                    sectionHeader.pinned = false
+                    sectionHeader.isPinned = false
                 }
             } else {
-                sectionHeader.pinned = false
+                sectionHeader.isPinned = false
             }
             i -= 1
         }

OpenEmu/ControlsKeyButton.swift
@@ -78,7 +78,7 @@ final class ControlsKeyButton: NSButton {
     }
     
     override func accessibilityTitle() -> String? {
-        guard title == "" else {
+        guard title.isEmpty else {
             return super.accessibilityTitle()
         }
         let announcement = NSLocalizedString("Not assigned", comment: "Controls preferences, accessibility title (key is not mapped, “empty”)")

OpenEmu/ControlsKeySeparatorView.swift
@@ -29,10 +29,6 @@ final class ControlsKeySeparatorView: NSView {
     private let topColor = NSColor(red: 85/255, green: 45/255, blue: 0, alpha: 1)
     private let bottomColor = NSColor(red: 1, green: 1, blue: 0, alpha: 0.2)
     
-    override var isOpaque: Bool {
-        return false
-    }
-    
     override func draw(_ dirtyRect: NSRect) {
         
         var lineRect = bounds

OpenEmu/ControlsLabels.swift
@@ -53,10 +53,10 @@ class ControlsLabel: NSTextField {
             font = .systemFont(ofSize: 11)
         }
         
-        setupAttributes()
+        setUpAttributes()
     }
     
-    func setupAttributes() {
+    func setUpAttributes() {
     }
     
     override func draw(_ dirtyRect: NSRect) {
@@ -73,7 +73,7 @@ class ControlsLabel: NSTextField {
 
 final class ControlsKeyHeadline: ControlsLabel {
     
-    override func setupAttributes() {
+    override func setUpAttributes() {
         if isWood {
             font = .boldSystemFont(ofSize: 11.5)
         } else {
@@ -84,7 +84,7 @@ final class ControlsKeyHeadline: ControlsLabel {
 
 final class ControlsKeyLabel: ControlsLabel {
     
-    override func setupAttributes() {
+    override func setUpAttributes() {
         alignment = .right
     }
 }

commit 68e8d6fcfa9905988a45d78079b4f21527f12dd6

Fix crash when opening games from Finder
‘Fatal error: Use of unimplemented initializer 'init()' for class 'OpenEmu.OEGameDocument'’
Regression from 63c69eb

OpenEmu/OEGameDocument.swift
@@ -217,6 +217,12 @@ final class OEGameDocument: NSDocument {
         }
     }
     
+    // make sure we inherit init() from the superclass,
+    // it is called by NSDocument.init(contentsOf:ofType:)
+    override init() {
+        super.init()
+    }
+    
     init(rom: OEDBRom, core: OECorePlugin?) throws {
         super.init()
         do {