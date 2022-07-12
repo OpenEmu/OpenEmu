OpenEmu/OpenEmu

commit 62092ebc2bd5f1602e464afddc1e646d266ed278

Update OpenEmuKit

OpenEmu/AppDelegate.swift
@@ -365,8 +365,8 @@ class AppDelegate: NSObject {
     }
     
     fileprivate func loadPlugins() {
-        OEPlugin.registerPluginClass(OECorePlugin.self)
-        OEPlugin.registerPluginClass(OESystemPlugin.self)
+        OECorePlugin.registerClass()
+        OESystemPlugin.registerClass()
         
         // Register all system controllers with the bindings controller.
         for plugin in OESystemPlugin.allPlugins {
@@ -800,7 +800,7 @@ extension AppDelegate: NSMenuDelegate {
         
         notificationCenter.addObserver(self, selector: #selector(didRepairBindings), name: .OEBindingsRepaired, object: nil)
         
-        notificationCenter.addObserver(self, selector: #selector(didRegisterSystemPlugin), name: .OESystemPluginDidRegister, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(didRegisterSystemPlugin), name: OESystemPlugin.didRegisterNotification, object: nil)
         
         NSDocumentController.shared.clearRecentDocuments(nil)
         

OpenEmu/AvailableLibrariesViewController.swift
@@ -23,7 +23,7 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Cocoa
-import OpenEmuKit.OECorePlugin
+import OpenEmuKit
 
 private extension NSUserInterfaceItemIdentifier {
     static let availableLibrariesCollectionViewItem = NSUserInterfaceItemIdentifier("AvailableLibrariesCollectionViewItem")
@@ -40,7 +40,7 @@ final class AvailableLibrariesViewController: NSViewController {
     var isEnableObservers: Bool = false {
         didSet {
             if isEnableObservers {
-                OEPlugin.addObserver(self, forKeyPath: #keyPath(OEPlugin.allPlugins), options: [.old, .new], context: nil)
+                OESystemPlugin.addObserver(self, forKeyPath: #keyPath(OESystemPlugin.allPlugins), options: [.old, .new], context: nil)
                 observerToken = NotificationCenter.default
                     .addObserver(forName: .OEDBSystemAvailabilityDidChange, object: nil, queue: .main) { [weak self] _ in
                         guard let self = self else { return }
@@ -48,7 +48,7 @@ final class AvailableLibrariesViewController: NSViewController {
                         self.loadData()
                 }
             } else {
-                OEPlugin.removeObserver(self, forKeyPath: #keyPath(OEPlugin.allPlugins))
+                OESystemPlugin.removeObserver(self, forKeyPath: #keyPath(OESystemPlugin.allPlugins))
                 observerToken = nil
             }
         }

OpenEmu/BIOSFile.swift
@@ -25,7 +25,7 @@
  */
 
 import Foundation
-import OpenEmuKit.OECorePlugin
+import OpenEmuKit
 
 extension Notification.Name {
     /// Posted whenever a new BIOS file was successfully imported.

OpenEmu/BlankSlateView.swift
@@ -23,8 +23,7 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Cocoa
-import OpenEmuKit.OECorePlugin
-import OpenEmuKit.OESystemPlugin
+import OpenEmuKit
 
 @objc(OEBlankSlateView)
 class BlankSlateView: NSView {

OpenEmu/CoreUpdater.swift
@@ -83,7 +83,7 @@ final class CoreUpdater: NSObject {
         }
     }
     
-    private func checkForUpdateInformation(url: URL, plugin: OEPlugin) throws -> CoreAppcastItem? {
+    private func checkForUpdateInformation(url: URL, plugin: OECorePlugin) throws -> CoreAppcastItem? {
         let items: [XMLElement]
         do {
             let appcast = try XMLDocument(contentsOf: url, options: [])

OpenEmu/MainWindowController.swift
@@ -23,7 +23,7 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Cocoa
-import OpenEmuKit.OECorePlugin
+import OpenEmuKit
 
 let OEForcePopoutGameWindowKey = "forcePopout"
 let OEFullScreenGameWindowKey = "fullScreen"

OpenEmu/OEDBSaveState.swift
@@ -23,7 +23,7 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Cocoa
-import OpenEmuKit.OECorePlugin
+import OpenEmuKit
 
 @objc extension OEDBSaveState {
     

OpenEmu/OEDBSystem.swift
@@ -24,7 +24,7 @@
 
 import Cocoa
 import OpenEmuSystem
-import OpenEmuKit.OESystemPlugin
+import OpenEmuKit
 
 extension Notification.Name {
     static let OEDBSystemAvailabilityDidChange = Notification.Name("OEDBSystemAvailabilityDidChangeNotification")

commit 3dd3440b82213ef16507b6804aef4dfd1989aba4

Fix missing system icons in sidebar
The main window was restored before the system plugins were loaded because it received the ‘libraryDidLoad’ notification earlier.
Regression from 62092ebc2

OpenEmu/AppDelegate.swift
@@ -210,6 +210,7 @@ class AppDelegate: NSObject {
             assert(OELibraryDatabase.default != nil, "No database available!")
             
             DispatchQueue.main.async {
+                self.libraryDatabaseDidLoad()
                 NotificationCenter.default.post(name: .libraryDidLoad, object: OELibraryDatabase.default!)
             }
             
@@ -793,7 +794,6 @@ extension AppDelegate: NSMenuDelegate {
         
         let notificationCenter = NotificationCenter.default
         
-        notificationCenter.addObserver(self, selector: #selector(libraryDatabaseDidLoad), name: .libraryDidLoad, object: nil)
         notificationCenter.addObserver(self, selector: #selector(openPreferencePane), name: PreferencesWindowController.openPaneNotificationName, object: nil)
         
         notificationCenter.addObserver(self, selector: #selector(didRepairBindings), name: .OEBindingsRepaired, object: nil)
@@ -822,7 +822,7 @@ extension AppDelegate: NSMenuDelegate {
         }
     }
     
-    func libraryDatabaseDidLoad(notification: Notification) {
+    func libraryDatabaseDidLoad() {
         
         libraryLoaded = true
 