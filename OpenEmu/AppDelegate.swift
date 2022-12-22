OpenEmu/OpenEmu

commit 877bc28a932d31d4f46ed3279c496adefd2dd1b4

Port OEROMImporter to Swift

OpenEmu/AppDelegate.swift
@@ -128,6 +128,7 @@ class AppDelegate: NSObject {
         UserDefaults.standard.register(defaults: [
             OEDefaultDatabasePathKey: path,
             OEDatabasePathKey: path,
+            OECopyToLibraryKey: true,
             OEAutomaticallyGetInfoKey: true,
             OEShadersModel.Preferences.global.key: "Pixellate",
             OEGameVolumeKey: 0.5,
@@ -902,7 +903,7 @@ extension AppDelegate: NSMenuDelegate {
         return false
     }
     
-    func application(_ sender: NSApplication, openFiles filenames: [String]) {
+    func application(_ application: NSApplication, open urls: [URL]) {
         
         guard UserDefaults.standard.bool(forKey: SetupAssistant.hasFinishedKey) else {
             NSApp.reply(toOpenOrPrint: .cancel)
@@ -911,9 +912,9 @@ extension AppDelegate: NSMenuDelegate {
         
         let block: StartupQueueClosure = {
             
-            if filenames.count == 1 {
+            if urls.count == 1 {
                 
-                let url = URL(fileURLWithPath: filenames.last!)
+                let url = urls.first!
                 self.documentController.openDocument(withContentsOf: url, display: true) { (document, documentWasAlreadyOpen, error) in
                     if error != nil {
                         self.documentController.presentError(error!)
@@ -925,7 +926,7 @@ extension AppDelegate: NSMenuDelegate {
                 
                 let reply: NSApplication.DelegateReply
                 let importer = OELibraryDatabase.default!.importer
-                if importer.importItems(atPaths: filenames) {
+                if importer.importItems(at: urls) {
                     reply = .success
                 } else {
                     reply = .failure

OpenEmu/GameScannerViewController.swift
@@ -49,7 +49,7 @@ final class GameScannerViewController: NSViewController {
     private var isScanningDirectory = false
     private var isGameScannerVisible = true // The game scanner view is already visible in SidebarController.xib.
     
-    private var importer: OEROMImporter { return OELibraryDatabase.default!.importer }
+    private var importer: ROMImporter { return OELibraryDatabase.default!.importer }
     
     required init?(coder: NSCoder) {
         
@@ -480,9 +480,9 @@ extension GameScannerViewController: NSTableViewDelegate {
 
 // MARK: - OEROMImporterDelegate
 
-extension GameScannerViewController: OEROMImporterDelegate {
+extension GameScannerViewController: ROMImporterDelegate {
     
-    func romImporterDidStart(_ importer: OEROMImporter) {
+    func romImporterDidStart(_ importer: ROMImporter) {
         
         DLog("")
         
@@ -496,17 +496,17 @@ extension GameScannerViewController: OEROMImporterDelegate {
         }
     }
     
-    func romImporterDidCancel(_ importer: OEROMImporter) {
+    func romImporterDidCancel(_ importer: ROMImporter) {
         DLog("")
         updateProgress()
     }
     
-    func romImporterDidPause(_ importer: OEROMImporter) {
+    func romImporterDidPause(_ importer: ROMImporter) {
         DLog("")
         updateProgress()
     }
     
-    func romImporterDidFinish(_ importer: OEROMImporter) {
+    func romImporterDidFinish(_ importer: ROMImporter) {
         
         DLog("")
         
@@ -523,13 +523,13 @@ extension GameScannerViewController: OEROMImporterDelegate {
         }
     }
     
-    func romImporterChangedItemCount(_ importer: OEROMImporter) {
+    func romImporterChangedItemCount(_ importer: ROMImporter) {
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200)) {
             self.updateProgress()
         }
     }
     
-    func romImporter(_ importer: OEROMImporter, stoppedProcessingItem item: OEImportOperation) {
+    func romImporter(_ importer: ROMImporter, stoppedProcessingItem item: OEImportOperation) {
 
         if let error = item.error {
             

OpenEmu/ImageCollectionViewController.swift
@@ -507,7 +507,7 @@ extension ImageCollectionViewController: NSCollectionViewDelegate {
         if pboard.canReadObject(forClasses: [NSURL.self], options: nil) {
             guard let files = pboard.readObjects(forClasses: [NSURL.self], options: nil) as? [URL] else { return false }
             let romImporter = database.importer
-            romImporter.importItems(at: files, intoCollectionWith: nil)
+            romImporter.importItems(at: files)
             return true
         }
         return false
@@ -552,7 +552,7 @@ extension ImageCollectionViewController: BlankSlateViewDelegate {
         if pboard.canReadObject(forClasses: [NSURL.self], options: nil) {
             guard let files = pboard.readObjects(forClasses: [NSURL.self], options: nil) as? [URL] else { return false }
             let romImporter = database.importer
-            romImporter.importItems(at: files, intoCollectionWith: nil)
+            romImporter.importItems(at: files)
             return true
         }
         return false

OpenEmu/LibraryController.swift
@@ -381,7 +381,7 @@ final class LibraryController: NSTabViewController, NSMenuItemValidation {
         
         openPanel.beginSheetModal(for: win) { result in
             if result == .OK {
-                self.database.importer.importItems(atURLs: openPanel.urls)
+                self.database.importer.importItems(at: openPanel.urls)
             }
         }
     }

OpenEmu/OECollectionViewController.m
@@ -26,8 +26,6 @@
 
 #import "OECollectionViewController.h"
 
-#import "OEROMImporter.h"
-
 #import "OEListViewDataSourceItem.h"
 
 #import "OECoverGridDataSourceItem.h"
@@ -439,7 +437,7 @@ static void *OEUserDefaultsDisplayGameTitleKVOContext = &OEUserDefaultsDisplayGa
     NSArray<NSURL*> *files = [pboard readObjectsForClasses:@[[NSURL class]] options:@{NSPasteboardURLReadingFileURLsOnlyKey: @YES}];
     OEROMImporter *romImporter = self.database.importer;
     OEDBCollection *collection = [[self representedObject] isKindOfClass:[OEDBCollection class]] ? (OEDBCollection *)[self representedObject] : nil;
-    [romImporter importItemsAtURLs:files intoCollectionWithID:[collection permanentID]];
+    [romImporter importItemsAtURLs:files intoCollectionWithID:[collection permanentID] withCompletionHandler:nil];
 
     return YES;
 }

OpenEmu/OEGameCollectionViewController.m
@@ -35,8 +35,6 @@
 #import "OEDBSystem.h"
 #import "OEDBSaveState.h"
 
-#import "OEROMImporter.h"
-
 #import "OpenEmu-Swift.h"
 
 NSNotificationName const OEGameCollectionViewControllerDidSetSelectionIndexesNotification = @"OEGameCollectionViewControllerDidSetSelectionIndexesNotification";
@@ -875,7 +873,7 @@ static NSString * const OEGameTableSortDescriptorsKey = @"OEGameTableSortDescrip
         NSArray *files = [draggingPasteboard readObjectsForClasses:@[[NSURL class]] options:@{NSPasteboardURLReadingFileURLsOnlyKey: @YES}];
         OEROMImporter *romImporter = self.database.importer;
         OEDBCollection *collection = [representedObject isKindOfClass:[OEDBCollection class]] ? (OEDBCollection *)representedObject : nil;
-        [romImporter importItemsAtURLs:files intoCollectionWithID:[collection permanentID]];
+        [romImporter importItemsAtURLs:files intoCollectionWithID:[collection permanentID] withCompletionHandler:nil];
     }
     else if (draggingOperation == IKImageBrowserDropNone)
     {
@@ -1025,7 +1023,7 @@ static NSString * const OEGameTableSortDescriptorsKey = @"OEGameTableSortDescrip
     NSArray *files = [pboard readObjectsForClasses:@[[NSURL class]] options:@{NSPasteboardURLReadingFileURLsOnlyKey: @YES}];
     OEROMImporter *romImporter = self.database.importer;
     OEDBCollection *collection = [[self representedObject] isKindOfClass:[OEDBCollection class]] ? (OEDBCollection*)[self representedObject] : nil;
-    [romImporter importItemsAtURLs:files intoCollectionWithID:[collection permanentID]];
+    [romImporter importItemsAtURLs:files intoCollectionWithID:[collection permanentID] withCompletionHandler:nil];
 
     return YES;
 }

OpenEmu/OEImportOperation.h
@@ -30,6 +30,28 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class OEROMImporter;
 
+#pragma mark - User Default Keys
+extern NSString *const OECopyToLibraryKey;
+extern NSString *const OEAutomaticallyGetInfoKey;
+
+#pragma mark - Error Codes
+extern NSString *const OEImportErrorDomainFatal;
+extern NSString *const OEImportErrorDomainResolvable;
+extern NSString *const OEImportErrorDomainSuccess;
+
+typedef NS_ENUM(NSInteger, OEImportErrorCode) {
+    OEImportErrorCodeAlreadyInDatabase     = -1,
+    OEImportErrorCodeMultipleSystems       = 2,
+    OEImportErrorCodeNoSystem              = 3,
+    OEImportErrorCodeInvalidFile           = 4,
+    OEImportErrorCodeAdditionalFiles       = 5,
+    OEImportErrorCodeNoHash                = 6,
+    OEImportErrorCodeNoGame                = 7,
+    OEImportErrorCodeDisallowArchivedFile  = 8,
+    OEImportErrorCodeEmptyFile             = 9,
+    OEImportErrorCodeAlreadyInDatabaseFileUnreachable = 10,
+};
+
 typedef NS_ENUM(NSInteger, OEImportExitStatus)  {
     OEImportExitNone,
     OEImportExitErrorResolvable,

OpenEmu/OEImportOperation.m
@@ -27,7 +27,6 @@
 #import "OEImportOperation.h"
 @import OpenEmuKit;
 
-#import "OEROMImporter.h"
 #import <XADMaster/XADArchive.h>
 
 #import "NSFileManager+OEHashingAdditions.h"
@@ -43,6 +42,15 @@
 #import "OpenEmu-Swift.h"
 
 NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark - User Default Keys
+NSString *const OECopyToLibraryKey         = @"copyToLibrary";
+NSString *const OEAutomaticallyGetInfoKey  = @"automaticallyGetInfo";
+
+#pragma mark - Error Codes
+NSString *const OEImportErrorDomainFatal      = @"OEImportFatalDomain";
+NSString *const OEImportErrorDomainResolvable = @"OEImportResolvableDomain";
+NSString *const OEImportErrorDomainSuccess    = @"OEImportSuccessDomain";
 
 NSString * const OEImportManualSystems = @"OEImportManualSystems";
 

commit bd9c3d81d521288d9c20076421b433c4c3fdc41e

Fix opening games from Finder
Regression from commit 877bc28.

OpenEmu/AppDelegate.swift
@@ -897,7 +897,11 @@ extension AppDelegate: NSMenuDelegate {
         return false
     }
     
-    func application(_ application: NSApplication, open urls: [URL]) {
+    // NOTE: When using ‘application:openURLs:’, “Document URLs that have an associated NSDocument class
+    // will be opened through NSDocumentController.”, thereby bypassing the startupQueue.
+    // FIXME: Handle ´PluginDocument´s
+    func application(_ sender: NSApplication, openFiles filenames: [String]) {
+        let urls = filenames.compactMap { URL(fileURLWithPath: $0) }
         
         guard UserDefaults.standard.bool(forKey: SetupAssistant.hasFinishedKey) else {
             NSApp.reply(toOpenOrPrint: .cancel)

OpenEmu/OEGameDocument.swift
@@ -429,7 +429,6 @@ final class OEGameDocument: NSDocument {
         DLog("\(typeName)")
         
         guard let libraryDB = OELibraryDatabase.default else {
-            // FIXME: If a file is opened from Finder or dragged on the dock icon while OpenEmu is not already running, we reach this point before the library database is available and opening the file fails (regression from a commit after 70ac442/2.3.3)
             throw Errors.libraryDatabaseUnavailable
         }
         