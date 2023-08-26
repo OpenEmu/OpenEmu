20b8541227539f78981298291d9a76ebd6440c01
J-rg <4042863+J-rg@users.noreply.github.com>
2023-08-26 14:42:24 +0200

    Fix system name in default shader preset name

--- OpenEmu/ShaderControl.swift
+++ OpenEmu/ShaderControl.swift
@@ -177,7 +177,7 @@ public class ShaderControl: NSObject {
     }
     
     static func presetForSystemShader(_ systemShader: OESystemShaderModel, _ systemPlugin: OESystemPlugin) -> ShaderPreset {
-        ShaderPreset(name: "Default \(systemShader.shader.name) preset for \(systemPlugin.displayName)",
+        ShaderPreset(name: "Default \(systemShader.shader.name) preset for \(systemPlugin.systemName)",
                      systemShader: systemShader)
     }
 }
