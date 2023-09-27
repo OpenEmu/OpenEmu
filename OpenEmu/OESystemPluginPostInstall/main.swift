OpenEmu/OpenEmu

commit 6b08dcba217ea05343e78619bf35e32c1f1c3a41

Update OESystemPluginPostInstall
*Add fr-CA localization so Finder displays North American system names instead of European ones in francophone Canada.
*Move "%@ Game" to InfoPlist.strings and remove it from the final build.
*Set LSHandlerRank of game files from Owner to Default to avoid snatching files from other emulators.
*Set LSHandlerRank to Alternate for archives and ISO files as None hides OpenEmu from “Open with” menus. DiskImageMounter, Archive Utility and the most common unarchiver apps have a default rank for those, so OpenEmu won’t take them over.
*Remove CFBundleTypeOSTypes key as type codes are long deprecated.

OpenEmu/OESystemPluginPostInstall/main.swift
@@ -31,21 +31,25 @@ import Foundation
 // IMPORTANT: to be updated when adding a language to OpenEmu
 let regionsToLanguages = [
     "eu": ["ca", "de", "en-GB", "es", "fr", "it", "nl", "pt", "ru"],
-    "na": ["en"],
+    "na": ["en", "fr-CA"],
     "jp": ["ja", "zh-Hans", "zh-Hant"]
 ]
 
 // Extensions we don't want to include in the Info.plist because they are
 // already included in it
 let extensionBlacklist = [
-    "tar.gz", "tar", "gz", "zip", "7z", "rar"
+    "tgz", "tar", "gz", "zip", "7z", "rar"
 ]
 
 // Extensions we want to include with LSHandlerRank set to None
-let extensionGrayList = [
-    "cue", "m3u", "iso"
+let extensionGrayListNone = [
+    "cue", "m3u"
 ]
 
+// Extensions we want to include with LSHandlerRank set to Alternate
+let extensionGrayListAlternate = [
+    "iso"
+]
 
 func regionalizedSystemName(plugin: Bundle, languageCode lcode: String) -> String?
 {
@@ -65,7 +69,7 @@ func regionalizedSystemName(plugin: Bundle, languageCode lcode: String) -> Strin
 func toGameFileName(systemName name: String, appBundle app: Bundle, localization loc: String) -> String
 {
     let locBundle = Bundle.init(url: app.url(forResource: loc, withExtension: "lproj")!)!
-    let format = locBundle.localizedString(forKey: "%@ Game", value: "%@ Game", table: nil)
+    let format = locBundle.localizedString(forKey: "%@ Game", value: "%@ Game", table: "InfoPlist")
     return String.init(format: format, name)
 }
 
@@ -81,7 +85,8 @@ func readLocalizedInfoPlistStrings(appBundle: Bundle) -> [String: [String: Strin
         let plist = appBundle.resourceURL!.appendingPathComponent(localization + ".lproj/InfoPlist.strings")
         do {
             let data = try Data.init(contentsOf: plist)
-            let strings = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as! [String: String]
+            var strings = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as! [String: String]
+            strings.removeValue(forKey: "%@ Game")
             res[localization] = strings
         } catch {
             res[localization] = [:]
@@ -163,25 +168,31 @@ func systemDocuments(typeName: String, extensions: [String]) -> [[String : Any]]
     let template: [String : Any] = [
         "NSDocumentClass": "OEGameDocument",
         "CFBundleTypeRole": "Viewer",
-        "CFBundleTypeOSTypes": ["????"],
         "CFBundleTypeName": typeName
     ]
     
-    let ownedExtensions = extensions.filter({ !extensionGrayList.contains($0) })
-    let unownedExtensions = extensions.filter({ extensionGrayList.contains($0) })
+    let extensionsDefault = extensions.filter({ !(extensionGrayListAlternate+extensionGrayListNone).contains($0) }).sorted()
+    let extensionsAlternate = extensions.filter({ extensionGrayListAlternate.contains($0) }).sorted()
+    let extensionsNone = extensions.filter({ extensionGrayListNone.contains($0) }).sorted()
     
     var res: [[String : Any]] = []
     
-    if ownedExtensions.count > 0 {
+    if extensionsDefault.count > 0 {
         res += [template.merging([
-            "LSHandlerRank": "Owner",
-            "CFBundleTypeExtensions": ownedExtensions
+            "LSHandlerRank": "Default",
+            "CFBundleTypeExtensions": extensionsDefault
         ]) { (a, _) in a }]
     }
-    if unownedExtensions.count > 0 {
+    if extensionsAlternate.count > 0 {
+        res += [template.merging([
+            "LSHandlerRank": "Alternate",
+            "CFBundleTypeExtensions": extensionsAlternate
+        ]) { (a, _) in a }]
+    }
+    if extensionsNone.count > 0 {
         res += [template.merging([
             "LSHandlerRank": "None",
-            "CFBundleTypeExtensions": unownedExtensions
+            "CFBundleTypeExtensions": extensionsNone
         ]) { (a, _) in a }]
     }
     

OpenEmu/OpenEmu-Info.plist
@@ -9,7 +9,7 @@
 		<dict>
 			<key>CFBundleTypeExtensions</key>
 			<array>
-				<string>tar.gz</string>
+				<string>tgz</string>
 				<string>tar</string>
 				<string>gz</string>
 				<string>zip</string>
@@ -23,7 +23,7 @@
 			<key>CFBundleTypeRole</key>
 			<string>Viewer</string>
 			<key>LSHandlerRank</key>
-			<string>None</string>
+			<string>Alternate</string>
 			<key>NSDocumentClass</key>
 			<string>OEGameDocument</string>
 		</dict>

OpenEmu/OpenEmu.xcodeproj/project.pbxproj
@@ -1736,6 +1736,7 @@
 		8F1482FA24EABEAB006E79DB /* HomebrewDeveloperLink.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = HomebrewDeveloperLink.swift; sourceTree = "<group>"; };
 		8F2F92A92519A9E700699691 /* GameScannerButton.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = GameScannerButton.swift; sourceTree = "<group>"; };
 		8F3062DA251A897E00BDFF1F /* OESearchField.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = OESearchField.swift; sourceTree = "<group>"; };
+		8F3693FC2557339300E044DA /* fr-CA */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = "fr-CA"; path = "fr-CA.lproj/InfoPlist.strings"; sourceTree = "<group>"; };
 		8F40EB781C1072B000683055 /* OEMigrationWindowController.xib */ = {isa = PBXFileReference; lastKnownFileType = file.xib; path = OEMigrationWindowController.xib; sourceTree = "<group>"; };
 		8F40EB791C1072B100683055 /* OEPrefControlsController.xib */ = {isa = PBXFileReference; lastKnownFileType = file.xib; path = OEPrefControlsController.xib; sourceTree = "<group>"; };
 		8F40EB7A1C1072B100683055 /* OEPrefCoresController.xib */ = {isa = PBXFileReference; lastKnownFileType = file.xib; path = OEPrefCoresController.xib; sourceTree = "<group>"; };
@@ -5652,6 +5653,7 @@
 				ca,
 				"zh-Hant",
 				"en-GB",
+				"fr-CA",
 			);
 			mainGroup = 2A37F4AAFDCFA73011CA2CEA /* OpenEmu */;
 			projectDirPath = "";
@@ -7306,6 +7308,7 @@
 				01B035531C090E9900BA80E2 /* ru */,
 				3C335CBF1C70E49F00E33B79 /* zh-Hans */,
 				82BBE7BB21D961A1006CD402 /* zh-Hant */,
+				8F3693FC2557339300E044DA /* fr-CA */,
 			);
 			name = InfoPlist.strings;
 			sourceTree = "<group>";

OpenEmu/ca.lproj/InfoPlist.strings
@@ -8,3 +8,5 @@
 "OpenEmu System Plugin" = "Connector del sistema";
 
 "OpenEmu Shader" = "OpenEmu Shader";
+
+"%@ Game" = "%@ Game";  // TODO // Used in OESystemPluginPostInstall to dynamically generate localized file type names

OpenEmu/ca.lproj/Localizable.strings
@@ -637,8 +637,6 @@
 
 "Downloading core list..." = "Downloading core list...";  // TODO
 
-"%@ Game" = "%@ Game";  // TODO // Used in OESystemPluginPostInstall to dynamically generate localized file type names
-
 "The %@ core has quit unexpectedly" = "The %@ core has quit unexpectedly";  // TODO
 
 "<b>If and only if this issue persists</b>, please submit feedback including:<br><br><ul><li>The model of Mac you are using <b>and</b> the version of macOS you have installed<ul><li>This information is found in  > About this Mac</li></ul></li><li>The <b>exact name</b> of the game you were playing</li><li>The crash report of OpenEmuHelperApp<ul><li>Open Console.app, click on \"User Reports\" in the sidebar, then look for the latest document with OpenEmuHelperApp in the name</ul></li></li></ul><br><b>Always search for similar feedback previously reported by other users!</b><br>If any of this information is omitted, or if similar feedback has already been issued, your issue report may be closed." = "<b>If and only if this issue persists</b>, please submit feedback including:<br><br><ul><li>The model of Mac you are using <b>and</b> the version of macOS you have installed<ul><li>This information is found in  > About this Mac</li></ul></li><li>The <b>exact name</b> of the game you were playing</li><li>The crash report of OpenEmuHelperApp<ul><li>Open Console.app, click on \"User Reports\" in the sidebar, then look for the latest document with OpenEmuHelperApp in the name</ul></li></li></ul><br><b>Always search for similar feedback previously reported by other users!</b><br>If any of this information is omitted, or if similar feedback has already been issued, your issue report may be closed.";  // Suggestion for crashed cores (HTML). Localizers: specify the report must be written in English  // TODO

OpenEmu/de.lproj/InfoPlist.strings
@@ -8,3 +8,5 @@
 "OpenEmu System Plugin" = "OpenEmu-System";
 
 "OpenEmu Shader" = "OpenEmu-Shader";
+
+"%@ Game" = "%@-Spiel";  // TODO // Used in OESystemPluginPostInstall to dynamically generate localized file type names

OpenEmu/de.lproj/Localizable.strings
@@ -639,8 +639,6 @@
 
 "Downloading core list..." = "Downloading core list...";  // TODO
 
-"%@ Game" = "%@ Game";  // TODO // Used in OESystemPluginPostInstall to dynamically generate localized file type names
-
 "The %@ core has quit unexpectedly" = "The %@ core has quit unexpectedly";  // TODO
 
 "<b>If and only if this issue persists</b>, please submit feedback including:<br><br><ul><li>The model of Mac you are using <b>and</b> the version of macOS you have installed<ul><li>This information is found in  > About this Mac</li></ul></li><li>The <b>exact name</b> of the game you were playing</li><li>The crash report of OpenEmuHelperApp<ul><li>Open Console.app, click on \"User Reports\" in the sidebar, then look for the latest document with OpenEmuHelperApp in the name</ul></li></li></ul><br><b>Always search for similar feedback previously reported by other users!</b><br>If any of this information is omitted, or if similar feedback has already been issued, your issue report may be closed." = "<b>If and only if this issue persists</b>, please submit feedback including:<br><br><ul><li>The model of Mac you are using <b>and</b> the version of macOS you have installed<ul><li>This information is found in  > About this Mac</li></ul></li><li>The <b>exact name</b> of the game you were playing</li><li>The crash report of OpenEmuHelperApp<ul><li>Open Console.app, click on \"User Reports\" in the sidebar, then look for the latest document with OpenEmuHelperApp in the name</ul></li></li></ul><br><b>Always search for similar feedback previously reported by other users!</b><br>If any of this information is omitted, or if similar feedback has already been issued, your issue report may be closed.";  // Suggestion for crashed cores (HTML). Localizers: specify the report must be written in English  // TODO

OpenEmu/en.lproj/Localizable.strings
@@ -637,8 +637,6 @@
 
 "Downloading core list..." = "Downloading core list...";
 
-"%@ Game" = "%@ Game";  // Used in OESystemPluginPostInstall to dynamically generate localized file type names
-
 "The %@ core has quit unexpectedly" = "The %@ core has quit unexpectedly";
 
 "<b>If and only if this issue persists</b>, please submit feedback including:<br><br><ul><li>The model of Mac you are using <b>and</b> the version of macOS you have installed<ul><li>This information is found in  > About this Mac</li></ul></li><li>The <b>exact name</b> of the game you were playing</li><li>The crash report of OpenEmuHelperApp<ul><li>Open Console.app, click on \"User Reports\" in the sidebar, then look for the latest document with OpenEmuHelperApp in the name</ul></li></li></ul><br><b>Always search for similar feedback previously reported by other users!</b><br>If any of this information is omitted, or if similar feedback has already been issued, your issue report may be closed." = "<b>If and only if this issue persists</b>, please submit feedback including:<br><br><ul><li>The model of Mac you are using <b>and</b> the version of macOS you have installed<ul><li>This information is found in  > About this Mac</li></ul></li><li>The <b>exact name</b> of the game you were playing</li><li>The crash report of OpenEmuHelperApp<ul><li>Open Console.app, click on \"User Reports\" in the sidebar, then look for the latest document with OpenEmuHelperApp in the name</ul></li></li></ul><br><b>Always search for similar feedback previously reported by other users!</b><br>If any of this information is omitted, or if similar feedback has already been issued, your issue report may be closed.";  // Suggestion for crashed cores (HTML). Localizers: specify the report must be written in English

commit 375f1b6705065549d3235688353743b5a87cc3f2

Fix unlocalized document type names in experimental builds
Regression from 6b08dcb.

OpenEmu/OESystemPluginPostInstall/main.swift
@@ -85,8 +85,7 @@ func readLocalizedInfoPlistStrings(appBundle: Bundle) -> [String: [String: Strin
         let plist = appBundle.resourceURL!.appendingPathComponent(localization + ".lproj/InfoPlist.strings")
         do {
             let data = try Data(contentsOf: plist)
-            var strings = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as! [String: String]
-            strings.removeValue(forKey: "%@ Game")
+            let strings = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as! [String: String]
             res[localization] = strings
         } catch {
             res[localization] = [:]